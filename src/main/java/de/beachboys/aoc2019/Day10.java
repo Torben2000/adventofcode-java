d39b2f90e981a0bc20f76080bf3c73da8b248b2f
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2020-12-01 09:02:51 +0100

    Add day 2019-10

--- src/main/java/de/beachboys/aoc2019/Day10.java
+++ src/main/java/de/beachboys/aoc2019/Day10.java
@@ -1,17 +1,145 @@
 package de.beachboys.aoc2019;
 
 import de.beachboys.Day;
+import org.javatuples.Pair;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Day10 extends Day {
 
+    Pair<Integer, Integer> station = Pair.with(-1, -1);
+
     public Object part1(List<String> input) {
-        return "part1result";
+        boolean[][] isAsteroid = buildAsteroidMap(input);
+        long maxAsteroids = 0L;
+        for (int i = 0; i < input.size(); i++) {
+            for (int j = 0; j < isAsteroid[i].length; j++) {
+                if (isAsteroid[i][j]) {
+                    boolean[][] asteroidsInSight = getAsteroidsInLineOfSight(isAsteroid, i, j);
+                    long numOfAsteroidsInSight = 0L;
+                    for (int x = 0; x < asteroidsInSight.length; x++) {
+                        for (int y = 0; y < asteroidsInSight[i].length; y++) {
+                            if (asteroidsInSight[x][y]) {
+                                numOfAsteroidsInSight++;
+                            }
+                        }
+                    }
+                    if (maxAsteroids < numOfAsteroidsInSight) {
+                        maxAsteroids = numOfAsteroidsInSight;
+                        station = Pair.with(i, j);
+                        io.logDebug(station  + ": " + numOfAsteroidsInSight);
+                    }
+                }
+            }
+        }
+        io.logInfo("Found station: " + station.getValue0() + "," + station.getValue1());
+        return maxAsteroids;
+    }
+
+    private boolean[][] buildAsteroidMap(List<String> input) {
+        boolean[][] isAsteroid = new boolean[input.get(0).length()][input.size()];
+        for (int y = 0; y < input.size(); y++) {
+            String currentLine = input.get(y);
+            for (int x = 0; x < currentLine.length(); x++) {
+                isAsteroid[x][y] = currentLine.charAt(x) == '#';
+            }
+        }
+        return isAsteroid;
+    }
+
+    private boolean[][] getAsteroidsInLineOfSight(boolean[][] isAsteroid, int i, int j) {
+        boolean[][] checkedAsteroids = new boolean[isAsteroid.length][isAsteroid[0].length];
+        boolean[][] asteroidsInSight = new boolean[isAsteroid.length][isAsteroid[0].length];
+
+
+        for (int stepX = 0; stepX >= -i; stepX--) {
+            for (int stepY = 0; stepY >= -j; stepY--) {
+                updateAsteroidsInSight(asteroidsInSight, isAsteroid, i, j, checkedAsteroids, stepX, stepY);
+
+            }
+        }
+        for (int stepX = 0; stepX >= -i; stepX--) {
+            for (int stepY = 0; stepY < isAsteroid[i].length - j; stepY++) {
+                updateAsteroidsInSight(asteroidsInSight, isAsteroid, i, j, checkedAsteroids, stepX, stepY);
+
+            }
+        }
+        for (int stepX = 0; stepX < isAsteroid.length - i; stepX++) {
+            for (int stepY = 0; stepY < isAsteroid[i].length - j; stepY++) {
+                updateAsteroidsInSight(asteroidsInSight, isAsteroid, i, j, checkedAsteroids, stepX, stepY);
+            }
+        }
+        for (int stepX = 0; stepX < isAsteroid.length - i; stepX++) {
+            for (int stepY = 0; stepY >= -j; stepY--) {
+                updateAsteroidsInSight(asteroidsInSight, isAsteroid, i, j, checkedAsteroids, stepX, stepY);
+            }
+        }
+        return asteroidsInSight;
+    }
+
+    private void updateAsteroidsInSight(boolean[][] asteroidsInSight, boolean[][] isAsteroid, int i, int j, boolean[][] checkedAsteroids, int stepX, int stepY) {
+        if (stepX == 0 && stepY == 0) {
+            return;
+        }
+        int x = i;
+        int y = j;
+        boolean asteroidFoundInLineOfSight = false;
+        while (isInRange(isAsteroid, x, y)) {
+            x += stepX;
+            y += stepY;
+            if (isInRange(isAsteroid, x, y) && !checkedAsteroids[x][y]) {
+                checkedAsteroids[x][y] = true;
+                if (!asteroidFoundInLineOfSight && isAsteroid[x][y]) {
+                    asteroidsInSight[x][y] = true;
+                    asteroidFoundInLineOfSight = true;
+                }
+            }
+        }
+    }
+
+    private boolean isInRange(boolean[][] isAsteroid, int x, int y) {
+        return x >= 0 && y >= 0 && x < isAsteroid.length && y < isAsteroid[0].length;
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        boolean[][] isAsteroid = buildAsteroidMap(input);
+
+        // find station
+        part1(input);
+
+        int asteroidIndex = Integer.parseInt(io.getInput("Asteroid index:"));
+        int currentIndex = 0;
+        boolean asteroidsFound = true;
+        while (asteroidsFound) {
+            boolean[][] asteroidsInSight = getAsteroidsInLineOfSight(isAsteroid, station.getValue0(), station.getValue1());
+            Map<Double, Pair<Integer, Integer>> orderedAsteroidsInSight = new TreeMap<>();
+            for (int i = 0; i < asteroidsInSight.length; i++) {
+                for (int j = 0; j < asteroidsInSight[i].length; j++) {
+                    if (asteroidsInSight[i][j]) {
+                        orderedAsteroidsInSight.put(getOrderValueOfAsteroid(station, i, j), Pair.with(i, j));
+                        isAsteroid[i][j] = false;
+                    }
+                }
+            }
+            for (Map.Entry<Double, Pair<Integer, Integer>> entry : orderedAsteroidsInSight.entrySet()) {
+                currentIndex++;
+                if (currentIndex == asteroidIndex) {
+                    return entry.getValue().getValue0()*100+entry.getValue().getValue1();
+                }
+
+            }
+            asteroidsFound = !orderedAsteroidsInSight.isEmpty();
+        }
+        return"error";
+    }
+
+    private Double getOrderValueOfAsteroid(Pair<Integer, Integer> station, int x, int y) {
+        int diffX = x-station.getValue0();
+        int diffY = y-station.getValue1();
+        return -1 * Math.atan2(diffX, diffY);
     }
 
 }
--- src/test/java/de/beachboys/aoc2019/Day10Test.java
+++ src/test/java/de/beachboys/aoc2019/Day10Test.java
@@ -3,6 +3,7 @@ package de.beachboys.aoc2019;
 import de.beachboys.Day;
 import de.beachboys.DayTest;
 import de.beachboys.IOHelper;
+import de.beachboys.IOHelperForTests;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -16,15 +17,98 @@ public class Day10Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputlines"), "part1result", null)
+                Arguments.of(List.of(".#..#",
+                        ".....",
+                        "#####",
+                        "....#",
+                        "...##"), 8, new IOHelperForTests(null, List.of("Found station: 3,4"))),
+                Arguments.of(List.of("......#.#.",
+                        "#..#.#....",
+                        "..#######.",
+                        ".#.#.###..",
+                        ".#..#.....",
+                        "..#....#.#",
+                        "#..#....#.",
+                        ".##.#..###",
+                        "##...#..#.",
+                        ".#....####"), 33, new IOHelperForTests(null, List.of("Found station: 5,8"))),
+                Arguments.of(List.of("#.#...#.#.",
+                        ".###....#.",
+                        ".#....#...",
+                        "##.#.#.#.#",
+                        "....#.#.#.",
+                        ".##..###.#",
+                        "..#...##..",
+                        "..##....##",
+                        "......#...",
+                        ".####.###."), 35, new IOHelperForTests(null, List.of("Found station: 1,2"))),
+                Arguments.of(List.of(".#..#..###",
+                        "####.###.#",
+                        "....###.#.",
+                        "..###.##.#",
+                        "##.##.#.#.",
+                        "....###..#",
+                        "..#.#..#.#",
+                        "#..#.#.###",
+                        ".##...##.#",
+                        ".....#.#.."), 41, new IOHelperForTests(null, List.of("Found station: 6,3"))),
+                Arguments.of(List.of(".#..##.###...#######",
+                        "##.############..##.",
+                        ".#.######.########.#",
+                        ".###.#######.####.#.",
+                        "#####.##.#.##.###.##",
+                        "..#####..#.#########",
+                        "####################",
+                        "#.####....###.#.#.##",
+                        "##.#################",
+                        "#####.##.###..####..",
+                        "..######..##.#######",
+                        "####.##.####...##..#",
+                        ".#####..#.######.###",
+                        "##...#.##########...",
+                        "#.##########.#######",
+                        ".####.#.###.###.#.##",
+                        "....##.##.###..#####",
+                        ".#.#.###########.###",
+                        "#.#.#.#####.####.###",
+                        "###.##.####.##.#..##"), 210, new IOHelperForTests(null, List.of("Found station: 11,13")))
 
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
+        List<String> map = List.of(".#..##.###...#######",
+                "##.############..##.",
+                ".#.######.########.#",
+                ".###.#######.####.#.",
+                "#####.##.#.##.###.##",
+                "..#####..#.#########",
+                "####################",
+                "#.####....###.#.#.##",
+                "##.#################",
+                "#####.##.###..####..",
+                "..######..##.#######",
+                "####.##.####...##..#",
+                ".#####..#.######.###",
+                "##...#.##########...",
+                "#.##########.#######",
+                ".####.#.###.###.#.##",
+                "....##.##.###..#####",
+                ".#.#.###########.###",
+                "#.#.#.#####.####.###",
+                "###.##.####.##.#..##");
         return Stream.of(
-                Arguments.of(List.of("inputlines"), 2, null)
-
+                Arguments.of(map, 1112, new IOHelperForTests(List.of("1"), List.of("Found station: 11,13"))),
+                Arguments.of(map, 1201, new IOHelperForTests(List.of("2"), List.of("Found station: 11,13"))),
+                Arguments.of(map, 1202, new IOHelperForTests(List.of("3"), List.of("Found station: 11,13"))),
+                Arguments.of(map, 1208, new IOHelperForTests(List.of("10"), List.of("Found station: 11,13"))),
+                Arguments.of(map, 1600, new IOHelperForTests(List.of("20"), List.of("Found station: 11,13"))),
+                Arguments.of(map, 1609, new IOHelperForTests(List.of("50"), List.of("Found station: 11,13"))),
+                Arguments.of(map, 1016, new IOHelperForTests(List.of("100"), List.of("Found station: 11,13"))),
+                Arguments.of(map, 906, new IOHelperForTests(List.of("199"), List.of("Found station: 11,13"))),
+                Arguments.of(map, 802, new IOHelperForTests(List.of("200"), List.of("Found station: 11,13"))),
+                Arguments.of(map, 1009, new IOHelperForTests(List.of("201"), List.of("Found station: 11,13"))),
+                Arguments.of(map, 1101, new IOHelperForTests(List.of("299"), List.of("Found station: 11,13")))
         );
     }
 
