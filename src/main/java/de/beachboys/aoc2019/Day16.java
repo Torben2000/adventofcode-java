e3b40d0bbc2c3e0dd74829f5090b97743abae3a1
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2020-12-05 22:32:37 +0100

    Add day 2019-16

--- src/main/java/de/beachboys/aoc2019/Day16.java
+++ src/main/java/de/beachboys/aoc2019/Day16.java
@@ -3,15 +3,81 @@ package de.beachboys.aoc2019;
 import de.beachboys.Day;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Day16 extends Day {
 
     public Object part1(List<String> input) {
-        return "part1result";
+        return calculateResult(input.get(0), 0, this::recalculateDigitsPart1);
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        String numbers = input.get(0).repeat(10000);
+        int offset = Integer.parseInt(numbers.substring(0, 7));
+        return calculateResult(numbers, offset, this::recalculateDigitsPart2);
+    }
+
+    private String calculateResult(String numbers, int offset, Consumer<int[]> recalculationLogic) {
+        int numberOfPhases = Integer.parseInt(io.getInput("Number of phases"));
+        for (int i = 0; i < numberOfPhases; i++) {
+            numbers = runPhase(numbers, recalculationLogic);
+        }
+        return numbers.substring(offset, offset + 8);
+    }
+
+    private String runPhase(String numbers, Consumer<int[]> recalculationLogic) {
+        int[] digits = getDigits((numbers));
+        recalculationLogic.accept(digits);
+        return getNumberString(digits);
+    }
+
+    private void recalculateDigitsPart1(int[] digits) {
+        for (int i = 0; i < digits.length; i++) {
+            int currentValue = 0;
+            for (int j = i; j < digits.length; j++) {
+                currentValue += digits[j] * getFactor(i, j);
+            }
+            digits[i] = Math.abs(currentValue % 10);
+        }
+    }
+
+    private int getFactor(int i, int j) {
+        switch (((((j + 1) / (i + 1)) + 1) % 4)) {
+            case 0:
+                return -1;
+            case 1:
+            case 3:
+                return 0;
+            case 2:
+                return 1;
+        }
+        throw new IllegalArgumentException();
+    }
+
+    private void recalculateDigitsPart2(int[] digits) {
+        // Magic that I only get partially...but it works
+        int currentValue = 0;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            currentValue += digits[i];
+            currentValue %= 10;
+            digits[i] = currentValue;
+        }
+    }
+
+    private String getNumberString(int[] digits) {
+        StringBuilder newNumber = new StringBuilder();
+        for (int digit : digits) {
+            newNumber.append(digit);
+        }
+        return newNumber.toString();
+    }
+
+    private int[] getDigits(String numbers) {
+        int[] digits = new int[numbers.length()];
+        for (int i = 0; i < digits.length; i++) {
+            digits[i] = numbers.charAt(i) - 48;
+        }
+        return digits;
     }
 
 }
--- src/test/java/de/beachboys/aoc2019/Day16Test.java
+++ src/test/java/de/beachboys/aoc2019/Day16Test.java
@@ -3,6 +3,7 @@ package de.beachboys.aoc2019;
 import de.beachboys.Day;
 import de.beachboys.DayTest;
 import de.beachboys.IOHelper;
+import de.beachboys.IOHelperForTests;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -16,14 +17,21 @@ public class Day16Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputlines"), "part1result", null)
+                Arguments.of(List.of("12345678"), "48226158", new IOHelperForTests(List.of("1"), null)),
+                Arguments.of(List.of("12345678"), "34040438", new IOHelperForTests(List.of("2"), null)),
+                Arguments.of(List.of("12345678"), "03415518", new IOHelperForTests(List.of("3"), null)),
+                Arguments.of(List.of("80871224585914546619083218645595"), "24176176", new IOHelperForTests(List.of("100"), null)),
+                Arguments.of(List.of("19617804207202209144916044189917"), "73745418", new IOHelperForTests(List.of("100"), null)),
+                Arguments.of(List.of("69317163492948606335995924319873"), "52432133", new IOHelperForTests(List.of("100"), null))
 
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputlines"), 2, null)
+                Arguments.of(List.of("03036732577212944063491565474664"), 84462026, new IOHelperForTests(List.of("100"), null)),
+                Arguments.of(List.of("02935109699940807407585447034323"), 78725270, new IOHelperForTests(List.of("100"), null)),
+                Arguments.of(List.of("03081770884921959731165446850517"), 53553731, new IOHelperForTests(List.of("100"), null))
 
         );
     }
