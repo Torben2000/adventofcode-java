4554f05a4a31e49ac6cc5df7facc48adc819f492
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2020-12-07 06:43:23 +0100

    Add day 2020-07

--- src/main/java/de/beachboys/aoc2020/Day07.java
+++ src/main/java/de/beachboys/aoc2020/Day07.java
@@ -1,17 +1,83 @@
 package de.beachboys.aoc2020;
 
 import de.beachboys.Day;
+import de.beachboys.Util;
+import org.javatuples.Pair;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Day07 extends Day {
 
+    private final Map<String, List<Pair<Long, String>>> bagRelations = new HashMap<>();
+
     public Object part1(List<String> input) {
-        return "part1result";
+        buildBagRelations(input);
+
+        Set<String> possibleBagColors = new HashSet<>();
+        Set<String> checkedBagColors = new HashSet<>();
+        Set<String> uncheckedBagColors = new HashSet<>(List.of("shiny gold"));
+
+        while (!uncheckedBagColors.isEmpty()) {
+            String bagColorToCheck = uncheckedBagColors.stream().findFirst().orElseThrow();
+            for (Map.Entry<String, List<Pair<Long, String>>> bagRelation : bagRelations.entrySet()) {
+                bagRelation.getValue().forEach(innerBag -> {
+                    String innerBagColor = innerBag.getValue1();
+                    if (innerBagColor.equals(bagColorToCheck)) {
+                        String newOuterBagColor = bagRelation.getKey();
+                        if (!checkedBagColors.contains(newOuterBagColor)) {
+                            possibleBagColors.add(newOuterBagColor);
+                            uncheckedBagColors.add(newOuterBagColor);
+                        }
+                    }
+                });
+            }
+            checkedBagColors.add(bagColorToCheck);
+            uncheckedBagColors.remove(bagColorToCheck);
+        }
+        return possibleBagColors.size();
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        buildBagRelations(input);
+
+        Map<String, Long> bagsToCheck = new HashMap<>();
+        bagsToCheck.put("shiny gold", 1L);
+        // start with -1 because the outer shiny gold one does not count
+        long counter = -1;
+
+        while (!bagsToCheck.isEmpty()) {
+            String bagColorToCheck = bagsToCheck.keySet().stream().findFirst().orElseThrow();
+            List<Pair<Long, String>> innerBags = bagRelations.get(bagColorToCheck);
+            for (Pair<Long, String> innerBag : innerBags) {
+                long newInnerBagCount = innerBag.getValue0() * bagsToCheck.get(bagColorToCheck);
+                Long existingInnerBagCount = bagsToCheck.getOrDefault(innerBag.getValue1(), 0L);
+                bagsToCheck.put(innerBag.getValue1(), existingInnerBagCount + newInnerBagCount);
+            }
+            counter += bagsToCheck.get(bagColorToCheck);
+            bagsToCheck.remove(bagColorToCheck);
+        }
+        return counter;
     }
 
+    private void buildBagRelations(List<String> input) {
+        for (String relationString : input) {
+            String[] relationOuterAndInner = relationString.split(" contain ");
+            List<String> innerBagsStringList = Util.parseToList(relationOuterAndInner[1], ", ");
+            List<Pair<Long, String>> innerBags = innerBagsStringList.stream().map(this::buildBagCountAndColor).filter(p -> p.getValue0() != 0L).collect(Collectors.toList());
+            bagRelations.put(getColor(relationOuterAndInner[0]), innerBags);
+        }
+    }
+
+    private Pair<Long, String> buildBagCountAndColor(String bagString) {
+        String[] splitBag = bagString.split(" ", 2);
+        long count = Long.parseLong(splitBag[0].replace("no", "0"));
+        String color = getColor(splitBag[1]);
+        return Pair.with(count, color);
+    }
+
+    private String getColor(String bagWithColor) {
+        // works for "bag", "bags" and "bags."
+        return bagWithColor.substring(0, bagWithColor.indexOf(" bag"));
+    }
 }
--- src/test/java/de/beachboys/aoc2020/Day07Test.java
+++ src/test/java/de/beachboys/aoc2020/Day07Test.java
@@ -14,14 +14,28 @@ public class Day07Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputlines"), "part1result", null)
+                Arguments.of(List.of("light red bags contain 1 bright white bag, 2 muted yellow bags.",
+                        "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+                        "bright white bags contain 1 shiny gold bag.",
+                        "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+                        "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+                        "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+                        "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+                        "faded blue bags contain no other bags.",
+                        "dotted black bags contain no other bags."), 4, null)
 
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputlines"), 2, null)
+                Arguments.of(List.of("shiny gold bags contain 2 dark red bags.",
+                        "dark red bags contain 2 dark orange bags.",
+                        "dark orange bags contain 2 dark yellow bags.",
+                        "dark yellow bags contain 2 dark green bags.",
+                        "dark green bags contain 2 dark blue bags.",
+                        "dark blue bags contain 2 dark violet bags.",
+                        "dark violet bags contain no other bags."), 126, null)
 
         );
     }
