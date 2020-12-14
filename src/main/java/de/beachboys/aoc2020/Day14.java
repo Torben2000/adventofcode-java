861ec58995bf04329e1cf4bb3bc6e827eed4fccf
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2020-12-14 07:18:46 +0100

    Add day 2020-14

--- src/main/java/de/beachboys/aoc2020/Day14.java
+++ src/main/java/de/beachboys/aoc2020/Day14.java
@@ -1,17 +1,125 @@
 package de.beachboys.aoc2020;
 
 import de.beachboys.Day;
+import org.javatuples.KeyValue;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Day14 extends Day {
 
     public Object part1(List<String> input) {
-        return "part1result";
+        Map<Long, Long> valueMap = new HashMap<>();
+        Boolean[] mask = new Boolean[0];
+        for (String line : input) {
+            if (line.startsWith("mask = ")) {
+                mask = parseMask(line);
+            } else {
+                KeyValue<Long, Long> memLine = parseMemLine(line);
+                valueMap.put(memLine.getKey(), adjustValueWithMask(mask, memLine.getValue()));
+            }
+        }
+        return getSumOfValues(valueMap);
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        Map<Long, Long> valueMap = new HashMap<>();
+        Boolean[] mask = new Boolean[0];
+        for (String line : input) {
+            if (line.startsWith("mask = ")) {
+                mask = parseMask(line);
+            } else {
+                KeyValue<Long, Long> memLine = parseMemLine(line);
+                long index = memLine.getKey();
+
+                List<Integer> floatingBits = new ArrayList<>();
+                for (int i = 0; i < mask.length; i++) {
+                    int bitIndex = mask.length - 1 - i;
+                    if (mask[i] != null) {
+                        if (mask[i]) {
+                            index = setTo1(index, bitIndex);
+                        }
+                    } else {
+                        floatingBits.add(i);
+                    }
+                }
+
+                for (int bitMask = 0; bitMask < Math.pow(2, floatingBits.size()); bitMask++) {
+                    long indexToSet = index;
+                    for (int i = 0; i < floatingBits.size(); i++) {
+                        int bitIndex = mask.length - 1 - floatingBits.get(i);
+                        int bitMaskBitIndex = floatingBits.size() - i - 1;
+                        if (is1AtIndex(bitMask, bitMaskBitIndex)) {
+                            indexToSet = setTo1(indexToSet, bitIndex);
+                        } else {
+                            indexToSet = setTo0(indexToSet, bitIndex);
+                        }
+                    }
+                    valueMap.put(indexToSet, memLine.getValue());
+                }
+            }
+        }
+        return getSumOfValues(valueMap);
+    }
+
+    private long adjustValueWithMask(Boolean[] mask, long value) {
+        long returnValue = value;
+        for (int i = 0; i < mask.length; i++) {
+            int bitIndex = mask.length - 1 - i;
+            if (mask[i] != null) {
+                if (mask[i]) {
+                    returnValue = setTo1(returnValue, bitIndex);
+                } else {
+                    returnValue = setTo0(returnValue, bitIndex);
+                }
+            }
+        }
+        return returnValue;
+    }
+
+    private long setTo1(long value, int bitIndex) {
+        return value | (1L << bitIndex);
+    }
+
+    private long setTo0(long value, int bitIndex) {
+        if (is1AtIndex(value, bitIndex)) {
+            value = value ^ (1L << bitIndex);
+        }
+        return value;
+    }
+
+    private boolean is1AtIndex(long bitMask, int bitMaskBitIndex) {
+        return (bitMask & (1L << bitMaskBitIndex)) != 0;
+    }
+
+    private Boolean[] parseMask(String line) {
+        Boolean[] mask;
+        String maskS = line.substring("mask = ".length());
+        mask = new Boolean[maskS.length()];
+        for (int i = 0; i < mask.length; i++) {
+            switch (maskS.charAt(i)) {
+                case '1':
+                    mask[i] = true;
+                    break;
+                case '0':
+                    mask[i] = false;
+                    break;
+            }
+        }
+        return mask;
+    }
+
+    private KeyValue<Long, Long> parseMemLine(String line) {
+        String[] a = line.split(" = ");
+        long index = Integer.parseInt(a[0].substring(4, a[0].length() - 1));
+        long value = Long.parseLong(a[1]);
+        return KeyValue.with(index, value);
+    }
+
+    private long getSumOfValues(Map<Long, Long> valueMap) {
+        return valueMap.values().stream().mapToLong(i -> i).sum();
     }
 
 }
--- src/test/java/de/beachboys/aoc2020/Day14Test.java
+++ src/test/java/de/beachboys/aoc2020/Day14Test.java
@@ -14,14 +14,20 @@ public class Day14Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputlines"), "part1result", null)
+                Arguments.of(List.of("mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X",
+                        "mem[8] = 11",
+                        "mem[7] = 101",
+                        "mem[8] = 0"), "165", null)
 
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputlines"), 2, null)
+                Arguments.of(List.of("mask = 000000000000000000000000000000X1001X",
+                        "mem[42] = 100",
+                        "mask = 00000000000000000000000000000000X0XX",
+                        "mem[26] = 1"), 208, null)
 
         );
     }
