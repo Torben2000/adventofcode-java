d4cbd876fbbb3c6230f2c70293944b7dd2c7c74f
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2020-12-14 05:52:53 +0100

    Add missing file

---
+++ src/main/java/de/beachboys/GraphConstructionHelper.java
@@ -0,0 +1,49 @@
+package de.beachboys;
+
+import org.javatuples.Pair;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class GraphConstructionHelper {
+
+    private final Set<String> unpassableMapValues;
+    protected final Map<Pair<Integer, Integer>, String> map;
+    private int crossingCounter = 0;
+
+    public GraphConstructionHelper(Map<Pair<Integer, Integer>, String> map) {
+        this(map, Set.of("#", " "));
+    }
+
+    public GraphConstructionHelper(Map<Pair<Integer, Integer>, String> map, Set<String> unpassableMapValues) {
+        this.map = map;
+        this.unpassableMapValues = unpassableMapValues;
+    }
+
+    public List<Pair<Integer, Integer>> getPossibleNavigationPositions(Pair<Integer, Integer> currentPosition) {
+        return List.of(Pair.with(currentPosition.getValue0(), currentPosition.getValue1() - 1),
+                Pair.with(currentPosition.getValue0(), currentPosition.getValue1() + 1),
+                Pair.with(currentPosition.getValue0() - 1, currentPosition.getValue1()),
+                Pair.with(currentPosition.getValue0() + 1, currentPosition.getValue1()));
+    }
+
+    public String getNodeName(Pair<Integer, Integer> nodePosition) {
+        String newNodeName;
+        if (".".equals(map.get(nodePosition))) {
+            newNodeName = "crossing" + crossingCounter++;
+        } else {
+            newNodeName = map.get(nodePosition);
+        }
+        return newNodeName;
+    }
+
+    public boolean createNodeForPosition(Pair<Integer, Integer> currentPosition) {
+        return !map.get(currentPosition).matches("\\.");
+    }
+
+    public boolean isPossibleNextStep(Pair<Integer, Integer> pos) {
+        return !unpassableMapValues.contains(map.get(pos));
+    }
+
+}
