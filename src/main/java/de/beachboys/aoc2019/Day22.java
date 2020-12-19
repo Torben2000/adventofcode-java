47a88c606000ae05cb4ac18cd73367f205f93caa
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2020-12-19 14:29:57 +0100

    Add day 2019-22.1

--- src/main/java/de/beachboys/aoc2019/Day22.java
+++ src/main/java/de/beachboys/aoc2019/Day22.java
@@ -2,14 +2,46 @@ package de.beachboys.aoc2019;
 
 import de.beachboys.Day;
 
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Day22 extends Day {
 
     public Object part1(List<String> input) {
-        return "part1result";
-    }
+        long deckSize = 10007;
+        String deckSizeAsInput = io.getInput("Deck size (default 10007):");
+        if (!deckSizeAsInput.isEmpty()) {
+            deckSize = Long.parseLong(deckSizeAsInput);
+        }
+        long positionOfCard = 2019;
+        String positionOfCardAsInput = io.getInput("Position of card (default 2019):");
+        if (!positionOfCardAsInput.isEmpty()) {
+            positionOfCard = Long.parseLong(positionOfCardAsInput);
+        }
 
+        List<Function<Long, Long>> ops = new ArrayList<>();
+
+        long finalDeckSize = deckSize;
+        for (String line : input) {
+            if (line.startsWith("deal into")) {
+                ops.add(pos -> finalDeckSize - pos - 1);
+            } else if (line.startsWith("cut")) {
+                final int cut = Integer.parseInt(line.substring(4));
+                ops.add(pos -> (pos - cut + finalDeckSize) % finalDeckSize);
+            } else {
+                final int increment = Integer.parseInt(line.substring("deal with increment ".length()));
+                ops.add(pos -> (pos * increment) % finalDeckSize);
+            }
+        }
+
+        for (Function<Long, Long> op : ops) {
+            positionOfCard = op.apply(positionOfCard);
+        }
+
+
+        return positionOfCard;
+    }
     public Object part2(List<String> input) {
         return 2;
     }
--- src/test/java/de/beachboys/aoc2019/Day22Test.java
+++ src/test/java/de/beachboys/aoc2019/Day22Test.java
@@ -3,6 +3,7 @@ package de.beachboys.aoc2019;
 import de.beachboys.Day;
 import de.beachboys.DayTest;
 import de.beachboys.IOHelper;
+import de.beachboys.IOHelperForTests;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -16,8 +17,41 @@ public class Day22Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputlines"), "part1result", null)
-
+                Arguments.of(List.of("deal with increment 7",
+                        "deal into new stack",
+                        "deal into new stack"), 7, new IOHelperForTests(List.of("10", "1"), null)),
+                Arguments.of(List.of("deal with increment 7",
+                        "deal into new stack",
+                        "deal into new stack"), 5, new IOHelperForTests(List.of("10", "5"), null)),
+                Arguments.of(List.of("deal with increment 7",
+                        "deal into new stack",
+                        "deal into new stack"), 1, new IOHelperForTests(List.of("10", "3"), null)),
+                Arguments.of(List.of("cut 6",
+                        "deal with increment 7",
+                        "deal into new stack"), 8, new IOHelperForTests(List.of("10", "9"), null)),
+                Arguments.of(List.of("deal with increment 7",
+                        "deal with increment 9",
+                        "cut -2"), 3, new IOHelperForTests(List.of("10", "7"), null)),
+                Arguments.of(List.of("deal into new stack",
+                        "cut -2",
+                        "deal with increment 7",
+                        "cut 8",
+                        "cut -4",
+                        "deal with increment 7",
+                        "cut 3",
+                        "deal with increment 9",
+                        "deal with increment 3",
+                        "cut -1"), 8, new IOHelperForTests(List.of("10", "3"), null)),
+                Arguments.of(List.of("deal into new stack",
+                        "cut -2",
+                        "deal with increment 7",
+                        "cut 8",
+                        "cut -4",
+                        "deal with increment 7",
+                        "cut 3",
+                        "deal with increment 9",
+                        "deal with increment 3",
+                        "cut -1"), 0, new IOHelperForTests(List.of("10", "9"), null))
         );
     }
 
