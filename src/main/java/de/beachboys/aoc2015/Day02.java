f7c67fb9d3567022a16f9fd1aeb3443d58c416c8
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2020-12-28 02:33:32 +0100

    Add day 2015-02

--- src/main/java/de/beachboys/aoc2015/Day02.java
+++ src/main/java/de/beachboys/aoc2015/Day02.java
@@ -7,11 +7,27 @@ import java.util.List;
 public class Day02 extends Day {
 
     public Object part1(List<String> input) {
-        return "part1result";
+        long sum = 0L;
+        for (String dimensionString : input) {
+            String[] dimensionsAsStrings = dimensionString.split("x");
+            long l = Long.parseLong(dimensionsAsStrings[0]);
+            long w = Long.parseLong(dimensionsAsStrings[1]);
+            long h = Long.parseLong(dimensionsAsStrings[2]);
+            sum += 2 * l * w + 2 * w * h + 2 * l * h + Math.min(Math.min(l * w, w * h), l * h);
+        }
+        return sum;
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        long sum = 0L;
+        for (String dimensionString : input) {
+            String[] dimensionsAsStrings = dimensionString.split("x");
+            long l = Long.parseLong(dimensionsAsStrings[0]);
+            long w = Long.parseLong(dimensionsAsStrings[1]);
+            long h = Long.parseLong(dimensionsAsStrings[2]);
+            sum += l * w * h + 2 * Math.min(Math.min(l + w, w + h), l + h);
+        }
+        return sum;
     }
 
 }
--- src/test/java/de/beachboys/aoc2015/Day02Test.java
+++ src/test/java/de/beachboys/aoc2015/Day02Test.java
@@ -15,15 +15,15 @@ public class Day02Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("2x3x4"), 58, null),
+                Arguments.of(List.of("1x1x10"), 43, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("2x3x4"), 34, null),
+                Arguments.of(List.of("1x1x10"), 14, null)
         );
     }
 
