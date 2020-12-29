8e40122217d98b17ca3c611b4c36d1374d5aa240
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2020-12-29 13:52:06 +0100

    Add day 2015-14

--- src/main/java/de/beachboys/aoc2015/Day14.java
+++ src/main/java/de/beachboys/aoc2015/Day14.java
@@ -2,16 +2,109 @@ package de.beachboys.aoc2015;
 
 import de.beachboys.Day;
 
-import java.util.List;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day14 extends Day {
 
     public Object part1(List<String> input) {
-        return "part1result";
+        int raceDuration = getRaceDuration();
+        List<Reindeer> reindeers = getReindeerList(input);
+
+        int maxDistance = 0;
+        for (Reindeer reindeer : reindeers) {
+            int totalDistance = getTotalDistance(raceDuration, reindeer);
+            maxDistance = Math.max(maxDistance, totalDistance);
+        }
+        return maxDistance;
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        int raceDuration = getRaceDuration();
+        List<Reindeer> reindeers = getReindeerList(input);
+
+        Map<String, Integer> points = new HashMap<>();
+        for (int currentTimestamp = 1; currentTimestamp <= raceDuration; currentTimestamp++) {
+            int maxDistance = 0;
+            Set<String> leadingReindeers = new HashSet<>();
+            for (Reindeer reindeer : reindeers) {
+                int totalDistance = getTotalDistance(currentTimestamp, reindeer);
+                if (totalDistance > maxDistance) {
+                    maxDistance = totalDistance;
+                    leadingReindeers.clear();
+                }
+                if (totalDistance >= maxDistance) {
+                    leadingReindeers.add(reindeer.getName());
+                }
+            }
+            for (String leadingReindeer : leadingReindeers) {
+                points.put(leadingReindeer, points.getOrDefault(leadingReindeer, 0) + 1);
+            }
+        }
+        return points.values().stream().mapToInt(Integer::intValue).max().orElseThrow();
     }
 
+    private List<Reindeer> getReindeerList(List<String> input) {
+        List<Reindeer> reindeers = new ArrayList<>();
+        for (String line : input) {
+            Matcher matcher = Pattern.compile("(.*) can fly ([0-9]+) km/s for ([0-9]+) seconds, but then must rest for ([0-9]+) seconds.").matcher(line);
+            if (matcher.matches()) {
+                String name = matcher.group(1);
+                int speed = Integer.parseInt(matcher.group(2));
+                int flyDuration = Integer.parseInt(matcher.group(3));
+                int restDuration = Integer.parseInt(matcher.group(4));
+                reindeers.add(new Reindeer(name, speed, flyDuration, restDuration));
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
+        return reindeers;
+    }
+
+    private int getRaceDuration() {
+        int raceDuration = 2503;
+        String raceDurationAsString = io.getInput("Length of race (default " + raceDuration + "):");
+        if (!raceDurationAsString.isEmpty()) {
+            raceDuration = Integer.parseInt(raceDurationAsString);
+        }
+        return raceDuration;
+    }
+
+    private int getTotalDistance(int raceDuration, Reindeer reindeer) {
+        int cycleTime = reindeer.getFlyDuration() + reindeer.getRestDuration();
+        int completeCycles = raceDuration / cycleTime;
+        int remainingTime = raceDuration % cycleTime;
+        return (completeCycles * reindeer.getFlyDuration() + Math.min(remainingTime, reindeer.getFlyDuration())) * reindeer.getSpeed();
+    }
+
+    private static class Reindeer {
+        private final String name;
+        private final int speed;
+        private final int flyDuration;
+        private final int restDuration;
+
+        private Reindeer(String name, int speed, int flyDuration, int restDuration) {
+            this.name = name;
+            this.speed = speed;
+            this.flyDuration = flyDuration;
+            this.restDuration = restDuration;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getSpeed() {
+            return speed;
+        }
+
+        public int getFlyDuration() {
+            return flyDuration;
+        }
+
+        public int getRestDuration() {
+            return restDuration;
+        }
+    }
 }
--- src/test/java/de/beachboys/aoc2015/Day14Test.java
+++ src/test/java/de/beachboys/aoc2015/Day14Test.java
@@ -3,6 +3,7 @@ package de.beachboys.aoc2015;
 import de.beachboys.Day;
 import de.beachboys.DayTest;
 import de.beachboys.IOHelper;
+import de.beachboys.IOHelperForTests;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -15,15 +16,15 @@ public class Day14Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.",
+                        "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds."), 1120, new IOHelperForTests(List.of("1000"), null))
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.",
+                        "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds."), 689, new IOHelperForTests(List.of("1000"), null))
         );
     }
 
