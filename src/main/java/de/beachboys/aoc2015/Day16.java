117766423f44640328771dfab1321e5295f191c9
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2020-12-29 18:45:18 +0100

    Add day 2015-16

--- src/main/java/de/beachboys/aoc2015/Day16.java
+++ src/main/java/de/beachboys/aoc2015/Day16.java
@@ -2,16 +2,87 @@ package de.beachboys.aoc2015;
 
 import de.beachboys.Day;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.BiPredicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day16 extends Day {
 
     public Object part1(List<String> input) {
-        return "part1result";
+        return runLogic(input, this::isValidPropertyPart1);
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        return runLogic(input, this::isValidPropertyPart2);
+    }
+
+    private Object runLogic(List<String> input, BiPredicate<Map.Entry<String, Integer>, Map<String, Integer>> isValidProperty) {
+        Map<String, Integer> tickerTape = getTickerTape();
+        Map<Integer, Map<String, Integer>> aunts = parseAunts(input);
+        return findCorrectAunt(aunts, tickerTape, isValidProperty);
+    }
+
+    private int findCorrectAunt(Map<Integer, Map<String, Integer>> aunts, Map<String, Integer> tickerTape, BiPredicate<Map.Entry<String, Integer>, Map<String, Integer>> isValidProperty) {
+        return aunts.entrySet().stream().filter(entry -> {
+            boolean match = true;
+            for (Map.Entry<String, Integer> auntProperty : entry.getValue().entrySet()) {
+                if (!isValidProperty.test(auntProperty, tickerTape)) {
+                    match = false;
+                }
+            }
+            return match;
+        }).mapToInt(Map.Entry::getKey).findFirst().orElseThrow();
+    }
+
+    private boolean isValidPropertyPart1(Map.Entry<String, Integer> auntProperty, Map<String, Integer> tickerTape) {
+        return auntProperty.getValue().equals(tickerTape.get(auntProperty.getKey()));
+    }
+
+    private boolean isValidPropertyPart2(Map.Entry<String, Integer> auntProperty, Map<String, Integer> tickerTape) {
+        String key = auntProperty.getKey();
+        Integer value = auntProperty.getValue();
+        Integer tickerValue = tickerTape.get(key);
+        if ("trees".equals(key) || "cats".equals(key)) {
+            return value > tickerValue;
+        }
+        if ("pomeranians".equals(key) || "goldfish".equals(key)) {
+            return value < tickerValue;
+        }
+        return value.equals(tickerValue);
+    }
+
+    private Map<String, Integer> getTickerTape() {
+        return Map.of(
+                "children", 3,
+                "cats", 7,
+                "samoyeds", 2,
+                "pomeranians", 3,
+                "akitas", 0,
+                "vizslas", 0,
+                "goldfish", 5,
+                "trees", 3,
+                "cars", 2,
+                "perfumes", 1
+        );
+    }
+
+    private Map<Integer, Map<String, Integer>> parseAunts(List<String> input) {
+        Map<Integer, Map<String, Integer>> aunts = new HashMap<>();
+        for (String line : input) {
+            Matcher m = Pattern.compile("Sue ([0-9]+): ([a-z]+): ([0-9]+), ([a-z]+): ([0-9]+), ([a-z]+): ([0-9]+)").matcher(line);
+            if (m.matches()) {
+                int number = Integer.parseInt(m.group(1));
+                Map<String, Integer> map = Map.of(
+                        m.group(2), Integer.parseInt(m.group(3)),
+                        m.group(4), Integer.parseInt(m.group(5)),
+                        m.group(6), Integer.parseInt(m.group(7)));
+                aunts.put(number, map);
+            }
+        }
+        return aunts;
     }
 
 }
--- src/test/java/de/beachboys/aoc2015/Day16Test.java
+++ src/test/java/de/beachboys/aoc2015/Day16Test.java
@@ -15,15 +15,39 @@ public class Day16Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("Sue 38: cars: 10, akitas: 5, vizslas: 8",
+                        "Sue 39: akitas: 5, trees: 9, children: 2",
+                        "Sue 40: vizslas: 0, cats: 7, akitas: 0",
+                        "Sue 41: cars: 9, trees: 10, perfumes: 8",
+                        "Sue 42: akitas: 4, trees: 2, goldfish: 3",
+                        "Sue 43: goldfish: 1, cats: 1, akitas: 8",
+                        "Sue 44: goldfish: 8, akitas: 9, vizslas: 4",
+                        "Sue 45: perfumes: 3, goldfish: 4, trees: 0",
+                        "Sue 240: goldfish: 9, trees: 1, perfumes: 1",
+                        "Sue 241: cars: 2, pomeranians: 1, samoyeds: 2",
+                        "Sue 242: akitas: 2, trees: 3, cars: 4",
+                        "Sue 243: vizslas: 6, akitas: 2, samoyeds: 7",
+                        "Sue 244: trees: 0, perfumes: 5, cars: 7",
+                        "Sue 245: goldfish: 10, perfumes: 5, vizslas: 8"), 40, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("Sue 38: cars: 10, akitas: 5, vizslas: 8",
+                        "Sue 39: akitas: 5, trees: 9, children: 2",
+                        "Sue 40: vizslas: 0, cats: 7, akitas: 0",
+                        "Sue 41: cars: 9, trees: 10, perfumes: 8",
+                        "Sue 42: akitas: 4, trees: 2, goldfish: 3",
+                        "Sue 43: goldfish: 1, cats: 1, akitas: 8",
+                        "Sue 44: goldfish: 8, akitas: 9, vizslas: 4",
+                        "Sue 45: perfumes: 3, goldfish: 4, trees: 0",
+                        "Sue 240: goldfish: 9, trees: 1, perfumes: 1",
+                        "Sue 241: cars: 2, pomeranians: 1, samoyeds: 2",
+                        "Sue 242: akitas: 2, trees: 3, cars: 4",
+                        "Sue 243: vizslas: 6, akitas: 2, samoyeds: 7",
+                        "Sue 244: trees: 0, perfumes: 5, cars: 7",
+                        "Sue 245: goldfish: 10, perfumes: 5, vizslas: 8"), 241, null)
         );
     }
 
