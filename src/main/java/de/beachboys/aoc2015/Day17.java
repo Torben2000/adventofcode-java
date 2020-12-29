b92d1b5a9852e8b83ebf81a48912aba4b5fc895a
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2020-12-29 19:49:05 +0100

    Add day 2015-17

--- src/main/java/de/beachboys/aoc2015/Day17.java
+++ src/main/java/de/beachboys/aoc2015/Day17.java
@@ -2,16 +2,50 @@ package de.beachboys.aoc2015;
 
 import de.beachboys.Day;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Day17 extends Day {
 
+    private final Map<Integer, Integer> countPerNumberOfContainers = new HashMap<>();
+
     public Object part1(List<String> input) {
-        return "part1result";
+        fillCountPerNumberOfContainersMap(input);
+        return countPerNumberOfContainers.values().stream().mapToInt(Integer::intValue).sum();
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        fillCountPerNumberOfContainersMap(input);
+        return countPerNumberOfContainers.get(countPerNumberOfContainers.keySet().stream().mapToInt(Integer::intValue).min().orElseThrow());
+    }
+
+    private void fillCountPerNumberOfContainersMap(List<String> input) {
+        int totalEggnog = 150;
+        String totalEggnogAsString = io.getInput("Total eggnog (default " + totalEggnog + "):");
+        if (!totalEggnogAsString.isEmpty()) {
+            totalEggnog = Integer.parseInt(totalEggnogAsString);
+        }
+        List<Integer> containers = input.stream().map(Integer::valueOf).collect(Collectors.toList());
+        countPossibilitiesPerNumberOfContainers(totalEggnog, containers, 0);
+    }
+
+    private void countPossibilitiesPerNumberOfContainers(int totalEggnog, List<Integer> containers, int currentContainerCount) {
+        if (containers.size() == 0) {
+            return;
+        }
+        Integer currentContainer = containers.get(0);
+        List<Integer> containersWithoutCurrentContainer = containers.subList(1, containers.size());
+
+        countPossibilitiesPerNumberOfContainers(totalEggnog, containersWithoutCurrentContainer, currentContainerCount);
+
+        int newContainerCount = currentContainerCount + 1;
+        if (totalEggnog == currentContainer) {
+            countPerNumberOfContainers.put(newContainerCount, countPerNumberOfContainers.getOrDefault(newContainerCount, 0) + 1);
+        } else if (totalEggnog > currentContainer){
+            countPossibilitiesPerNumberOfContainers(totalEggnog - currentContainer, containersWithoutCurrentContainer, newContainerCount);
+        }
     }
 
 }
--- src/test/java/de/beachboys/aoc2015/Day17Test.java
+++ src/test/java/de/beachboys/aoc2015/Day17Test.java
@@ -3,6 +3,7 @@ package de.beachboys.aoc2015;
 import de.beachboys.Day;
 import de.beachboys.DayTest;
 import de.beachboys.IOHelper;
+import de.beachboys.IOHelperForTests;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -15,15 +16,13 @@ public class Day17Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("20", "15", "10", "5", "5"), 4, new IOHelperForTests(List.of("25"), null))
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("20", "15", "10", "5", "5"), 3, new IOHelperForTests(List.of("25"), null))
         );
     }
 
