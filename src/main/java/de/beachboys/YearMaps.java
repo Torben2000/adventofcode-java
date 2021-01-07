3adea984025b90bf4cc749dba08cf9a642b7c760
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2021-01-07 22:58:24 +0100

    Add empty year 2016

--- src/main/java/de/beachboys/YearMaps.java
+++ src/main/java/de/beachboys/YearMaps.java
@@ -6,6 +6,7 @@ import java.util.Map;
 public class YearMaps {
 
     public static final Map<Integer, Map<Integer, Day>> YEARS = new HashMap<>();
+
     static {
         buildStaticMapOfDays();
     }
@@ -17,8 +18,10 @@ public class YearMaps {
     private static void buildStaticMapOfDays() {
         YEARS.put(2020, getDays2020());
         YEARS.put(2019, getDays2019());
+        YEARS.put(2016, getDays2016());
         YEARS.put(2015, getDays2015());
     }
+
     private static Map<Integer, Day> getDays2020() {
         Map<Integer, Day> days = new HashMap<>();
         days.put(1, new de.beachboys.aoc2020.Day01());
@@ -49,7 +52,7 @@ public class YearMaps {
         return days;
     }
 
-    public static Map<Integer, Day> getDays2019() {
+    private static Map<Integer, Day> getDays2019() {
         Map<Integer, Day> days = new HashMap<>();
         days.put(1, new de.beachboys.aoc2019.Day01());
         days.put(2, new de.beachboys.aoc2019.Day02());
@@ -76,6 +79,36 @@ public class YearMaps {
         days.put(23, new de.beachboys.aoc2019.Day23());
         days.put(24, new de.beachboys.aoc2019.Day24());
         days.put(25, new de.beachboys.aoc2019.Day25());
+        return days;
+    }
+
+    private static Map<Integer, Day> getDays2016() {
+        Map<Integer, Day> days = new HashMap<>();
+        days.put(1, new de.beachboys.aoc2016.Day01());
+        days.put(2, new de.beachboys.aoc2016.Day02());
+        days.put(3, new de.beachboys.aoc2016.Day03());
+        days.put(4, new de.beachboys.aoc2016.Day04());
+        days.put(5, new de.beachboys.aoc2016.Day05());
+        days.put(6, new de.beachboys.aoc2016.Day06());
+        days.put(7, new de.beachboys.aoc2016.Day07());
+        days.put(8, new de.beachboys.aoc2016.Day08());
+        days.put(9, new de.beachboys.aoc2016.Day09());
+        days.put(10, new de.beachboys.aoc2016.Day10());
+        days.put(11, new de.beachboys.aoc2016.Day11());
+        days.put(12, new de.beachboys.aoc2016.Day12());
+        days.put(13, new de.beachboys.aoc2016.Day13());
+        days.put(14, new de.beachboys.aoc2016.Day14());
+        days.put(15, new de.beachboys.aoc2016.Day15());
+        days.put(16, new de.beachboys.aoc2016.Day16());
+        days.put(17, new de.beachboys.aoc2016.Day17());
+        days.put(18, new de.beachboys.aoc2016.Day18());
+        days.put(19, new de.beachboys.aoc2016.Day19());
+        days.put(20, new de.beachboys.aoc2016.Day20());
+        days.put(21, new de.beachboys.aoc2016.Day21());
+        days.put(22, new de.beachboys.aoc2016.Day22());
+        days.put(23, new de.beachboys.aoc2016.Day23());
+        days.put(24, new de.beachboys.aoc2016.Day24());
+        days.put(25, new de.beachboys.aoc2016.Day25());
         return days;
     }
 
