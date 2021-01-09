4bf8ccd5dc4fcb2ddb464537ec381e577f0c3ae0
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2021-01-09 23:28:32 +0100

    Add day 2016.9

--- src/main/java/de/beachboys/aoc2016/Day09.java
+++ src/main/java/de/beachboys/aoc2016/Day09.java
@@ -3,15 +3,43 @@ package de.beachboys.aoc2016;
 import de.beachboys.Day;
 
 import java.util.List;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day09 extends Day {
 
+    private Function<String, Long> getRepeatedStringLength;
+
     public Object part1(List<String> input) {
-        return "part1result";
+        getRepeatedStringLength = repeatedString -> (long) repeatedString.length();
+        return getLength(input.get(0));
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        getRepeatedStringLength = this::getLength;
+        return getLength(input.get(0));
+    }
+
+    private long getLength(String compressed) {
+        Pattern p = Pattern.compile("(\\(([0-9]+)x([0-9]+)\\)).*");
+        int position = 0;
+        while (position < compressed.length()) {
+            if (compressed.charAt(position) == '(') {
+                Matcher m = p.matcher(compressed.substring(position));
+                if (m.matches()) {
+                    int markerLength = m.group(1).length();
+                    int numCharacters = Integer.parseInt(m.group(2));
+                    int repeat = Integer.parseInt(m.group(3));
+                    String substringToRepeat = compressed.substring(position + markerLength, position + markerLength + numCharacters);
+                    String followingSubstring = compressed.substring(position + markerLength + numCharacters);
+                    return position + repeat * getRepeatedStringLength.apply(substringToRepeat) + getLength(followingSubstring);
+                }
+            } else {
+                position++;
+            }
+        }
+        return position;
     }
 
 }
--- src/test/java/de/beachboys/aoc2016/Day09Test.java
+++ src/test/java/de/beachboys/aoc2016/Day09Test.java
@@ -15,14 +15,22 @@ public class Day09Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("ADVENT"), 6, null),
+                Arguments.of(List.of("A(1x5)BC"), 7, null),
+                Arguments.of(List.of("(3x3)XYZ"), 9, null),
+                Arguments.of(List.of("A(2x2)BCD(2x2)EFG"), 11, null),
+                Arguments.of(List.of("(6x1)(1x3)A"), 6, null),
+                Arguments.of(List.of("X(8x2)(3x3)ABCY"), 18, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
+                Arguments.of(List.of("ADVENT"), 6, null),
+                Arguments.of(List.of("(3x3)XYZ"), 9, null),
+                Arguments.of(List.of("X(8x2)(3x3)ABCY"), 20, null),
+                Arguments.of(List.of("(27x12)(20x12)(13x14)(7x10)(1x12)A"), 241920, null),
+                Arguments.of(List.of("(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"), 445, null)
 
         );
     }
