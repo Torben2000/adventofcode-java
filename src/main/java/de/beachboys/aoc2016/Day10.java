47626449bba93f4a4b0e50c5a912aeef23a53cf7
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2021-01-10 02:35:34 +0100

    Add day 2016.10

--- src/main/java/de/beachboys/aoc2016/Day10.java
+++ src/main/java/de/beachboys/aoc2016/Day10.java
@@ -1,17 +1,86 @@
 package de.beachboys.aoc2016;
 
 import de.beachboys.Day;
+import de.beachboys.Util;
+import org.javatuples.Pair;
 
-import java.util.List;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Day10 extends Day {
 
+    enum TargetType {
+        BOT, OUTPUT
+    }
+
+    private final Map<Integer, Set<Integer>> botInputs = new HashMap<>();
+    private final Map<Integer, Integer> outputValues = new HashMap<>();
+
     public Object part1(List<String> input) {
-        return "part1result";
+        Integer input1 = Util.getIntValueFromUser("First chip value", 61, io);
+        Integer input2 = Util.getIntValueFromUser("Second chip value", 17, io);
+        runLogic(input);
+        return botInputs.entrySet().stream().filter(e -> e.getValue().equals(Set.of(input1, input2))).map(Map.Entry::getKey).findFirst().orElseThrow();
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        runLogic(input);
+        return outputValues.get(0) * outputValues.get(1) * outputValues.get(2);
+    }
+
+    private void runLogic(List<String> input) {
+        Map<Integer, Pair<Pair<TargetType, Integer>, Pair<TargetType, Integer>>> botRules = buildRulesAndFillInitialInput(input);
+
+        Set<Integer> processedBots = new HashSet<>();
+        while (processedBots.size() < botRules.size()) {
+            Set<Integer> botsToProcess = botInputs.entrySet().stream().filter(e -> !processedBots.contains(e.getKey()) && e.getValue().size() == 2).map(Map.Entry::getKey).collect(Collectors.toSet());
+            for (Integer bot : botsToProcess) {
+                Integer lowValue = botInputs.get(bot).stream().min(Comparator.naturalOrder()).orElseThrow();
+                Integer highValue = botInputs.get(bot).stream().max(Comparator.naturalOrder()).orElseThrow();
+                setTargetValue(botRules.get(bot).getValue0(), lowValue);
+                setTargetValue(botRules.get(bot).getValue1(), highValue);
+                processedBots.add(bot);
+            }
+        }
+    }
+
+    private Map<Integer, Pair<Pair<TargetType, Integer>, Pair<TargetType, Integer>>> buildRulesAndFillInitialInput(List<String> input) {
+        Map<Integer, Pair<Pair<TargetType, Integer>, Pair<TargetType, Integer>>> botRules = new HashMap<>();
+        Pattern inputPattern = Pattern.compile("value ([0-9]+) goes to bot ([0-9]+)");
+        Pattern rulePattern = Pattern.compile("bot ([0-9]+) gives low to (bot|output) ([0-9]+) and high to (bot|output) ([0-9]+)");
+        for (String line : input) {
+            if (line.startsWith("bot")) {
+                Matcher m = rulePattern.matcher(line);
+                if (m.matches()) {
+                    int bot = Integer.parseInt(m.group(1));
+                    TargetType lowTargetType = TargetType.valueOf(m.group(2).toUpperCase());
+                    int lowTarget = Integer.parseInt(m.group(3));
+                    TargetType highTargetType = TargetType.valueOf(m.group(4).toUpperCase());
+                    int highTarget = Integer.parseInt(m.group(5));
+                    botRules.put(bot, Pair.with(Pair.with(lowTargetType, lowTarget), Pair.with(highTargetType, highTarget)));
+                    botInputs.putIfAbsent(bot, new HashSet<>());
+                }
+            } else {
+                Matcher m = inputPattern.matcher(line);
+                if (m.matches()) {
+                    int value = Integer.parseInt(m.group(1));
+                    int bot = Integer.parseInt(m.group(2));
+                    botInputs.putIfAbsent(bot, new HashSet<>());
+                    botInputs.get(bot).add(value);
+                }
+            }
+        }
+        return botRules;
+    }
+
+    private void setTargetValue(Pair<TargetType, Integer> target, Integer value) {
+        if (TargetType.BOT.equals(target.getValue0())) {
+            botInputs.get(target.getValue1()).add(value);
+        } else {
+            outputValues.put(target.getValue1(), value);
+        }
     }
 
 }
--- src/test/java/de/beachboys/aoc2016/Day10Test.java
+++ src/test/java/de/beachboys/aoc2016/Day10Test.java
@@ -3,6 +3,7 @@ package de.beachboys.aoc2016;
 import de.beachboys.Day;
 import de.beachboys.DayTest;
 import de.beachboys.IOHelper;
+import de.beachboys.IOHelperForTests;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -15,14 +16,36 @@ public class Day10Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
+                Arguments.of(List.of("value 5 goes to bot 2",
+                        "bot 2 gives low to bot 1 and high to bot 0",
+                        "value 3 goes to bot 1",
+                        "bot 1 gives low to output 1 and high to bot 0",
+                        "bot 0 gives low to output 2 and high to output 0",
+                        "value 2 goes to bot 2"), 2, new IOHelperForTests(List.of("2", "5"), null)),
+                Arguments.of(List.of("value 5 goes to bot 2",
+                        "bot 2 gives low to bot 1 and high to bot 0",
+                        "value 3 goes to bot 1",
+                        "bot 1 gives low to output 1 and high to bot 0",
+                        "bot 0 gives low to output 2 and high to output 0",
+                        "value 2 goes to bot 2"), 1, new IOHelperForTests(List.of("2", "3"), null)),
+                Arguments.of(List.of("value 5 goes to bot 2",
+                        "bot 2 gives low to bot 1 and high to bot 0",
+                        "value 3 goes to bot 1",
+                        "bot 1 gives low to output 1 and high to bot 0",
+                        "bot 0 gives low to output 2 and high to output 0",
+                        "value 2 goes to bot 2"), 0, new IOHelperForTests(List.of("3", "5"), null))
 
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
+                Arguments.of(List.of("value 5 goes to bot 2",
+                        "bot 2 gives low to bot 1 and high to bot 0",
+                        "value 3 goes to bot 1",
+                        "bot 1 gives low to output 1 and high to bot 0",
+                        "bot 0 gives low to output 2 and high to output 0",
+                        "value 2 goes to bot 2"), 30, null)
 
         );
     }
