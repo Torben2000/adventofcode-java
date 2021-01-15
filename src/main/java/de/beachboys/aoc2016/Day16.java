a1cbbc68d354370bee4e2729718dd8d6e9fa6196
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2021-01-16 00:08:48 +0100

    Add day 2016.16

--- src/main/java/de/beachboys/aoc2016/Day16.java
+++ src/main/java/de/beachboys/aoc2016/Day16.java
@@ -1,17 +1,64 @@
 package de.beachboys.aoc2016;
 
 import de.beachboys.Day;
+import de.beachboys.Util;
 
 import java.util.List;
 
 public class Day16 extends Day {
 
     public Object part1(List<String> input) {
-        return "part1result";
+        return runLogic(input, 272);
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        return runLogic(input, 35651584);
+    }
+
+    private String runLogic(List<String> input, int fileLengthDefaultValue) {
+        int fileLength = Util.getIntValueFromUser("fileLength", fileLengthDefaultValue, io);
+        String fileContent = buildFileContent(input, fileLength);
+        return getChecksum(fileContent);
+    }
+
+    private String buildFileContent(List<String> input, int fileLength) {
+        StringBuilder fileContent = new StringBuilder(input.get(0));
+        while(fileContent.length() < fileLength) {
+            String b = new StringBuilder(
+                    fileContent.toString()
+                    .replaceAll("1", "2")
+                    .replaceAll("0", "1")
+                    .replaceAll("2", "0"))
+                    .reverse().toString();
+            fileContent.append("0").append(b);
+        }
+        return fileContent.substring(0, fileLength);
+    }
+
+    private String getChecksum(String fileContent) {
+        String checksum = fileContent;
+        while (checksum.length() % 2 == 0) {
+            checksum = getNextIterationOfChecksum(checksum);
+        }
+        return checksum;
+    }
+
+    private String getNextIterationOfChecksum(String checksum) {
+        Character firstChar = null;
+        StringBuilder newChecksum = new StringBuilder();
+        for (char character : checksum.toCharArray()) {
+            if (firstChar == null) {
+                firstChar = character;
+            } else {
+                if (firstChar == character) {
+                    newChecksum.append('1');
+                } else {
+                    newChecksum.append('0');
+                }
+                firstChar = null;
+            }
+        }
+        return newChecksum.toString();
     }
 
 }
--- src/test/java/de/beachboys/aoc2016/Day16Test.java
+++ src/test/java/de/beachboys/aoc2016/Day16Test.java
@@ -3,6 +3,7 @@ package de.beachboys.aoc2016;
 import de.beachboys.Day;
 import de.beachboys.DayTest;
 import de.beachboys.IOHelper;
+import de.beachboys.IOHelperForTests;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -15,15 +16,13 @@ public class Day16Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("10000"), "01100", new IOHelperForTests(List.of("20"), null))
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("10000"), "01100", new IOHelperForTests(List.of("20"), null))
         );
     }
 
