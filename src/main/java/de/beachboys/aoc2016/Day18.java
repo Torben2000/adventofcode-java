91980a0cd04b2b68c0d4f1a5ddb8ff5a8f019f11
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2021-01-16 22:08:45 +0100

    Add day 2016.18

--- src/main/java/de/beachboys/aoc2016/Day18.java
+++ src/main/java/de/beachboys/aoc2016/Day18.java
@@ -1,17 +1,50 @@
 package de.beachboys.aoc2016;
 
 import de.beachboys.Day;
+import de.beachboys.Util;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Day18 extends Day {
 
     public Object part1(List<String> input) {
-        return "part1result";
+        return runLogic(input, 40);
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        return runLogic(input, 400000);
+    }
+
+    private long runLogic(List<String> input, int defaultNumberOfLines) {
+        int numberOfLines = Util.getIntValueFromUser("Number of lines", defaultNumberOfLines, io);
+        String firstLine = input.get(0);
+        int width = firstLine.length();
+        Set<Integer> safeTilesNextLine = new HashSet<>();
+        for (int i = 0; i < width; i++) {
+            if (".".equals(firstLine.substring(i, i + 1))) {
+                safeTilesNextLine.add(i);
+            }
+        }
+        long numberOfSafeTiles = safeTilesNextLine.size();
+
+        for (int i = 0; i < numberOfLines - 1; i++) {
+            Set<Integer> safeTilesCurrentLine = safeTilesNextLine;
+            safeTilesNextLine = new HashSet<>();
+            safeTilesCurrentLine.add(-1);
+            safeTilesCurrentLine.add(width);
+            for (int j = 0; j < width; j++) {
+                boolean left = safeTilesCurrentLine.contains(j - 1);
+                boolean right = safeTilesCurrentLine.contains(j + 1);
+                if (left == right) {
+                    safeTilesNextLine.add(j);
+                }
+            }
+            numberOfSafeTiles += safeTilesNextLine.size();
+        }
+
+        return numberOfSafeTiles;
     }
 
 }
--- src/test/java/de/beachboys/aoc2016/Day18Test.java
+++ src/test/java/de/beachboys/aoc2016/Day18Test.java
@@ -3,6 +3,7 @@ package de.beachboys.aoc2016;
 import de.beachboys.Day;
 import de.beachboys.DayTest;
 import de.beachboys.IOHelper;
+import de.beachboys.IOHelperForTests;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -15,15 +16,15 @@ public class Day18Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("..^^."), 6, new IOHelperForTests(List.of("3"), null)),
+                Arguments.of(List.of(".^^.^.^^^^"), 38, new IOHelperForTests(List.of("10"), null))
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("..^^."), 6, new IOHelperForTests(List.of("3"), null)),
+                Arguments.of(List.of(".^^.^.^^^^"), 38, new IOHelperForTests(List.of("10"), null))
         );
     }
 
