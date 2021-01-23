b183d1adb76efdb9111810bb3e274c82a314b8a9
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2021-01-23 21:41:45 +0100

    Add day 2016.22

--- src/main/java/de/beachboys/aoc2016/Day22.java
+++ src/main/java/de/beachboys/aoc2016/Day22.java
@@ -1,17 +1,147 @@
 package de.beachboys.aoc2016;
 
 import de.beachboys.Day;
+import de.beachboys.Direction;
+import org.javatuples.Pair;
 
-import java.util.List;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Day22 extends Day {
 
-    public Object part1(List<String> input) {
-        return "part1result";
+    private static class State {
+
+        final Set<Pair<Integer, Integer>> emptyNodes;
+        final Pair<Integer, Integer> nodeWithGoalData;
+        final int stepsUntilThisState;
+        String stringRepresentation;
+
+        public State(Set<Pair<Integer, Integer>> emptyNodes, Pair<Integer, Integer> nodeWithGoalData, int stepsUntilThisState) {
+            this.emptyNodes = emptyNodes;
+            this.nodeWithGoalData = nodeWithGoalData;
+            this.stepsUntilThisState = stepsUntilThisState;
+        }
+
+        public String getStringRepresentation() {
+            if (stringRepresentation == null) {
+                stringRepresentation = nodeWithGoalData.toString() + "|" + emptyNodes.stream().sorted().collect(Collectors.toList()).toString();
+            }
+            return stringRepresentation;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            State state = (State) o;
+            return getStringRepresentation().equals(state.getStringRepresentation());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getStringRepresentation());
+        }
+
     }
 
-    public Object part2(List<String> input) {
-        return 2;
+    public Object part1(List<String> input) {
+        Map<Pair<Integer, Integer>, Pair<Integer, Integer>> nodes = parseNodes(input);
+
+        long viablePairCount = 0L;
+        for (Map.Entry<Pair<Integer, Integer>, Pair<Integer, Integer>> innerNode : nodes.entrySet()) {
+            if (innerNode.getValue().getValue0() > 0) {
+                for (Map.Entry<Pair<Integer, Integer>, Pair<Integer, Integer>> outerNode : nodes.entrySet()) {
+                    if (innerNode.getKey() != outerNode.getKey() && innerNode.getValue().getValue0() <= outerNode.getValue().getValue1()) {
+                        viablePairCount++;
+                    }
+                }
+            }
+        }
+        return viablePairCount;
     }
+
+    private final Deque<State> queue = new LinkedList<>();
+    private final Set<State> knownStates = new HashSet<>();
+    private final Set<Pair<Integer, Integer>> allUsefulNodes = new HashSet<>();
+
+    @Override
+    public Object part2(List<String> input) {
+        Map<Pair<Integer, Integer>, Pair<Integer, Integer>> nodes = parseNodes(input);
+        Pair<Integer, Integer> target = Pair.with(0, 0);
+        int targetSize = nodes.get(target).getValue0() + nodes.get(target).getValue1();
+        int smallestData = nodes.values().stream().map(Pair::getValue0).filter(data -> data != 0).mapToInt(Integer::intValue).min().orElseThrow();
+        Set<Pair<Integer, Integer>> emptyNodes = getInitialEmptyNodesAndFillAllUsefulNodes(nodes, targetSize, smallestData);
+
+        Pair<Integer, Integer> source = Pair.with(nodes.keySet().stream().map(Pair::getValue0).max(Comparator.naturalOrder()).orElseThrow(), 0);
+        queue.add(new State(emptyNodes, source, 0));
+        return getNumberOfStepsUntilDataAtTargetByHandlingQueue(target);
+    }
+
+    private Object getNumberOfStepsUntilDataAtTargetByHandlingQueue(Pair<Integer, Integer> target) {
+        while (!queue.isEmpty()) {
+            State state = queue.poll();
+            for (Pair<Integer, Integer> emptyNode : state.emptyNodes) {
+                for (Direction dir : Direction.values()) {
+                    Pair<Integer, Integer> neighbor = dir.move(emptyNode, 1);
+                    if (allUsefulNodes.contains(neighbor)) {
+                        int stepsUntilThisState = state.stepsUntilThisState + 1;
+                        Pair<Integer, Integer> nodeWithGoalData = state.nodeWithGoalData;
+                        if (nodeWithGoalData.equals(neighbor)) {
+                            nodeWithGoalData = emptyNode;
+                        }
+                        if (target.equals(nodeWithGoalData)) {
+                            return stepsUntilThisState;
+                        }
+                        Set<Pair<Integer, Integer>> newEmpty = new HashSet<>(state.emptyNodes);
+                        newEmpty.add(neighbor);
+                        newEmpty.remove(emptyNode);
+                        State newState = new State(newEmpty, nodeWithGoalData, stepsUntilThisState);
+                        if (!knownStates.contains(newState)) {
+                            queue.add(newState);
+                            knownStates.add(newState);
+                        }
+                    }
+                }
+            }
+        }
+        return "not possible";
+    }
+
+    private Set<Pair<Integer, Integer>> getInitialEmptyNodesAndFillAllUsefulNodes(Map<Pair<Integer, Integer>, Pair<Integer, Integer>> nodes, int targetSize, int smallestData) {
+        Set<Pair<Integer, Integer>> emptyNodes = new HashSet<>();
+        for (Pair<Integer, Integer> node : nodes.keySet()) {
+            Pair<Integer, Integer> content = nodes.get(node);
+            if (content.getValue0() != 0 && content.getValue1() > smallestData) {
+                throw new IllegalStateException("idea not working");
+            }
+            if (content.getValue0() == 0) {
+                emptyNodes.add(node);
+            }
+            if (content.getValue0() <= targetSize) {
+                allUsefulNodes.add(node);
+            }
+        }
+        allUsefulNodes.addAll(emptyNodes);
+        return emptyNodes;
+    }
+
+    private Map<Pair<Integer, Integer>, Pair<Integer, Integer>> parseNodes(List<String> input) {
+        Pattern p = Pattern.compile("/dev/grid/node-x([0-9]+)-y([0-9]+) +([0-9]+)T +([0-9]+)T +([0-9]+)T +([0-9]+)%");
+        Map<Pair<Integer, Integer>, Pair<Integer, Integer>> map = new HashMap<>();
+        for (String line : input) {
+            Matcher m = p.matcher(line);
+            if (m.matches()) {
+                int x = Integer.parseInt(m.group(1));
+                int y = Integer.parseInt(m.group(2));
+                int used = Integer.parseInt(m.group(4));
+                int avail = Integer.parseInt(m.group(5));
+                map.put(Pair.with(x, y), Pair.with(used, avail));
+            }
+        }
+        return map;
+    }
+
 
 }
--- src/test/java/de/beachboys/aoc2016/Day22Test.java
+++ src/test/java/de/beachboys/aoc2016/Day22Test.java
@@ -15,15 +15,31 @@ public class Day22Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("Filesystem            Size  Used  Avail  Use%",
+                        "/dev/grid/node-x0-y0   10T    8T     2T   80%",
+                        "/dev/grid/node-x0-y1   11T    6T     5T   54%",
+                        "/dev/grid/node-x0-y2   32T   28T     4T   87%",
+                        "/dev/grid/node-x1-y0    9T    7T     2T   77%",
+                        "/dev/grid/node-x1-y1    8T    0T     8T    0%",
+                        "/dev/grid/node-x1-y2   11T    7T     4T   63%",
+                        "/dev/grid/node-x2-y0   10T    6T     4T   60%",
+                        "/dev/grid/node-x2-y1    9T    8T     1T   88%",
+                        "/dev/grid/node-x2-y2    9T    6T     3T   66%"), 7, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("Filesystem            Size  Used  Avail  Use%",
+                        "/dev/grid/node-x0-y0   10T    8T     2T   80%",
+                        "/dev/grid/node-x0-y1   11T    6T     5T   54%",
+                        "/dev/grid/node-x0-y2   32T   28T     4T   87%",
+                        "/dev/grid/node-x1-y0    9T    7T     2T   77%",
+                        "/dev/grid/node-x1-y1    8T    0T     8T    0%",
+                        "/dev/grid/node-x1-y2   11T    7T     4T   63%",
+                        "/dev/grid/node-x2-y0   10T    6T     4T   60%",
+                        "/dev/grid/node-x2-y1    9T    8T     1T   88%",
+                        "/dev/grid/node-x2-y2    9T    6T     3T   66%"), 7, null)
         );
     }
 
