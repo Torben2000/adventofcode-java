f1361abd95dea41eae3424528268e2b89475486a
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2021-01-30 00:00:53 +0100

    Add day 2017.5

--- src/main/java/de/beachboys/aoc2017/Day05.java
+++ src/main/java/de/beachboys/aoc2017/Day05.java
@@ -3,15 +3,30 @@ package de.beachboys.aoc2017;
 import de.beachboys.Day;
 
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Day05 extends Day {
 
     public Object part1(List<String> input) {
-        return "part1result";
+        return runLogic(input, offset -> offset + 1);
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        return runLogic(input, offset -> offset < 3 ? offset + 1 : offset - 1);
+    }
+
+    private int runLogic(List<String> input, Function<Integer, Integer> adjustOffset) {
+        List<Integer> list = input.stream().map(Integer::valueOf).collect(Collectors.toList());
+        int stepCount = 0;
+        int pointer = 0;
+        while (pointer < list.size()) {
+            stepCount++;
+            Integer offset = list.get(pointer);
+            list.set(pointer, adjustOffset.apply(offset));
+            pointer += offset;
+        }
+        return stepCount;
     }
 
 }
--- src/test/java/de/beachboys/aoc2017/Day05Test.java
+++ src/test/java/de/beachboys/aoc2017/Day05Test.java
@@ -15,15 +15,21 @@ public class Day05Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("0",
+                        "3",
+                        "0",
+                        "1",
+                        "-3"), 5, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("0",
+                        "3",
+                        "0",
+                        "1",
+                        "-3"), 10, null)
         );
     }
 
