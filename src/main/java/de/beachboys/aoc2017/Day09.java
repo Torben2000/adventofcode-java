7e7767991805cd5cf87e0eb1339a5c2c6083c04b
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2021-01-31 00:03:09 +0100

    Add day 2017.9

--- src/main/java/de/beachboys/aoc2017/Day09.java
+++ src/main/java/de/beachboys/aoc2017/Day09.java
@@ -3,15 +3,50 @@ package de.beachboys.aoc2017;
 import de.beachboys.Day;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day09 extends Day {
 
+    public static final String GARBAGE_REGEX = "<([^>]*)>";
+
     public Object part1(List<String> input) {
-        return "part1result";
+        String stream = getStreamWithoutCanceledCharacters(input);
+        stream = stream.replaceAll(GARBAGE_REGEX, "");
+        int sum = 0;
+        int openGroups = 0;
+        for (char character : stream.toCharArray()) {
+            switch (character) {
+                case '{':
+                    openGroups++;
+                    break;
+                case '}':
+                    sum += openGroups;
+                    openGroups--;
+                default:
+                    //do nothing
+                    break;
+            }
+        }
+        if (openGroups != 0) {
+            throw new IllegalStateException("malformed stream");
+        }
+        return sum;
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        String stream = getStreamWithoutCanceledCharacters(input);
+        Pattern p = Pattern.compile(GARBAGE_REGEX);
+        Matcher m = p.matcher(stream);
+        int sum = 0;
+        while (m.find()) {
+            sum += m.group(1).length();
+        }
+        return sum;
+    }
+
+    private String getStreamWithoutCanceledCharacters(List<String> input) {
+        return input.get(0).replaceAll("!.", "");
     }
 
 }
--- src/test/java/de/beachboys/aoc2017/Day09Test.java
+++ src/test/java/de/beachboys/aoc2017/Day09Test.java
@@ -15,15 +15,26 @@ public class Day09Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("{}"), 1, null),
+                Arguments.of(List.of("{{{}}}"), 6, null),
+                Arguments.of(List.of("{{},{}}"), 5, null),
+                Arguments.of(List.of("{{{},{},{{}}}}"), 16, null),
+                Arguments.of(List.of("{<a>,<a>,<a>,<a>}"), 1, null),
+                Arguments.of(List.of("{{<ab>},{<ab>},{<ab>},{<ab>}}"), 9, null),
+                Arguments.of(List.of("{{<!!>},{<!!>},{<!!>},{<!!>}}"), 9, null),
+                Arguments.of(List.of("{{<a!>},{<a!>},{<a!>},{<ab>}}"), 3, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("<>"), 0, null),
+                Arguments.of(List.of("<random characters>"), 17, null),
+                Arguments.of(List.of("<<<<>"), 3, null),
+                Arguments.of(List.of("<{!>}>"), 2, null),
+                Arguments.of(List.of("<!!>"), 0, null),
+                Arguments.of(List.of("<!!!>>"), 0, null),
+                Arguments.of(List.of("<{o\"i!a,<{i<a>"), 10, null)
         );
     }
 
