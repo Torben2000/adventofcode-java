db3ace037d1552ba379ffea81be37076669e70fc
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2021-01-30 21:57:02 +0100

    Add day 2017.7

--- src/main/java/de/beachboys/aoc2017/Day07.java
+++ src/main/java/de/beachboys/aoc2017/Day07.java
@@ -2,16 +2,94 @@ package de.beachboys.aoc2017;
 
 import de.beachboys.Day;
 
-import java.util.List;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Day07 extends Day {
 
+    private final Map<String, Set<String>> programToProgramsOnTop = new HashMap<>();
+
+    private final Map<String, Integer> weights = new HashMap<>();
+
+    private final List<Integer> correctedWeights = new ArrayList<>();
+
     public Object part1(List<String> input) {
-        return "part1result";
+        return parseInputAndReturnProgramAtBottom(input);
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        String programAtBottom = parseInputAndReturnProgramAtBottom(input);
+        getWeightIncludingProgramsOnTop(programAtBottom);
+        if (correctedWeights.size() != 1) {
+            throw new IllegalStateException("this should not happen");
+        }
+        return correctedWeights.get(0);
+    }
+
+    private String parseInputAndReturnProgramAtBottom(List<String> input) {
+        parseInput(input);
+        return getProgramAtBottom();
+    }
+
+    private void parseInput(List<String> input) {
+        Pattern p = Pattern.compile("([a-z]+) \\(([0-9]+)\\)( -> ([a-z, ]+)+)?");
+        for (String line : input) {
+            Matcher m = p.matcher(line);
+            if (m.matches()) {
+                String program = m.group(1);
+                Integer weight = Integer.valueOf(m.group(2));
+                weights.put(program, weight);
+                if (m.group(4) != null) {
+                    String[] rightSideParts = m.group(4).split(", ");
+                    Set<String> programsOnTop = new HashSet<>(Arrays.asList(rightSideParts));
+                    programToProgramsOnTop.put(program, programsOnTop);
+                }
+            }
+        }
+    }
+
+    private String getProgramAtBottom() {
+        Set<String> programAtTheBottom = new HashSet<>(programToProgramsOnTop.keySet());
+        programToProgramsOnTop.values().forEach(programAtTheBottom::removeAll);
+        if (programAtTheBottom.size() != 1) {
+            throw new IllegalStateException("this should not happen");
+        }
+        return programAtTheBottom.stream().findFirst().orElseThrow();
+    }
+
+    private int getWeightIncludingProgramsOnTop(String program) {
+        int totalWeight = weights.get(program);
+        if (programToProgramsOnTop.containsKey(program)) {
+            Map<String, Integer> weightsOfProgramsOnTop = new HashMap<>();
+            for (String programOnTop : programToProgramsOnTop.get(program)) {
+                int weightOfProgramOnTop = getWeightIncludingProgramsOnTop(programOnTop);
+                weightsOfProgramsOnTop.put(programOnTop, weightOfProgramOnTop);
+            }
+            totalWeight += weightsOfProgramsOnTop.size() * getCorrectWeightForProgramOnTop(weightsOfProgramsOnTop);
+        }
+        return totalWeight;
+    }
+
+    private int getCorrectWeightForProgramOnTop(Map<String, Integer> weightsOfProgramsOnTop) {
+        Set<Integer> distinctValues = new HashSet<>(weightsOfProgramsOnTop.values());
+        int correctWeightForProgramOnTop = distinctValues.stream().findFirst().orElseThrow();
+        if (distinctValues.size() == 2) {
+            int differenceBetweenWeightWithProgramsOnTopAndIndividualWeight = 0;
+            for (Integer weight : distinctValues) {
+                List<String> matchingPrograms = weightsOfProgramsOnTop.entrySet().stream().filter(entry -> entry.getValue().equals(weight)).map(Map.Entry::getKey).collect(Collectors.toList());
+                if (matchingPrograms.size() == 1) {
+                    differenceBetweenWeightWithProgramsOnTopAndIndividualWeight = weight - weights.get(matchingPrograms.get(0));
+                } else {
+                    correctWeightForProgramOnTop = weight;
+                }
+            }
+            correctedWeights.add(correctWeightForProgramOnTop - differenceBetweenWeightWithProgramsOnTopAndIndividualWeight);
+        } else if (distinctValues.size() > 2) {
+            throw new IllegalStateException("this should not happen");
+        }
+        return correctWeightForProgramOnTop;
     }
 
 }
--- src/test/java/de/beachboys/aoc2017/Day07Test.java
+++ src/test/java/de/beachboys/aoc2017/Day07Test.java
@@ -15,15 +15,37 @@ public class Day07Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("pbga (66)",
+                        "xhth (57)",
+                        "ebii (61)",
+                        "havc (66)",
+                        "ktlj (57)",
+                        "fwft (72) -> ktlj, cntj, xhth",
+                        "qoyq (66)",
+                        "padx (45) -> pbga, havc, qoyq",
+                        "tknk (41) -> ugml, padx, fwft",
+                        "jptl (61)",
+                        "ugml (68) -> gyxo, ebii, jptl",
+                        "gyxo (61)",
+                        "cntj (57)"), "tknk", null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("pbga (66)",
+                        "xhth (57)",
+                        "ebii (61)",
+                        "havc (66)",
+                        "ktlj (57)",
+                        "fwft (72) -> ktlj, cntj, xhth",
+                        "qoyq (66)",
+                        "padx (45) -> pbga, havc, qoyq",
+                        "tknk (41) -> ugml, padx, fwft",
+                        "jptl (61)",
+                        "ugml (68) -> gyxo, ebii, jptl",
+                        "gyxo (61)",
+                        "cntj (57)"), 60, null)
         );
     }
 
