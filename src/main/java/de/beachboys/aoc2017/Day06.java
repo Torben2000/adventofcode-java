7c946f8eb698015914541589626a63f4356e2a5d
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2021-01-30 01:48:36 +0100

    Add day 2017.6

--- src/main/java/de/beachboys/aoc2017/Day06.java
+++ src/main/java/de/beachboys/aoc2017/Day06.java
@@ -1,17 +1,62 @@
 package de.beachboys.aoc2017;
 
 import de.beachboys.Day;
+import de.beachboys.Util;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Day06 extends Day {
 
+    private int steps = 0;
+
+    private List<Integer> memory;
+
+    private final Map<String, Integer> seenMemoryStates = new HashMap<>();
+
     public Object part1(List<String> input) {
-        return "part1result";
+        runLogic(input);
+        return steps;
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        runLogic(input);
+        return steps - seenMemoryStates.get(getMemoryRepresentation());
+    }
+
+    private void runLogic(List<String> input) {
+        memory = Util.parseToIntList(input.get(0), "\t");
+        while (!seenMemoryStates.containsKey(getMemoryRepresentation())) {
+            seenMemoryStates.put(getMemoryRepresentation(), steps);
+            steps++;
+
+            int maxBlocks = memory.stream().mapToInt(Integer::intValue).max().orElseThrow();
+            int firstIndexWithMaxBlocks = memory.indexOf(maxBlocks);
+
+            redistributeBlocks(maxBlocks, firstIndexWithMaxBlocks);
+        }
+    }
+
+    private void redistributeBlocks(int blocksToRedistribute, int blockSourceIndex) {
+        memory.set(blockSourceIndex, 0);
+        int blocksToAddPerMemoryBank = blocksToRedistribute / memory.size();
+        int indexOffsetToAddOneMoreBlock = blocksToRedistribute % memory.size();
+        for (int i = 0; i < memory.size(); i++) {
+            int blocksToAddForThisMemoryBank = blocksToAddPerMemoryBank;
+            if (isIndexToAddMore(i, blockSourceIndex, indexOffsetToAddOneMoreBlock)) {
+                blocksToAddForThisMemoryBank++;
+            }
+            memory.set(i, memory.get(i) + blocksToAddForThisMemoryBank);
+        }
+    }
+
+    private boolean isIndexToAddMore(int index, int blockSourceIndex, int indexOffsetToAddOneMoreBlock) {
+        return index > blockSourceIndex && index <= blockSourceIndex + indexOffsetToAddOneMoreBlock
+                || index < blockSourceIndex && index <= (blockSourceIndex + indexOffsetToAddOneMoreBlock - memory.size());
+    }
+
+    private String getMemoryRepresentation() {
+        return memory.stream().map(String::valueOf).collect(Collectors.joining(","));
     }
 
 }
--- src/test/java/de/beachboys/aoc2017/Day06Test.java
+++ src/test/java/de/beachboys/aoc2017/Day06Test.java
@@ -15,15 +15,13 @@ public class Day06Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("0\t2\t7\t0"), 5, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("0\t2\t7\t0"), 4, null)
         );
     }
 
