37851851175f6596db7084c98f155c22c827f3c6
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2021-02-01 23:31:01 +0100

    Add day 2017.13

--- src/main/java/de/beachboys/aoc2017/Day13.java
+++ src/main/java/de/beachboys/aoc2017/Day13.java
@@ -2,16 +2,74 @@ package de.beachboys.aoc2017;
 
 import de.beachboys.Day;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day13 extends Day {
 
+    private final Map<Integer, Integer> layerToRange = new HashMap<>();
+
+
     public Object part1(List<String> input) {
-        return "part1result";
+        parseInputToMap(input);
+        return getSeverity();
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        parseInputToMap(input);
+        int maxLayer = getMaxLayer();
+        int maxValue = 100000000;
+        for (int delay = 0; delay < maxValue; delay++) {
+            if (!checkForDetection(maxLayer, delay)) {
+                return delay;
+            }
+        }
+        return "not possible";
+   }
+
+    private int getSeverity() {
+        int maxLayer = getMaxLayer();
+        int sum = 0;
+        for (int i = 0; i <= maxLayer; i++) {
+            if (isDetected(i, 0)) {
+                sum += i * layerToRange.get(i);
+            }
+        }
+        return sum;
+    }
+
+    private boolean checkForDetection(int maxLayer, int delay) {
+        boolean detected = false;
+        for (int i = 0; i <= maxLayer; i++) {
+            if (isDetected(i, delay)) {
+                detected = true;
+                break;
+            }
+        }
+        return detected;
+    }
+
+    private int getMaxLayer() {
+        return layerToRange.keySet().stream().mapToInt(Integer::intValue).max().orElseThrow();
+    }
+
+    private boolean isDetected(int layer, int delay) {
+        return layerToRange.containsKey(layer) && (layer + delay) % (2 * (layerToRange.get(layer) - 1)) == 0;
+    }
+
+    private void parseInputToMap(List<String> input) {
+        layerToRange.clear();
+        Pattern p = Pattern.compile("([0-9]+): ([0-9]+)");
+        for (String line : input) {
+            Matcher m = p.matcher(line);
+            if (m.matches()) {
+                layerToRange.put(Integer.valueOf(m.group(1)), Integer.valueOf(m.group(2)));
+            }
+        }
+
     }
 
 }
--- src/test/java/de/beachboys/aoc2017/Day13Test.java
+++ src/test/java/de/beachboys/aoc2017/Day13Test.java
@@ -15,15 +15,19 @@ public class Day13Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("0: 3",
+                        "1: 2",
+                        "4: 4",
+                        "6: 4"), 24, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("0: 3",
+                        "1: 2",
+                        "4: 4",
+                        "6: 4"), 10, null)
         );
     }
 
