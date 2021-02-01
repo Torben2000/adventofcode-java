00e569e644e360035ea971ffa6f699e0829aa807
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2021-02-01 22:46:42 +0100

    Add day 2017.12

--- src/main/java/de/beachboys/aoc2017/Day12.java
+++ src/main/java/de/beachboys/aoc2017/Day12.java
@@ -1,17 +1,47 @@
 package de.beachboys.aoc2017;
 
 import de.beachboys.Day;
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.alg.connectivity.ConnectivityInspector;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.DefaultUndirectedGraph;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day12 extends Day {
 
     public Object part1(List<String> input) {
-        return "part1result";
+        ConnectivityInspector<Integer, DefaultEdge> inspector = buildGraphAndReturnInspector(input);
+        return inspector.connectedSetOf(0).size();
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        ConnectivityInspector<Integer, DefaultEdge> inspector = buildGraphAndReturnInspector(input);
+        return inspector.connectedSets().size();
+    }
+
+    private ConnectivityInspector<Integer, DefaultEdge> buildGraphAndReturnInspector(List<String> input) {
+        Pattern linePattern = Pattern.compile("([0-9]+) <-> ([0-9, ]+)");
+        Pattern numberPattern = Pattern.compile("[0-9]+");
+        Graph<Integer, DefaultEdge> graph = new DefaultUndirectedGraph<>(DefaultEdge.class);
+        for (String line : input) {
+            Matcher lineMatcher = linePattern.matcher(line);
+            if (lineMatcher.matches()) {
+                Integer leftSideNode = Integer.parseInt(lineMatcher.group(1));
+                List<Integer> rightSideNodes = new ArrayList<>();
+                String rightSide = lineMatcher.group(2);
+                Matcher rightSideNodeMatcher = numberPattern.matcher(rightSide);
+                while (rightSideNodeMatcher.find()) {
+                    rightSideNodes.add(Integer.valueOf(rightSideNodeMatcher.group()));
+                }
+                rightSideNodes.forEach(node -> Graphs.addEdgeWithVertices(graph, leftSideNode, node));
+            }
+        }
+        return new ConnectivityInspector<>(graph);
     }
 
 }
--- src/test/java/de/beachboys/aoc2017/Day12Test.java
+++ src/test/java/de/beachboys/aoc2017/Day12Test.java
@@ -15,15 +15,25 @@ public class Day12Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("0 <-> 2",
+                        "1 <-> 1",
+                        "2 <-> 0, 3, 4",
+                        "3 <-> 2, 4",
+                        "4 <-> 2, 3, 6",
+                        "5 <-> 6",
+                        "6 <-> 4, 5"), 6, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("0 <-> 2",
+                        "1 <-> 1",
+                        "2 <-> 0, 3, 4",
+                        "3 <-> 2, 4",
+                        "4 <-> 2, 3, 6",
+                        "5 <-> 6",
+                        "6 <-> 4, 5"), 2, null)
         );
     }
 
