9f3a909fd3f727b32e7b7da9cec66b0d9b510ea9
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2021-02-06 01:35:32 +0100

    Add day 2017.17

--- src/main/java/de/beachboys/aoc2017/Day17.java
+++ src/main/java/de/beachboys/aoc2017/Day17.java
@@ -6,12 +6,43 @@ import java.util.List;
 
 public class Day17 extends Day {
 
+    private static class Element {
+        Element next = this;
+        int value;
+
+        Element(int value) {
+            this.value = value;
+        }
+    }
+
+
     public Object part1(List<String> input) {
-        return "part1result";
+        int stepSize = Integer.parseInt(input.get(0));
+        Element currentElement = new Element(0);
+        for (int i = 1; i <= 2017; i++) {
+            int neededSteps = stepSize % i;
+            for (int j = 0; j < neededSteps; j++) {
+                currentElement = currentElement.next;
+            }
+            Element newElement = new Element(i);
+            newElement.next = currentElement.next;
+            currentElement.next = newElement;
+            currentElement = newElement;
+        }
+        return currentElement.next.value;
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        int stepSize = Integer.parseInt(input.get(0));
+        int currentReturnValue = 0;
+        int currentIndex = 0;
+        for (int i = 1; i <= 50000000; i++) {
+            currentIndex = ((currentIndex + stepSize) % i) + 1;
+            if (currentIndex == 1) {
+                currentReturnValue = i;
+            }
+        }
+        return currentReturnValue;
     }
 
 }
--- src/test/java/de/beachboys/aoc2017/Day17Test.java
+++ src/test/java/de/beachboys/aoc2017/Day17Test.java
@@ -15,15 +15,13 @@ public class Day17Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("3"), 638, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("3"), 1222153, null)
         );
     }
 
