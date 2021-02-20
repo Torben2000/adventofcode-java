5a50beab343f25891de3707ea9569948a1b9c4ec
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2021-02-21 00:35:53 +0100

    Add day 2017.22

--- src/main/java/de/beachboys/aoc2017/Day22.java
+++ src/main/java/de/beachboys/aoc2017/Day22.java
@@ -1,17 +1,71 @@
 package de.beachboys.aoc2017;
 
 import de.beachboys.Day;
+import de.beachboys.Direction;
+import de.beachboys.Util;
+import org.javatuples.Pair;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 
 public class Day22 extends Day {
 
+    private Direction currentDirection = Direction.NORTH;
+
     public Object part1(List<String> input) {
-        return "part1result";
+        return runLogic(input, 10000, this::updateDirectionAndReturnNewMapValuePart1);
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        return runLogic(input, 10000000, this::updateDirectionAndReturnNewMapValuePart2);
+    }
+
+    private int runLogic(List<String> input, int bursts, Function<String, String> updateDirectionAndReturnNewMapValue) {
+        Map<Pair<Integer, Integer>, String> map = Util.buildImageMap(input);
+        Pair<Integer, Integer> currentPosition = Pair.with(input.get(0).length() / 2, input.size() / 2);
+        int counter = 0;
+        for (int i = 0; i < bursts; i++) {
+            String currentMapValue = map.getOrDefault(currentPosition, ".");
+            String newMapValue = updateDirectionAndReturnNewMapValue.apply(currentMapValue);
+            map.put(currentPosition, newMapValue);
+            if ("#".equals(newMapValue)) {
+                counter++;
+            }
+            currentPosition = currentDirection.move(currentPosition, 1);
+        }
+        return counter;
+    }
+
+    private String updateDirectionAndReturnNewMapValuePart1(String currentMapValue) {
+        switch (currentMapValue) {
+            case "#":
+                currentDirection = currentDirection.turnRight();
+                return ".";
+            case ".":
+                currentDirection = currentDirection.turnLeft();
+                return "#";
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+
+    private String updateDirectionAndReturnNewMapValuePart2(String currentMapValue) {
+        switch (currentMapValue) {
+            case "#":
+                currentDirection = currentDirection.turnRight();
+                return "F";
+            case ".":
+                currentDirection = currentDirection.turnLeft();
+                return "W";
+            case "W":
+                return "#";
+            case "F":
+                currentDirection = currentDirection.getOpposite();
+                return ".";
+            default:
+                throw new IllegalArgumentException();
+        }
     }
 
 }
--- src/test/java/de/beachboys/aoc2017/Day22Test.java
+++ src/test/java/de/beachboys/aoc2017/Day22Test.java
@@ -15,15 +15,17 @@ public class Day22Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("..#",
+                        "#..",
+                        "..."), 5587, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("..#",
+                        "#..",
+                        "..."), 2511944, null)
         );
     }
 
