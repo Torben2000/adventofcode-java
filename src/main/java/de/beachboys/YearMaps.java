9d989d2bb7af19e2039b8e577e036f03199cace1
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2021-02-21 03:42:27 +0100

    Add year 2018

--- src/main/java/de/beachboys/YearMaps.java
+++ src/main/java/de/beachboys/YearMaps.java
@@ -18,6 +18,7 @@ public class YearMaps {
     private static void buildStaticMapOfDays() {
         YEARS.put(2020, getDays2020());
         YEARS.put(2019, getDays2019());
+        YEARS.put(2018, getDays2018());
         YEARS.put(2017, getDays2017());
         YEARS.put(2016, getDays2016());
         YEARS.put(2015, getDays2015());
@@ -80,6 +81,36 @@ public class YearMaps {
         days.put(23, new de.beachboys.aoc2019.Day23());
         days.put(24, new de.beachboys.aoc2019.Day24());
         days.put(25, new de.beachboys.aoc2019.Day25());
+        return days;
+    }
+
+    private static Map<Integer, Day> getDays2018() {
+        Map<Integer, Day> days = new HashMap<>();
+        days.put(1, new de.beachboys.aoc2018.Day01());
+        days.put(2, new de.beachboys.aoc2018.Day02());
+        days.put(3, new de.beachboys.aoc2018.Day03());
+        days.put(4, new de.beachboys.aoc2018.Day04());
+        days.put(5, new de.beachboys.aoc2018.Day05());
+        days.put(6, new de.beachboys.aoc2018.Day06());
+        days.put(7, new de.beachboys.aoc2018.Day07());
+        days.put(8, new de.beachboys.aoc2018.Day08());
+        days.put(9, new de.beachboys.aoc2018.Day09());
+        days.put(10, new de.beachboys.aoc2018.Day10());
+        days.put(11, new de.beachboys.aoc2018.Day11());
+        days.put(12, new de.beachboys.aoc2018.Day12());
+        days.put(13, new de.beachboys.aoc2018.Day13());
+        days.put(14, new de.beachboys.aoc2018.Day14());
+        days.put(15, new de.beachboys.aoc2018.Day15());
+        days.put(16, new de.beachboys.aoc2018.Day16());
+        days.put(17, new de.beachboys.aoc2018.Day17());
+        days.put(18, new de.beachboys.aoc2018.Day18());
+        days.put(19, new de.beachboys.aoc2018.Day19());
+        days.put(20, new de.beachboys.aoc2018.Day20());
+        days.put(21, new de.beachboys.aoc2018.Day21());
+        days.put(22, new de.beachboys.aoc2018.Day22());
+        days.put(23, new de.beachboys.aoc2018.Day23());
+        days.put(24, new de.beachboys.aoc2018.Day24());
+        days.put(25, new de.beachboys.aoc2018.Day25());
         return days;
     }
 
