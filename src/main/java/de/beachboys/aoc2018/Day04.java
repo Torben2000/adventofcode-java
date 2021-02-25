58f01a2ecd1ee6508db32f037fefa47e51c095cf
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2021-02-25 23:13:40 +0100

    Add day 2018.4

--- src/main/java/de/beachboys/aoc2018/Day04.java
+++ src/main/java/de/beachboys/aoc2018/Day04.java
@@ -2,16 +2,82 @@ package de.beachboys.aoc2018;
 
 import de.beachboys.Day;
 
-import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.function.ToIntFunction;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Day04 extends Day {
 
     public Object part1(List<String> input) {
-        return "part1result";
+        return runLogic(input, this::getSum);
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        return runLogic(input, this::getMax);
+    }
+
+    private int runLogic(List<String> input, ToIntFunction<Collection<Integer>> getRelevantAsleepMinutesFromAllAsleepMinutes) {
+        Map<Integer, Map<Integer, Integer>> asleepMinutes = parseInput(input);
+        Map<Integer, Integer> guardToAsleepMinutes = asleepMinutes.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, entry -> getRelevantAsleepMinutesFromAllAsleepMinutes.applyAsInt(entry.getValue().values())));
+        int guardWithMostAsleepMinutes = guardToAsleepMinutes.entrySet().stream().max(Comparator.comparingInt(Map.Entry::getValue)).orElseThrow().getKey();
+        int minuteWhenGuardIsMostAsleep = asleepMinutes.get(guardWithMostAsleepMinutes).entrySet().stream().max(Comparator.comparingInt(Map.Entry::getValue)).orElseThrow().getKey();
+        return guardWithMostAsleepMinutes * minuteWhenGuardIsMostAsleep;
+    }
+
+    private int getSum(Collection<Integer> values) {
+        return values.stream().mapToInt(Integer::intValue).sum();
+    }
+
+    private int getMax(Collection<Integer> values) {
+        return values.stream().mapToInt(Integer::intValue).max().orElseThrow();
+    }
+
+    private Map<Integer, Map<Integer, Integer>> parseInput(List<String> input) {
+        List<String> sortedInput = new ArrayList<>(input);
+        sortedInput.sort(String::compareTo);
+        Pattern guardPattern = Pattern.compile("\\[(.*)] Guard #([0-9]+) begins shift");
+        Pattern asleepPattern = Pattern.compile("\\[(.*)] falls asleep");
+        Pattern wakePattern = Pattern.compile("\\[(.*)] wakes up");
+        Map<Integer, Map<Integer, Integer>> asleepMinutes = new HashMap<>();
+        int guard = 0;
+        int asleepMinute = 0;
+        for (String line : sortedInput) {
+            Matcher guardMatcher = guardPattern.matcher(line);
+            if (guardMatcher.matches()) {
+                guard = Integer.parseInt(guardMatcher.group(2));
+            } else {
+                Matcher asleepMatcher = asleepPattern.matcher(line);
+                if (asleepMatcher.matches()) {
+                    asleepMinute = getMinutes(asleepMatcher.group(1));
+                } else {
+                    Matcher wakeMatcher = wakePattern.matcher(line);
+                    if (wakeMatcher.matches()) {
+                        int wakeMinute = getMinutes(wakeMatcher.group(1));
+                        asleepMinutes.putIfAbsent(guard, new HashMap<>());
+                        Map<Integer, Integer> guardAsleepMinutes = asleepMinutes.get(guard);
+                        for (int minute = asleepMinute; minute < wakeMinute; minute++) {
+                            guardAsleepMinutes.merge(minute, 1, Integer::sum);
+                        }
+                    }
+                }
+            }
+        }
+        return asleepMinutes;
+    }
+
+    private final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+    private int getMinutes(String dateString) {
+        try {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(DATE_FORMAT.parse(dateString));
+            return calendar.get(Calendar.MINUTE);
+        } catch (ParseException e) {
+            throw new IllegalArgumentException(e);
+        }
     }
 
 }
--- src/test/java/de/beachboys/aoc2018/Day04Test.java
+++ src/test/java/de/beachboys/aoc2018/Day04Test.java
@@ -15,15 +15,45 @@ public class Day04Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("[1518-11-01 00:00] Guard #10 begins shift",
+                        "[1518-11-01 00:05] falls asleep",
+                        "[1518-11-01 00:25] wakes up",
+                        "[1518-11-01 00:30] falls asleep",
+                        "[1518-11-01 00:55] wakes up",
+                        "[1518-11-01 23:58] Guard #99 begins shift",
+                        "[1518-11-02 00:40] falls asleep",
+                        "[1518-11-02 00:50] wakes up",
+                        "[1518-11-03 00:05] Guard #10 begins shift",
+                        "[1518-11-03 00:24] falls asleep",
+                        "[1518-11-03 00:29] wakes up",
+                        "[1518-11-04 00:02] Guard #99 begins shift",
+                        "[1518-11-04 00:36] falls asleep",
+                        "[1518-11-04 00:46] wakes up",
+                        "[1518-11-05 00:03] Guard #99 begins shift",
+                        "[1518-11-05 00:45] falls asleep",
+                        "[1518-11-05 00:55] wakes up"), 240, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("[1518-11-01 00:00] Guard #10 begins shift",
+                        "[1518-11-01 00:05] falls asleep",
+                        "[1518-11-01 00:25] wakes up",
+                        "[1518-11-01 00:30] falls asleep",
+                        "[1518-11-01 00:55] wakes up",
+                        "[1518-11-01 23:58] Guard #99 begins shift",
+                        "[1518-11-02 00:40] falls asleep",
+                        "[1518-11-02 00:50] wakes up",
+                        "[1518-11-03 00:05] Guard #10 begins shift",
+                        "[1518-11-03 00:24] falls asleep",
+                        "[1518-11-03 00:29] wakes up",
+                        "[1518-11-04 00:02] Guard #99 begins shift",
+                        "[1518-11-04 00:36] falls asleep",
+                        "[1518-11-04 00:46] wakes up",
+                        "[1518-11-05 00:03] Guard #99 begins shift",
+                        "[1518-11-05 00:45] falls asleep",
+                        "[1518-11-05 00:55] wakes up"), 4455, null)
         );
     }
 
