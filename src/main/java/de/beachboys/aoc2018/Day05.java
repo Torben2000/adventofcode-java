fe92eb5fa7b16a984aedf77eb4ccb0fc97630394
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2021-02-26 22:02:46 +0100

    Add day 2018.5

--- src/main/java/de/beachboys/aoc2018/Day05.java
+++ src/main/java/de/beachboys/aoc2018/Day05.java
@@ -2,16 +2,61 @@ package de.beachboys.aoc2018;
 
 import de.beachboys.Day;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Day05 extends Day {
 
     public Object part1(List<String> input) {
-        return "part1result";
+        return runAllReactions(input.get(0)).length();
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        String polymer = runAllReactions(input.get(0));
+
+        Set<String> units = getLowerCaseUnitsFromPolymer(polymer);
+
+        int minLength = Integer.MAX_VALUE;
+        for (String unit : units) {
+            String polymerWithoutUnit = polymer.replace(unit, "").replace(unit.toUpperCase(), "");
+            minLength = Math.min(minLength, runAllReactions(polymerWithoutUnit).length());
+        }
+        return minLength;
+    }
+
+    private Set<String> getLowerCaseUnitsFromPolymer(String polymer) {
+        Set<String> units = new HashSet<>();
+        for (char character : polymer.toCharArray()) {
+            units.add((character + "").toLowerCase());
+        }
+        return units;
+    }
+
+    private String runAllReactions(String polymer) {
+        String currentPolymer = polymer;
+        String nextPolymer = runReactionCycle(currentPolymer);
+        while (!currentPolymer.equals(nextPolymer)) {
+            currentPolymer = nextPolymer;
+            nextPolymer = runReactionCycle(currentPolymer);
+        }
+        return currentPolymer;
+    }
+
+    private String runReactionCycle(String polymer) {
+        StringBuilder sb = new StringBuilder();
+        String previousChar = "";
+        for (char character : polymer.toCharArray()) {
+            String currentChar = "" + character;
+            if (previousChar.equals(currentChar) || !previousChar.equalsIgnoreCase(currentChar)) {
+                sb.append(previousChar);
+                previousChar = currentChar;
+            } else {
+                previousChar = "";
+            }
+        }
+        sb.append(previousChar);
+        return sb.toString();
     }
 
 }
--- src/test/java/de/beachboys/aoc2018/Day05Test.java
+++ src/test/java/de/beachboys/aoc2018/Day05Test.java
@@ -15,15 +15,13 @@ public class Day05Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("dabAcCaCBAcCcaDA"), 10, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("dabAcCaCBAcCcaDA"), 4, null)
         );
     }
 
