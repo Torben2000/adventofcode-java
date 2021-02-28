bf6c202e449cb2eabc4b4cbfa746876475645843
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2021-02-28 17:27:30 +0100

    Add day 2018.7

--- src/main/java/de/beachboys/aoc2018/Day07.java
+++ src/main/java/de/beachboys/aoc2018/Day07.java
@@ -1,17 +1,100 @@
 package de.beachboys.aoc2018;
 
 import de.beachboys.Day;
+import de.beachboys.Util;
+import org.javatuples.Pair;
 
-import java.util.List;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day07 extends Day {
 
+    private final Set<Pair<String, String>> rules = new HashSet<>();
+
     public Object part1(List<String> input) {
-        return "part1result";
+        parseRules(input);
+        Set<String> remainingSteps = buildInitialRemainingSteps();
+
+        StringBuilder order = new StringBuilder();
+        while (!remainingSteps.isEmpty()) {
+            TreeSet<String> possibilities = getPossibilitiesPart1(remainingSteps);
+            String nextStep = possibilities.first();
+            order.append(nextStep);
+            remainingSteps.remove(nextStep);
+        }
+
+        return order.toString();
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        int workers = Util.getIntValueFromUser("Number of workers", 5, io);
+        int durationOffset = Util.getIntValueFromUser("Step duration offset", 60, io);
+
+        parseRules(input);
+        Set<String> remainingSteps = buildInitialRemainingSteps();
+        TreeMap<Integer, String> runningSteps = new TreeMap<>();
+        int timestamp = 0;
+
+        while (!remainingSteps.isEmpty()) {
+            TreeSet<String> possibilities = getPossibilitiesPart2(remainingSteps, runningSteps);
+            if (runningSteps.size() < workers && possibilities.size() > 0) {
+                String nextStep = possibilities.first();
+                runningSteps.put(buildTargetTimestamp(timestamp, durationOffset, nextStep), nextStep);
+            } else {
+                Map.Entry<Integer, String> endingStep = runningSteps.pollFirstEntry();
+                remainingSteps.remove(endingStep.getValue());
+                timestamp = endingStep.getKey();
+            }
+        }
+
+        return timestamp;
+    }
+
+    private TreeSet<String> getPossibilitiesPart1(Set<String> remainingSteps) {
+        TreeSet<String> possibilities = new TreeSet<>(remainingSteps);
+        for (Pair<String, String> rule : rules) {
+            if (remainingSteps.contains(rule.getValue0())) {
+                possibilities.remove(rule.getValue1());
+            }
+        }
+        return possibilities;
+    }
+
+    private TreeSet<String> getPossibilitiesPart2(Set<String> remainingSteps, TreeMap<Integer, String> runningSteps) {
+        TreeSet<String> possibilities = new TreeSet<>(remainingSteps);
+        for (Pair<String, String> rule : rules) {
+            possibilities.removeAll(runningSteps.values());
+            if (remainingSteps.contains(rule.getValue0()) || runningSteps.containsValue(rule.getValue0())) {
+                possibilities.remove(rule.getValue1());
+            }
+        }
+        return possibilities;
+    }
+
+    private int buildTargetTimestamp(int timestamp, int durationOffset, String step) {
+        int letterValue = step.charAt(0) - 'A' + 1;
+        return timestamp + durationOffset + letterValue;
+    }
+
+    private Set<String> buildInitialRemainingSteps() {
+        Set<String> remainingSteps = new HashSet<>();
+        rules.forEach(pair -> {
+            remainingSteps.add(pair.getValue0());
+            remainingSteps.add(pair.getValue1());
+        });
+        return remainingSteps;
+    }
+
+    private void parseRules(List<String> input) {
+        rules.clear();
+        Pattern p = Pattern.compile("Step ([A-Z]) must be finished before step ([A-Z]) can begin.");
+        for (String line : input) {
+            Matcher m = p.matcher(line);
+            if (m.matches()) {
+                rules.add(Pair.with(m.group(1), m.group(2)));
+            }
+        }
     }
 
 }
--- src/test/java/de/beachboys/aoc2018/Day07Test.java
+++ src/test/java/de/beachboys/aoc2018/Day07Test.java
@@ -3,6 +3,7 @@ package de.beachboys.aoc2018;
 import de.beachboys.Day;
 import de.beachboys.DayTest;
 import de.beachboys.IOHelper;
+import de.beachboys.IOHelperForTests;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -15,15 +16,25 @@ public class Day07Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("Step C must be finished before step A can begin.",
+                        "Step C must be finished before step F can begin.",
+                        "Step A must be finished before step B can begin.",
+                        "Step A must be finished before step D can begin.",
+                        "Step B must be finished before step E can begin.",
+                        "Step D must be finished before step E can begin.",
+                        "Step F must be finished before step E can begin."), "CABDFE", null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("Step C must be finished before step A can begin.",
+                        "Step C must be finished before step F can begin.",
+                        "Step A must be finished before step B can begin.",
+                        "Step A must be finished before step D can begin.",
+                        "Step B must be finished before step E can begin.",
+                        "Step D must be finished before step E can begin.",
+                        "Step F must be finished before step E can begin."), 15, new IOHelperForTests(List.of("2", "0"), null))
         );
     }
 
