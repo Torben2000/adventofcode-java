3065a5f8cef63af92e30ab1bb456bb66afd28cae
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2021-12-02 06:27:45 +0100

    Add 2021-2

--- src/main/java/de/beachboys/aoc2021/Day02.java
+++ src/main/java/de/beachboys/aoc2021/Day02.java
@@ -1,17 +1,60 @@
 package de.beachboys.aoc2021;
 
 import de.beachboys.Day;
+import de.beachboys.Direction;
+import org.javatuples.Pair;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Day02 extends Day {
 
     public Object part1(List<String> input) {
-        return "part1result";
+        long horizontal = 0;
+        long depth = 0;
+        for (String line : input) {
+            int value = Integer.parseInt(line.split(" ")[1]);
+            switch (line.charAt(0)) {
+                case 'f':
+                    horizontal += value;
+                    break;
+                case 'u':
+                    depth -= value;
+                    break;
+                case 'd':
+                    depth += value;
+                    break;
+                default:
+                    throw new IllegalArgumentException();
+            }
+        }
+
+        return horizontal * depth;
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        long aim = 0;
+        long horizontal = 0;
+        long depth = 0;
+        for (String line : input) {
+            int value = Integer.parseInt(line.split(" ")[1]);
+            switch (line.charAt(0)) {
+                case 'f':
+                    horizontal += value;
+                    depth += aim * value;
+                    break;
+                case 'u':
+                    aim -= value;
+                    break;
+                case 'd':
+                    aim += value;
+                    break;
+                default:
+                    throw new IllegalArgumentException();
+            }
+        }
+
+        return horizontal * depth;
     }
 
 }
--- src/test/java/de/beachboys/aoc2021/Day02Test.java
+++ src/test/java/de/beachboys/aoc2021/Day02Test.java
@@ -15,15 +15,23 @@ public class Day02Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("forward 5",
+                        "down 5",
+                        "forward 8",
+                        "up 3",
+                        "down 8",
+                        "forward 2"), 150, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("forward 5",
+                        "down 5",
+                        "forward 8",
+                        "up 3",
+                        "down 8",
+                        "forward 2"), 900, null)
         );
     }
 
