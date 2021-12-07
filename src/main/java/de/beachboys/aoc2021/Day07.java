cb227c2b6e1abf7290b1ae03fe7b448ac086fdb8
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2021-12-07 06:34:08 +0100

    Add 2021-7

--- src/main/java/de/beachboys/aoc2021/Day07.java
+++ src/main/java/de/beachboys/aoc2021/Day07.java
@@ -1,17 +1,46 @@
 package de.beachboys.aoc2021;
 
 import de.beachboys.Day;
+import de.beachboys.Util;
 
 import java.util.List;
+import java.util.function.LongUnaryOperator;
 
 public class Day07 extends Day {
 
     public Object part1(List<String> input) {
-        return "part1result";
+        return runLogic(input, this::incrementCalculatorPart1);
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        return runLogic(input, this::incrementCalculatorPart2);
+    }
+
+    private long runLogic(List<String> input, LongUnaryOperator incrementCalculator) {
+        List<Integer> crabs = Util.parseIntCsv(input.get(0));
+
+        int minPos = crabs.stream().mapToInt(Integer::intValue).min().orElseThrow();
+        int maxPos = crabs.stream().mapToInt(Integer::intValue).max().orElseThrow();
+
+        long minSum = Long.MAX_VALUE;
+        for (int i = minPos; i <= maxPos; i++) {
+            long sum = 0L;
+            for (Integer pos : crabs) {
+                long diff = Math.abs(i - pos);
+                sum += incrementCalculator.applyAsLong(diff);
+            }
+            minSum = Math.min(minSum, sum);
+        }
+
+        return minSum;
+    }
+
+    private long incrementCalculatorPart1(long diff) {
+        return diff;
+    }
+
+    private long incrementCalculatorPart2(long diff) {
+        return diff * (diff + 1) / 2;
     }
 
 }
--- src/test/java/de/beachboys/aoc2021/Day07Test.java
+++ src/test/java/de/beachboys/aoc2021/Day07Test.java
@@ -15,15 +15,13 @@ public class Day07Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("16,1,2,0,4,2,7,1,2,14"), 37, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("16,1,2,0,4,2,7,1,2,14"), 168, null)
         );
     }
 
