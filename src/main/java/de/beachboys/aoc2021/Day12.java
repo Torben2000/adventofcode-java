26130776c64be31885a4c68b504be764831a2a71
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2021-12-12 07:02:34 +0100

    Add 2021-12

--- src/main/java/de/beachboys/aoc2021/Day12.java
+++ src/main/java/de/beachboys/aoc2021/Day12.java
@@ -2,16 +2,61 @@ package de.beachboys.aoc2021;
 
 import de.beachboys.Day;
 
-import java.util.List;
+import java.util.*;
 
 public class Day12 extends Day {
 
+    private final Map<String, List<String>> graph = new HashMap<>();
+
     public Object part1(List<String> input) {
-        return "part1result";
+        parseGraph(input);
+        return findWaysToEnd(List.of(), "start", false, Set.of()).size();
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        parseGraph(input);
+        return findWaysToEnd(List.of(), "start", true, Set.of()).size();
+    }
+
+    private void parseGraph(List<String> input) {
+        graph.clear();
+        for (String line : input) {
+            String[] nodes = line.split("-");
+            graph.putIfAbsent(nodes[0], new ArrayList<>());
+            graph.putIfAbsent(nodes[1], new ArrayList<>());
+            graph.get(nodes[0]).add(nodes[1]);
+            graph.get(nodes[1]).add(nodes[0]);
+        }
+    }
+
+    private Set<List<String>> findWaysToEnd(List<String> pathToCave, String cave, boolean allowSecondVisitOfOneSmallCave, Set<String> visitedSmallCaves) {
+        Set<List<String>> waysToEnd = new HashSet<>();
+        if ("end".equals(cave)) {
+            waysToEnd.addAll(List.of(pathToCave));
+        } else if (pathToCave.size() == 0 || !"start".equals(cave)) {
+
+            boolean allowSecondVisitOfOneSmallCaveNew = allowSecondVisitOfOneSmallCave;
+            Set<String> visitedSmallCavesInclCurrent = new HashSet<>(visitedSmallCaves);
+            if (allowSecondVisitOfOneSmallCave && visitedSmallCaves.contains(cave)) {
+                allowSecondVisitOfOneSmallCaveNew = false;
+            } else if (isSmallCave(cave)) {
+                visitedSmallCavesInclCurrent.add(cave);
+            }
+
+            if (!isSmallCave(cave) || allowSecondVisitOfOneSmallCave || !visitedSmallCaves.contains(cave)) {
+                List<String> pathWithCave = new ArrayList<>(pathToCave);
+                pathWithCave.add(cave);
+                for (String nextCave : graph.get(cave)) {
+                    waysToEnd.addAll(findWaysToEnd(pathWithCave, nextCave, allowSecondVisitOfOneSmallCaveNew, visitedSmallCavesInclCurrent));
+                }
+            }
+
+        }
+        return waysToEnd;
+    }
+
+    private boolean isSmallCave(String node) {
+        return node.toLowerCase().equals(node);
     }
 
 }
--- src/test/java/de/beachboys/aoc2021/Day12Test.java
+++ src/test/java/de/beachboys/aoc2021/Day12Test.java
@@ -15,15 +15,81 @@ public class Day12Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("start-A",
+                        "start-b",
+                        "A-c",
+                        "A-b",
+                        "b-d",
+                        "A-end",
+                        "b-end"), 10, null),
+                Arguments.of(List.of("dc-end",
+                        "HN-start",
+                        "start-kj",
+                        "dc-start",
+                        "dc-HN",
+                        "LN-dc",
+                        "HN-end",
+                        "kj-sa",
+                        "kj-HN",
+                        "kj-dc"), 19, null),
+                Arguments.of(List.of("fs-end",
+                        "he-DX",
+                        "fs-he",
+                        "start-DX",
+                        "pj-DX",
+                        "end-zg",
+                        "zg-sl",
+                        "zg-pj",
+                        "pj-he",
+                        "RW-he",
+                        "fs-DX",
+                        "pj-RW",
+                        "zg-RW",
+                        "start-pj",
+                        "he-WI",
+                        "zg-he",
+                        "pj-fs",
+                        "start-RW"), 226, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("start-A",
+                        "start-b",
+                        "A-c",
+                        "A-b",
+                        "b-d",
+                        "A-end",
+                        "b-end"), 36, null),
+                Arguments.of(List.of("dc-end",
+                        "HN-start",
+                        "start-kj",
+                        "dc-start",
+                        "dc-HN",
+                        "LN-dc",
+                        "HN-end",
+                        "kj-sa",
+                        "kj-HN",
+                        "kj-dc"), 103, null),
+                Arguments.of(List.of("fs-end",
+                        "he-DX",
+                        "fs-he",
+                        "start-DX",
+                        "pj-DX",
+                        "end-zg",
+                        "zg-sl",
+                        "zg-pj",
+                        "pj-he",
+                        "RW-he",
+                        "fs-DX",
+                        "pj-RW",
+                        "zg-RW",
+                        "start-pj",
+                        "he-WI",
+                        "zg-he",
+                        "pj-fs",
+                        "start-RW"), 3509, null)
         );
     }
 
