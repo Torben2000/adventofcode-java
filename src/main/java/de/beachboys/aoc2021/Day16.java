abb5708e2f7568358e75f93c028b7848055153c0
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2021-12-16 07:45:14 +0100

    Add 2021-16

--- src/main/java/de/beachboys/aoc2021/Day16.java
+++ src/main/java/de/beachboys/aoc2021/Day16.java
@@ -1,17 +1,150 @@
 package de.beachboys.aoc2021;
 
 import de.beachboys.Day;
+import org.javatuples.Pair;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static de.beachboys.aoc2021.Day16.ParseMode.*;
 
 public class Day16 extends Day {
 
+    enum ParseMode {
+        VERSION(3), TYPE(3), ID(1), PACKET_LENGTH(15), PACKET_COUNT(11), VALUE(5);
+
+        final int length;
+
+        ParseMode(int length) {
+            this.length = length;
+        }
+    }
+
+    private long versionSum = 0L;
+
     public Object part1(List<String> input) {
-        return "part1result";
+        runLogic(input);
+        return versionSum;
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        return runLogic(input).getValue1();
+    }
+
+    private Pair<Integer, Long> runLogic(List<String> input) {
+        versionSum = 0;
+        String bits = parseBitsFromHex(input.get(0));
+        return parsePacket(bits, 0);
+    }
+
+    private Pair<Integer, Long> parsePacket(String bits, int pointer) {
+        int localPointer = pointer;
+        ParseMode parseMode = VERSION;
+        int packetType = 0;
+        List<Long> values = new ArrayList<>();
+        boolean packetEndReached = false;
+
+        while (!packetEndReached) {
+            int value = Integer.valueOf(bits.substring(localPointer, localPointer + parseMode.length), 2);
+            localPointer += parseMode.length;
+            switch (parseMode) {
+                case VERSION:
+                    versionSum += value;
+                    parseMode = TYPE;
+                    break;
+                case TYPE:
+                    packetType = value;
+                    parseMode = ID;
+                    if (packetType == 4) {
+                        parseMode = VALUE;
+                    }
+                    break;
+                case VALUE:
+                    long curValue = value % 16;
+                    values.add(curValue);
+                    if (value < 16) {
+                        packetEndReached = true;
+                    }
+                    break;
+                case ID:
+                    if (value == 0) {
+                        parseMode = PACKET_LENGTH;
+                    } else {
+                        parseMode = PACKET_COUNT;
+                    }
+                    break;
+                case PACKET_LENGTH:
+                    int packetEnd = localPointer + value;
+                    while (localPointer < packetEnd) {
+                        Pair<Integer, Long> packet = parsePacket(bits, localPointer);
+                        localPointer = packet.getValue0();
+                        values.add(packet.getValue1());
+                    }
+                    packetEndReached = true;
+                    break;
+                case PACKET_COUNT:
+                    for (int i = 0; i < value; i++) {
+                        Pair<Integer, Long> packet = parsePacket(bits, localPointer);
+                        localPointer = packet.getValue0();
+                        values.add(packet.getValue1());
+                    }
+                    packetEndReached = true;
+                    break;
+                default:
+                    throw new IllegalArgumentException();
+            }
+        }
+        return Pair.with(localPointer, calculatePacketValue(packetType, values));
+    }
+
+    private long calculatePacketValue(int packetType, List<Long> values) {
+        switch (packetType) {
+            case 0:
+                return values.stream().reduce(0L, Long::sum);
+            case 1:
+                return values.stream().reduce(1L, (l1, l2) -> l1 * l2);
+            case 2:
+                return values.stream().reduce(Long.MAX_VALUE, Math::min);
+            case 3:
+                return values.stream().reduce(Long.MIN_VALUE, Math::max);
+            case 4:
+                return values.stream().reduce(0L, (l1, l2) -> l1 * 16 + l2);
+            case 5:
+                return values.get(0) > values.get(1) ? 1 : 0;
+            case 6:
+                return values.get(0) < values.get(1) ? 1 : 0;
+            case 7:
+                return values.get(0).equals(values.get(1)) ? 1 : 0;
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+
+    private String parseBitsFromHex(String input) {
+        StringBuilder output = new StringBuilder();
+        Map<Character, String> replacements = new HashMap<>();
+        replacements.put('0', "0000");
+        replacements.put('1', "0001");
+        replacements.put('2', "0010");
+        replacements.put('3', "0011");
+        replacements.put('4', "0100");
+        replacements.put('5', "0101");
+        replacements.put('6', "0110");
+        replacements.put('7', "0111");
+        replacements.put('8', "1000");
+        replacements.put('9', "1001");
+        replacements.put('A', "1010");
+        replacements.put('B', "1011");
+        replacements.put('C', "1100");
+        replacements.put('D', "1101");
+        replacements.put('E', "1110");
+        replacements.put('F', "1111");
+        for (char c : input.toCharArray()) {
+            output.append(replacements.get(c));
+        }
+        return output.toString();
     }
 
 }
--- src/test/java/de/beachboys/aoc2021/Day16Test.java
+++ src/test/java/de/beachboys/aoc2021/Day16Test.java
@@ -15,15 +15,26 @@ public class Day16Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("D2FE28"), 6, null),
+                Arguments.of(List.of("38006F45291200"), 9, null),
+                Arguments.of(List.of("EE00D40C823060"), 14, null),
+                Arguments.of(List.of("8A004A801A8002F478"), 16, null),
+                Arguments.of(List.of("620080001611562C8802118E34"), 12, null),
+                Arguments.of(List.of("C0015000016115A2E0802F182340"), 23, null),
+                Arguments.of(List.of("A0016C880162017C3686B18A3D4780"), 31, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("C200B40A82"), 3, null),
+                Arguments.of(List.of("04005AC33890"), 54, null),
+                Arguments.of(List.of("880086C3E88112"), 7, null),
+                Arguments.of(List.of("CE00C43D881120"), 9, null),
+                Arguments.of(List.of("D8005AC2A8F0"), 1, null),
+                Arguments.of(List.of("F600BC2D8F"), 0, null),
+                Arguments.of(List.of("9C005AC2F8F0"), 0, null),
+                Arguments.of(List.of("9C0141080250320F1802104A08"), 1, null)
         );
     }
 
