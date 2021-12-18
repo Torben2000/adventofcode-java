f62ea8566a2ca62f2684caad7e6bf86b481e3a81
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2021-12-18 09:39:42 +0100

    Add 2021-18

--- src/main/java/de/beachboys/aoc2021/Day18.java
+++ src/main/java/de/beachboys/aoc2021/Day18.java
@@ -1,17 +1,221 @@
 package de.beachboys.aoc2021;
 
 import de.beachboys.Day;
+import org.javatuples.Pair;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day18 extends Day {
 
-    public Object part1(List<String> input) {
-        return "part1result";
+    Pattern pairPattern = Pattern.compile("(\\[#[0-9]+,#[0-9]+])");
+    Pattern numberPattern = Pattern.compile("([\\[,])([0-9]+)");
+
+    public Long part1(List<String> input) {
+        List<SnailfishNumber> snailfishNumbers = parseSnailfishNumbers(input);
+        SnailfishNumber snailfishNumber = sumUpSnails(snailfishNumbers);
+        return getMagnitude(snailfishNumber);
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        long max = 0;
+        for (int i = 0; i < input.size(); i++) {
+            for (int j = 0; j < input.size(); j++) {
+                if (i == j) {
+                    continue;
+                }
+                max = Math.max(max, part1(List.of(input.get(i), input.get(j))));
+            }
+        }
+        return max;
     }
 
+    public List<SnailfishNumber> parseSnailfishNumbers(List<String> input) {
+        List<SnailfishNumber> snailfishNumbers = new ArrayList<>();
+        for (String line : input) {
+            snailfishNumbers.add(parseSnailfishNumber(line));
+        }
+        return snailfishNumbers;
+    }
+
+    public SnailfishNumber sumUpSnails(List<SnailfishNumber> snailfishNumbers) {
+        return snailfishNumbers.stream().reduce(null, this::addSnailfishNumbers);
+    }
+
+    public SnailfishNumber parseSnailfishNumber(String line) {
+        Map<String, SnailfishNumber> map = new HashMap<>();
+        int replacementCounter = 0;
+        String workingString = line;
+        while (true) {
+            Matcher m = numberPattern.matcher(workingString);
+            if (m.find()) {
+                String match = m.group(2);
+                String replacement = "#" + replacementCounter++;
+                map.put(replacement, new SnailfishNumber(Integer.parseInt(match)));
+                workingString = m.replaceFirst(m.group(1) + replacement);
+            } else {
+                break;
+            }
+        }
+        SnailfishNumber snailfishNumber = null;
+        while (true) {
+            Matcher m = pairPattern.matcher(workingString);
+            if (m.find()) {
+                String match = m.group(1);
+                String[] splitMatch = match.substring(1, match.length() - 1).split(",");
+                SnailfishNumber left = map.get(splitMatch[0]);
+                SnailfishNumber right = map.get(splitMatch[1]);
+                snailfishNumber = new SnailfishNumber(left, right);
+                String replacement = "#" + replacementCounter++;
+                map.put(replacement, snailfishNumber);
+                workingString = m.replaceFirst(replacement);
+            } else {
+                break;
+            }
+        }
+        return snailfishNumber;
+    }
+
+    protected long getMagnitude(SnailfishNumber snailfishNumber) {
+        if (snailfishNumber.value != -1) {
+            return snailfishNumber.value;
+        }
+        return 3 * getMagnitude(snailfishNumber.left) + 2 * getMagnitude(snailfishNumber.right);
+    }
+
+    private SnailfishNumber addSnailfishNumbers(SnailfishNumber snailfishNumber1, SnailfishNumber snailfishNumber2) {
+        if (snailfishNumber1 == null) {
+            return snailfishNumber2;
+        } else if (snailfishNumber2 == null) {
+            return snailfishNumber1;
+        }
+        SnailfishNumber snailfishNumber = new SnailfishNumber(snailfishNumber1, snailfishNumber2);
+        return reduceSnailfishNumber(snailfishNumber);
+    }
+
+    private SnailfishNumber reduceSnailfishNumber(SnailfishNumber snailfishNumber) {
+        String oldString = "";
+        while (!oldString.equals(snailfishNumber.toString())) {
+            oldString = snailfishNumber.toString();
+            Pair<Boolean, SnailfishNumber> explodeResult = explodeLeftmostPair(snailfishNumber, 0);
+            if (explodeResult.getValue0()) {
+                snailfishNumber = explodeResult.getValue1();
+            } else {
+                snailfishNumber = splitLeftmostNumber(snailfishNumber).getValue1();
+            }
+        }
+        return snailfishNumber;
+    }
+
+    public Pair<Boolean, SnailfishNumber> splitLeftmostNumber(SnailfishNumber snailfishNumber) {
+        boolean numberSplit = false;
+        if (snailfishNumber.value != -1) {
+            if (snailfishNumber.value >= 10) {
+                SnailfishNumber newSnailfishNumberLeft = new SnailfishNumber(snailfishNumber.value / 2);
+                SnailfishNumber newSnailfishNumberRight = new SnailfishNumber((int) Math.ceil(((double) snailfishNumber.value) / 2));
+                SnailfishNumber newSnailfishNumber = new SnailfishNumber(newSnailfishNumberLeft, newSnailfishNumberRight);
+
+                newSnailfishNumber.parent = snailfishNumber.parent;
+                return Pair.with(true, newSnailfishNumber);
+            }
+        } else {
+            Pair<Boolean, SnailfishNumber> left = splitLeftmostNumber(snailfishNumber.left);
+            if (left.getValue0()) {
+                snailfishNumber.left = left.getValue1();
+                numberSplit = true;
+            } else {
+                Pair<Boolean, SnailfishNumber> right = splitLeftmostNumber(snailfishNumber.right);
+                if (right.getValue0()) {
+                    snailfishNumber.right = right.getValue1();
+                    numberSplit = true;
+                }
+            }
+        }
+        return Pair.with(numberSplit, snailfishNumber);
+    }
+
+    public Pair<Boolean, SnailfishNumber> explodeLeftmostPair(SnailfishNumber snailfishNumber, int i) {
+        boolean pairExploded = false;
+        if (snailfishNumber.value == -1) {
+            Pair<Boolean, SnailfishNumber> explodeResultLeft = explodeLeftmostPair(snailfishNumber.left, i + 1);
+            if (explodeResultLeft.getValue0()) {
+                snailfishNumber.left = explodeResultLeft.getValue1();
+                pairExploded = true;
+            } else {
+                Pair<Boolean, SnailfishNumber> explodeResultRight = explodeLeftmostPair(snailfishNumber.right, i + 1);
+                if (explodeResultRight.getValue0()) {
+                    snailfishNumber.right = explodeResultRight.getValue1();
+                    pairExploded = true;
+                }
+            }
+
+            if (!pairExploded && i >= 4) {
+
+                SnailfishNumber nextLeftNeighbor = findNextValueNeighbor(snailfishNumber, s1 -> s1.parent.left, s -> s.right);
+                if (nextLeftNeighbor != null) {
+                    nextLeftNeighbor.value += snailfishNumber.left.value;
+                }
+                SnailfishNumber nextRightNeighbor = findNextValueNeighbor(snailfishNumber, s1 -> s1.parent.right, s -> s.left);
+                if (nextRightNeighbor != null) {
+                    nextRightNeighbor.value += snailfishNumber.right.value;
+                }
+                
+                SnailfishNumber newSnailfishNumber = new SnailfishNumber(0);
+                newSnailfishNumber.parent = snailfishNumber.parent;
+                return Pair.with(true, newSnailfishNumber);
+             }
+        }
+        return Pair.with(pairExploded, snailfishNumber);
+    }
+
+    private SnailfishNumber findNextValueNeighbor(SnailfishNumber snailfishNumber, Function<SnailfishNumber, SnailfishNumber> getSiblingToGoDownAt, Function<SnailfishNumber, SnailfishNumber> nextSnailfishNumberWhenGoingDown) {
+        if (snailfishNumber.parent == null) {
+            return null;
+        }
+        SnailfishNumber possibleSibling = getSiblingToGoDownAt.apply(snailfishNumber);
+        if (possibleSibling == snailfishNumber) {
+            return findNextValueNeighbor(snailfishNumber.parent, getSiblingToGoDownAt, nextSnailfishNumberWhenGoingDown);
+        }
+        return moveThroughTreeAndFindValue(possibleSibling, nextSnailfishNumberWhenGoingDown);
+    }
+
+    private SnailfishNumber moveThroughTreeAndFindValue(SnailfishNumber snailfishNumber, Function<SnailfishNumber, SnailfishNumber> nextSnailfishNumber) {
+       if (snailfishNumber.value != -1) {
+           return snailfishNumber;
+       }
+       return moveThroughTreeAndFindValue(nextSnailfishNumber.apply(snailfishNumber), nextSnailfishNumber);
+   }
+
+    public static class SnailfishNumber {
+
+        SnailfishNumber left;
+        SnailfishNumber right;
+        SnailfishNumber parent;
+        long value = -1;
+
+        public SnailfishNumber(long value) {
+            this.value = value;
+        }
+
+        public SnailfishNumber(SnailfishNumber left, SnailfishNumber right) {
+            this.left = left;
+            this.right = right;
+            left.parent = this;
+            right.parent = this;
+        }
+
+        @Override
+        public String toString() {
+            if (value != -1) {
+                return String.valueOf(value);
+            }
+            return "[" + left + "," + right + "]";
+        }
+
+    }
 }
--- src/test/java/de/beachboys/aoc2021/Day18Test.java
+++ src/test/java/de/beachboys/aoc2021/Day18Test.java
@@ -1,29 +1,152 @@
 package de.beachboys.aoc2021;
 
-import de.beachboys.Day;
 import de.beachboys.DayTest;
 import de.beachboys.IOHelper;
+import org.javatuples.Pair;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+
 import java.util.List;
 import java.util.stream.Stream;
 
 public class Day18Test extends DayTest {
 
-    private final Day day = new Day18();
+    private final Day18 day = new Day18();
+
+    private static Stream<Arguments> provideTestDataForExplode() {
+        return Stream.of(
+                Arguments.of("[[[[[9,8],1],2],3],4]", "[[[[0,9],2],3],4]"),
+                Arguments.of("[7,[6,[5,[4,[3,2]]]]]", "[7,[6,[5,[7,0]]]]"),
+                Arguments.of("[[6,[5,[4,[3,2]]]],1]", "[[6,[5,[7,0]]],3]"),
+                Arguments.of("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"),
+                Arguments.of("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"),
+                Arguments.of("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]", "[[[[0,7],4],[7,[[8,4],9]]],[1,1]]"),
+                Arguments.of("[[[[0,7],4],[7,[[8,4],9]]],[1,1]]", "[[[[0,7],4],[15,[0,13]]],[1,1]]"),
+                Arguments.of("[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]", "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")
+        );
+
+    }
+    @ParameterizedTest
+    @MethodSource("provideTestDataForExplode")
+    public void testExplode(String snailFishNumber, String expectedSnailfishNumber) {
+        Day18.SnailfishNumber s = day.parseSnailfishNumber(snailFishNumber);
+        Pair<Boolean, Day18.SnailfishNumber> result = day.explodeLeftmostPair(s, 0);
+        Assertions.assertEquals(true, result.getValue0());
+        Assertions.assertEquals(expectedSnailfishNumber, result.getValue1().toString());
+    }
+
+    private static Stream<Arguments> provideTestDataForSplit() {
+        return Stream.of(
+                Arguments.of("[[[[0,7],4],[15,[0,13]]],[1,1]]", "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]"),
+                Arguments.of("[[[[0,7],4],[[7,8],[0,13]]],[1,1]]", "[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTestDataForSplit")
+    public void testSplit(String snailFishNumber, String expectedSnailfishNumber) {
+        Day18.SnailfishNumber s = day.parseSnailfishNumber(snailFishNumber);
+        Pair<Boolean, Day18.SnailfishNumber> result = day.splitLeftmostNumber(s);
+        Assertions.assertEquals(true, result.getValue0());
+        Assertions.assertEquals(expectedSnailfishNumber, result.getValue1().toString());
+    }
+
+    private static Stream<Arguments> provideTestDataForSumUp() {
+        return Stream.of(
+                Arguments.of(List.of("[[[[4,3],4],4],[7,[[8,4],9]]]",
+                        "[1,1]"), "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"),
+                Arguments.of(List.of("[1,1]",
+                        "[2,2]",
+                        "[3,3]",
+                        "[4,4]"), "[[[[1,1],[2,2]],[3,3]],[4,4]]"),
+                Arguments.of(List.of("[1,1]",
+                        "[2,2]",
+                        "[3,3]",
+                        "[4,4]",
+                        "[5,5]"), "[[[[3,0],[5,3]],[4,4]],[5,5]]"),
+                Arguments.of(List.of("[1,1]",
+                        "[2,2]",
+                        "[3,3]",
+                        "[4,4]",
+                        "[5,5]",
+                        "[6,6]"), "[[[[5,0],[7,4]],[5,5]],[6,6]]"),
+                Arguments.of(List.of("[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]",
+                        "[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]",
+                        "[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]",
+                        "[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]",
+                        "[7,[5,[[3,8],[1,4]]]]",
+                        "[[2,[2,2]],[8,[8,1]]]",
+                        "[2,9]",
+                        "[1,[[[9,3],9],[[9,0],[0,7]]]]",
+                        "[[[5,[7,4]],7],1]",
+                        "[[[[4,2],2],6],[8,7]]"), "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]"),
+                Arguments.of(List.of("[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]",
+                        "[[[5,[2,8]],4],[5,[[9,9],0]]]",
+                        "[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]",
+                        "[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]",
+                        "[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]",
+                        "[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]",
+                        "[[[[5,4],[7,7]],8],[[8,3],8]]",
+                        "[[9,3],[[9,9],[6,[4,9]]]]",
+                        "[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]",
+                        "[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]"), "[[[[6,6],[7,6]],[[7,7],[7,0]]],[[[7,7],[7,7]],[[7,8],[9,9]]]]")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTestDataForSumUp")
+    public void testSumUp(List<String> snailfishNumbers, String expectedSnailfish) {
+        List<Day18.SnailfishNumber> list = day.parseSnailfishNumbers(snailfishNumbers);
+        Assertions.assertEquals(expectedSnailfish, day.sumUpSnails(list).toString());
+    }
+
+    private static Stream<Arguments> provideTestDataForGetMagnitude() {
+        return Stream.of(
+                Arguments.of("[[1,2],[[3,4],5]]", 143),
+                Arguments.of("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]", 1384),
+                Arguments.of("[[[[1,1],[2,2]],[3,3]],[4,4]]", 445),
+                Arguments.of("[[[[3,0],[5,3]],[4,4]],[5,5]]", 791),
+                Arguments.of("[[[[5,0],[7,4]],[5,5]],[6,6]]", 1137),
+                Arguments.of("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]", 3488)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTestDataForGetMagnitude")
+    public void testGetMagnitude(String snailFishNumber, long expectedMagnitude) {
+        Day18.SnailfishNumber s = day.parseSnailfishNumber(snailFishNumber);
+        Assertions.assertEquals(expectedMagnitude, day.getMagnitude(s));
+    }
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]",
+                        "[[[5,[2,8]],4],[5,[[9,9],0]]]",
+                        "[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]",
+                        "[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]",
+                        "[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]",
+                        "[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]",
+                        "[[[[5,4],[7,7]],8],[[8,3],8]]",
+                        "[[9,3],[[9,9],[6,[4,9]]]]",
+                        "[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]",
+                        "[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]"), 4140, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]",
+                        "[[[5,[2,8]],4],[5,[[9,9],0]]]",
+                        "[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]",
+                        "[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]",
+                        "[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]",
+                        "[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]",
+                        "[[[[5,4],[7,7]],8],[[8,3],8]]",
+                        "[[9,3],[[9,9],[6,[4,9]]]]",
+                        "[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]",
+                        "[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]"), 3993, null)
         );
     }
 
