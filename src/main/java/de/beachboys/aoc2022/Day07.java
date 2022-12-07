e325b66f0b34e6dc65aaad65121a75b3824b04c3
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2022-12-07 06:41:42 +0100

    Add day 2022-07

--- src/main/java/de/beachboys/aoc2022/Day07.java
+++ src/main/java/de/beachboys/aoc2022/Day07.java
@@ -2,16 +2,89 @@ package de.beachboys.aoc2022;
 
 import de.beachboys.Day;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day07 extends Day {
 
     public Object part1(List<String> input) {
-        return "part1result";
+        List<Directory> allDirectories = parseFilesystem(input);
+        return allDirectories.stream().map(Directory::getSize).filter(size -> size <= 100000).reduce(Long::sum).orElseThrow();
+    }
+    public Object part2(List<String> input) {
+        List<Directory> allDirectories = parseFilesystem(input);
+        long neededSpace = allDirectories.get(0).getSize() - 40000000;
+        return allDirectories.stream().map(Directory::getSize).filter(size -> size >= neededSpace).min(Long::compareTo).orElseThrow();
     }
 
-    public Object part2(List<String> input) {
-        return 2;
+    private static List<Directory> parseFilesystem(List<String> input) {
+        List<Directory> allDirectories = new ArrayList<>();
+        Directory root = new Directory("/", null);
+        allDirectories.add(root);
+        Directory currentDirectory = root;
+        for (String line : input) {
+            String[] splitLine = line.split(" ");
+            if (splitLine[0].equals("$") && splitLine[1].equals("cd")) {
+                if (splitLine[2].equals("/")) {
+                    currentDirectory = root;
+                } else if (splitLine[2].equals("..")) {
+                    currentDirectory = currentDirectory.parent;
+                } else {
+                    Directory newDir = new Directory(splitLine[2], currentDirectory);
+                    allDirectories.add(newDir);
+                    currentDirectory.dirs.add(newDir);
+                    currentDirectory = newDir;
+                }
+            } else {
+                try {
+                    int size = Integer.parseInt(splitLine[0]);
+                    File file = new File(splitLine[1], size);
+                    currentDirectory.files.add(file);
+                } catch (NumberFormatException e) {
+                    // not a file so we don't care
+                }
+            }
+        }
+        return allDirectories;
+    }
+
+    private static class Directory {
+
+        String name;
+        Directory parent;
+        List<Directory> dirs = new ArrayList<>();
+        List<File> files = new ArrayList<>();
+        long size;
+
+        Directory(String name, Directory parent) {
+            this.name = name;
+            this.parent = parent;
+        }
+
+        long getSize() {
+            if (size == 0) {
+                for (Directory dir : dirs) {
+                    size += dir.getSize();
+                }
+                for (File file : files) {
+                    size += file.size;
+                }
+            }
+            return size;
+        }
+
+    }
+
+    private static class File {
+
+        String name;
+        int size;
+
+        File(String name, int size) {
+            this.name = name;
+            this.size = size;
+        }
+
     }
 
 }
--- src/test/java/de/beachboys/aoc2022/Day07Test.java
+++ src/test/java/de/beachboys/aoc2022/Day07Test.java
@@ -15,15 +15,57 @@ public class Day07Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("$ cd /",
+                        "$ ls",
+                        "dir a",
+                        "14848514 b.txt",
+                        "8504156 c.dat",
+                        "dir d",
+                        "$ cd a",
+                        "$ ls",
+                        "dir e",
+                        "29116 f",
+                        "2557 g",
+                        "62596 h.lst",
+                        "$ cd e",
+                        "$ ls",
+                        "584 i",
+                        "$ cd ..",
+                        "$ cd ..",
+                        "$ cd d",
+                        "$ ls",
+                        "4060174 j",
+                        "8033020 d.log",
+                        "5626152 d.ext",
+                        "7214296 k"), 95437, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("$ cd /",
+                        "$ ls",
+                        "dir a",
+                        "14848514 b.txt",
+                        "8504156 c.dat",
+                        "dir d",
+                        "$ cd a",
+                        "$ ls",
+                        "dir e",
+                        "29116 f",
+                        "2557 g",
+                        "62596 h.lst",
+                        "$ cd e",
+                        "$ ls",
+                        "584 i",
+                        "$ cd ..",
+                        "$ cd ..",
+                        "$ cd d",
+                        "$ ls",
+                        "4060174 j",
+                        "8033020 d.log",
+                        "5626152 d.ext",
+                        "7214296 k"), 24933642, null)
         );
     }
 
