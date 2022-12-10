625672bdd94913582b341f2a7c99ab8a367bacd6
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2022-12-10 07:00:12 +0100

    Add day 2022-10

--- src/main/java/de/beachboys/aoc2022/Day10.java
+++ src/main/java/de/beachboys/aoc2022/Day10.java
@@ -1,17 +1,65 @@
 package de.beachboys.aoc2022;
 
 import de.beachboys.Day;
+import de.beachboys.OCR;
+import de.beachboys.Util;
+import org.javatuples.Pair;
 
-import java.util.List;
+import java.util.*;
 
 public class Day10 extends Day {
 
+    private int currentCycle = 1;
+    private int currentX = 1;
+    private int part1Result = 0;
+    private final Set<Pair<Integer, Integer>> pixels = new HashSet<>();
+
     public Object part1(List<String> input) {
-        return "part1result";
+        runLogic(input, this::checkPart1);
+
+        return part1Result;
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        runLogic(input, this::checkPart2);
+
+        return OCR.runOCRAndReturnOriginalOnError(Util.paintSet(pixels));
+    }
+
+    private void runLogic(List<String> input, Runnable check) {
+        resetFields();
+        for (String line : input) {
+            currentCycle++;
+            check.run();
+            String[] split = line.split(" ");
+            if ("addx".equals(split[0])) {
+                int value = Integer.parseInt(split[1]);
+                currentX += value;
+                currentCycle++;
+                check.run();
+            }
+        }
+    }
+
+    private void resetFields() {
+        currentX = 1;
+        currentCycle = 1;
+        part1Result = 0;
+        pixels.clear();
+        pixels.add(Pair.with(0, 0));
+    }
+
+    private void checkPart1() {
+        if (currentCycle % 40 == 20) {
+            part1Result += currentCycle * currentX;
+        }
+    }
+
+    private void checkPart2() {
+        int x = (currentCycle - 1) % 40;
+        if (x - 1 <= currentX && x + 1 >= currentX) {
+            pixels.add(Pair.with(x, (currentCycle - 1) / 40));
+        }
     }
 
 }
