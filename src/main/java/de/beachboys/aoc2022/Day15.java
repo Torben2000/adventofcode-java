ab04acb4ced9b6269f3de5d45510aed9158994a8
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2022-12-15 08:02:13 +0100

    Add day 2022-15

--- src/main/java/de/beachboys/aoc2022/Day15.java
+++ src/main/java/de/beachboys/aoc2022/Day15.java
@@ -1,17 +1,91 @@
 package de.beachboys.aoc2022;
 
 import de.beachboys.Day;
+import de.beachboys.Util;
+import org.javatuples.Pair;
 
-import java.util.List;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Day15 extends Day {
 
+    private static final Pattern LINE_PATTERN = Pattern.compile("Sensor at x=([-0-9]+), y=([-0-9]+): closest beacon is at x=([-0-9]+), y=([-0-9]+)");
     public Object part1(List<String> input) {
-        return "part1result";
+        long result = 0;
+        int rowToCheck = Util.getIntValueFromUser("Row to check", 2000000, io);
+        Map<Integer, List<Pair<Integer, Integer>>> blockedSpaces = getBlockedSpaceByRow(input, rowToCheck, rowToCheck);
+
+        for (Pair<Integer, Integer> blockedSpace : blockedSpaces.get(rowToCheck)) {
+            result += blockedSpace.getValue1() - blockedSpace.getValue0();
+        }
+
+        return result;
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        int max = Util.getIntValueFromUser("Max coordinate", 4000000, io);
+        Map<Integer, List<Pair<Integer, Integer>>> blockedSpaces = getBlockedSpaceByRow(input, 0, max);
+
+        for (int y = 0; y <= max; y++) {
+            List<Pair<Integer, Integer>> blockedSpacesByRow = blockedSpaces.get(y);
+            blockedSpacesByRow = blockedSpacesByRow.stream().sorted(Comparator.comparingInt(Pair::getValue0)).collect(Collectors.toList());
+            int x = 0;
+            for (Pair<Integer, Integer> pair : blockedSpacesByRow) {
+                if (pair.getValue0() > x) {
+                    return 4000000L * (long) x + y;
+                }
+                x = Math.max(x, pair.getValue1() + 1);
+            }
+        }
+
+        return "No space found";
+    }
+
+    private Map<Integer, List<Pair<Integer, Integer>>> getBlockedSpaceByRow(List<String> input, int min, int max) {
+        Map<Integer, List<Pair<Integer, Integer>>> blockedSpaceByRow = new HashMap<>();
+        for (String line : input) {
+            Matcher m = LINE_PATTERN.matcher(line);
+            if (m.matches()) {
+                Pair<Integer, Integer> sensor = Pair.with(Integer.parseInt(m.group(1)), Integer.parseInt(m.group(2)));
+                Pair<Integer, Integer> beacon = Pair.with(Integer.parseInt(m.group(3)), Integer.parseInt(m.group(4)));
+                int distance = Util.getManhattanDistance(sensor, beacon);
+
+                for (int xDistance = 0; xDistance <= distance; xDistance++) {
+                    int yDistance = distance - xDistance;
+                    int x1 = sensor.getValue0() - xDistance;
+                    int x2 = sensor.getValue0() + xDistance;
+                    int y1 = sensor.getValue1() - yDistance;
+                    int y2 = sensor.getValue1() + yDistance;
+                    if (y1 >= min) {
+                        addBlockedSpace(blockedSpaceByRow, x1, x2, y1);
+                    }
+                    if (y2 <= max) {
+                        addBlockedSpace(blockedSpaceByRow, x1, x2, y2);
+                    }
+                }
+            }
+
+        }
+        return blockedSpaceByRow;
+    }
+
+    private static void addBlockedSpace(Map<Integer, List<Pair<Integer, Integer>>> blockedSpaceByRow, int start, int end, int row) {
+        List<Pair<Integer, Integer>> blockedSpacesOfRow = blockedSpaceByRow.getOrDefault(row, new ArrayList<>());
+        int mergedStart = start;
+        int mergedEnd = end;
+        Set<Pair<Integer, Integer>> blockedSpacesToRemove = new HashSet<>();
+        for (Pair<Integer, Integer> blockedSpace : blockedSpacesOfRow) {
+            if (mergedStart <= blockedSpace.getValue1() && blockedSpace.getValue0() <= mergedEnd) {
+                blockedSpacesToRemove.add(blockedSpace);
+                mergedStart = Math.min(mergedStart, blockedSpace.getValue0());
+                mergedEnd = Math.max(mergedEnd, blockedSpace.getValue1());
+            }
+        }
+        blockedSpacesOfRow.removeAll(blockedSpacesToRemove);
+        blockedSpacesOfRow.add(Pair.with(mergedStart, mergedEnd));
+        blockedSpaceByRow.put(row, blockedSpacesOfRow);
     }
 
 }
--- src/test/java/de/beachboys/aoc2022/Day15Test.java
+++ src/test/java/de/beachboys/aoc2022/Day15Test.java
@@ -3,6 +3,7 @@ package de.beachboys.aoc2022;
 import de.beachboys.Day;
 import de.beachboys.DayTest;
 import de.beachboys.IOHelper;
+import de.beachboys.IOHelperForTests;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -15,15 +16,39 @@ public class Day15Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("Sensor at x=2, y=18: closest beacon is at x=-2, y=15",
+                        "Sensor at x=9, y=16: closest beacon is at x=10, y=16",
+                        "Sensor at x=13, y=2: closest beacon is at x=15, y=3",
+                        "Sensor at x=12, y=14: closest beacon is at x=10, y=16",
+                        "Sensor at x=10, y=20: closest beacon is at x=10, y=16",
+                        "Sensor at x=14, y=17: closest beacon is at x=10, y=16",
+                        "Sensor at x=8, y=7: closest beacon is at x=2, y=10",
+                        "Sensor at x=2, y=0: closest beacon is at x=2, y=10",
+                        "Sensor at x=0, y=11: closest beacon is at x=2, y=10",
+                        "Sensor at x=20, y=14: closest beacon is at x=25, y=17",
+                        "Sensor at x=17, y=20: closest beacon is at x=21, y=22",
+                        "Sensor at x=16, y=7: closest beacon is at x=15, y=3",
+                        "Sensor at x=14, y=3: closest beacon is at x=15, y=3",
+                        "Sensor at x=20, y=1: closest beacon is at x=15, y=3"), 26, new IOHelperForTests(List.of("10"), null))
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("Sensor at x=2, y=18: closest beacon is at x=-2, y=15",
+                        "Sensor at x=9, y=16: closest beacon is at x=10, y=16",
+                        "Sensor at x=13, y=2: closest beacon is at x=15, y=3",
+                        "Sensor at x=12, y=14: closest beacon is at x=10, y=16",
+                        "Sensor at x=10, y=20: closest beacon is at x=10, y=16",
+                        "Sensor at x=14, y=17: closest beacon is at x=10, y=16",
+                        "Sensor at x=8, y=7: closest beacon is at x=2, y=10",
+                        "Sensor at x=2, y=0: closest beacon is at x=2, y=10",
+                        "Sensor at x=0, y=11: closest beacon is at x=2, y=10",
+                        "Sensor at x=20, y=14: closest beacon is at x=25, y=17",
+                        "Sensor at x=17, y=20: closest beacon is at x=21, y=22",
+                        "Sensor at x=16, y=7: closest beacon is at x=15, y=3",
+                        "Sensor at x=14, y=3: closest beacon is at x=15, y=3",
+                        "Sensor at x=20, y=1: closest beacon is at x=15, y=3"), 56000011, new IOHelperForTests(List.of("20"), null))
         );
     }
 
