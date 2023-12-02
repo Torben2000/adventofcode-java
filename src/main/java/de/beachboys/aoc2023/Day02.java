111dc43dd44ac0ebc497b68cbeeba0b461c2667f
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2023-12-02 06:46:55 +0100

    Add day 2023-02

--- src/main/java/de/beachboys/aoc2023/Day02.java
+++ src/main/java/de/beachboys/aoc2023/Day02.java
@@ -1,17 +1,81 @@
 package de.beachboys.aoc2023;
 
 import de.beachboys.Day;
+import org.javatuples.Pair;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day02 extends Day {
 
     public Object part1(List<String> input) {
-        return "part1result";
+        long result = 0;
+
+        for (String line : input) {
+            Game game = parseGame(line);
+            boolean gamePossible = true;
+            for (Pair<String, Integer> cubeSet : game.cubeSets) {
+                if (!cubeSet.getValue0().equals("red") && !cubeSet.getValue0().equals("green") && !cubeSet.getValue0().equals("blue")
+                        || cubeSet.getValue0().equals("red") && cubeSet.getValue1() > 12
+                        || cubeSet.getValue0().equals("green") && cubeSet.getValue1() > 13
+                        || cubeSet.getValue0().equals("blue") && cubeSet.getValue1() > 14) {
+                    gamePossible = false;
+                    break;
+                }
+            }
+            if (gamePossible) {
+                result += game.id;
+            }
+        }
+
+        return result;
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        long result = 0;
+
+        for (String line : input) {
+            Game game = parseGame(line);
+            int minRedCubes = 0;
+            int minBlueCubes = 0;
+            int minGreenCubes = 0;
+            for (Pair<String, Integer> cubeSet : game.cubeSets) {
+                if (cubeSet.getValue0().equals("red")) {
+                    minRedCubes = Math.max(minRedCubes, cubeSet.getValue1());
+                }
+                if (cubeSet.getValue0().equals("green")) {
+                    minGreenCubes = Math.max(minGreenCubes, cubeSet.getValue1());
+                }
+                if (cubeSet.getValue0().equals("blue")) {
+                    minBlueCubes = Math.max(minBlueCubes, cubeSet.getValue1());
+                }
+
+            }
+            result += (long) minRedCubes * minGreenCubes * minBlueCubes;
+        }
+
+        return result;
+    }
+
+    private static Game parseGame(String line) {
+        Game game = new Game();
+        String[] gameIdAndCubeSets = line.split(":");
+        game.id = Integer.parseInt(gameIdAndCubeSets[0].substring("Game ".length()));
+        String[] multiColorCubeSets = gameIdAndCubeSets[1].trim().split("; ");
+        game.cubeSets = new ArrayList<>();
+        for (String multiColorCubeSet : multiColorCubeSets) {
+            String[] singleColorCubeSets = multiColorCubeSet.split(", ");
+            for (String singleColorCubeSet : singleColorCubeSets) {
+                String[] numberAndColor = singleColorCubeSet.split(" ");
+                game.cubeSets.add(Pair.with(numberAndColor[1], Integer.parseInt(numberAndColor[0])));
+            }
+        }
+        return game;
+    }
+
+    private static class Game {
+        int id;
+        List<Pair<String, Integer>> cubeSets;
     }
 
 }
--- src/test/java/de/beachboys/aoc2023/Day02Test.java
+++ src/test/java/de/beachboys/aoc2023/Day02Test.java
@@ -15,15 +15,21 @@ public class Day02Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+                        "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+                        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+                        "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+                        "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"), 8, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+                        "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+                        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+                        "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+                        "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"), 2286, null)
         );
     }
 
