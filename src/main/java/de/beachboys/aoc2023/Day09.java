8f11434fbc12b0c4bfd54c6a4efd78e19987470d
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2023-12-09 06:31:31 +0100

    Add day 2023-09

--- src/main/java/de/beachboys/aoc2023/Day09.java
+++ src/main/java/de/beachboys/aoc2023/Day09.java
@@ -1,17 +1,53 @@
 package de.beachboys.aoc2023;
 
 import de.beachboys.Day;
+import de.beachboys.Util;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day09 extends Day {
 
     public Object part1(List<String> input) {
-        return "part1result";
+        long result = 0;
+        for (String line : input) {
+            List<List<Long>> allLists = getValueAndDiffLists(line);
+            result += allLists.stream().mapToLong(List::getLast).sum();
+        }
+        return result;
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        long result = 0;
+        for (String line : input) {
+            List<List<Long>> allLists = getValueAndDiffLists(line);
+            long previousValue = 0;
+            for (int j = allLists.size() - 1; j >= 0; j--) {
+                previousValue = allLists.get(j).getFirst() - previousValue;
+            }
+            result += previousValue;
+        }
+        return result;
+    }
+
+    private static List<List<Long>> getValueAndDiffLists(String line) {
+        List<Long> currentList = Util.parseToLongList(line, " ");
+        List<List<Long>> allLists = new ArrayList<>();
+        boolean containsOnlyZeros = false;
+        while (!containsOnlyZeros) {
+            allLists.add(currentList);
+            List<Long> newList = new ArrayList<>();
+            containsOnlyZeros = true;
+            for (int i = 0; i < currentList.size() - 1; i++) {
+                long diff = currentList.get(i + 1) - currentList.get(i);
+                newList.add(diff);
+                if (diff != 0) {
+                    containsOnlyZeros = false;
+                }
+            }
+            currentList = newList;
+        }
+        return allLists;
     }
 
 }
--- src/test/java/de/beachboys/aoc2023/Day09Test.java
+++ src/test/java/de/beachboys/aoc2023/Day09Test.java
@@ -15,15 +15,17 @@ public class Day09Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("0 3 6 9 12 15",
+                        "1 3 6 10 15 21",
+                        "10 13 16 21 30 45"), 114, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("0 3 6 9 12 15",
+                        "1 3 6 10 15 21",
+                        "10 13 16 21 30 45"), 2, null)
         );
     }
 
