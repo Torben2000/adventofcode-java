6e839d342ea9c6915afa01e2fbf82acb93903e43
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2023-12-22 07:55:41 +0100

    Add day 2023-22

--- src/main/java/de/beachboys/aoc2023/Day22.java
+++ src/main/java/de/beachboys/aoc2023/Day22.java
@@ -1,17 +1,141 @@
 package de.beachboys.aoc2023;
 
 import de.beachboys.Day;
+import org.javatuples.Triplet;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Day22 extends Day {
 
+    private Set<Set<Triplet<Integer, Integer, Integer>>> bricks;
+    private Map<Set<Triplet<Integer, Integer, Integer>>, Set<Set<Triplet<Integer, Integer, Integer>>>> lowerToUpperBricks;
+    private Map<Set<Triplet<Integer, Integer, Integer>>, Set<Set<Triplet<Integer, Integer, Integer>>>> upperToLowerBricks;
+
     public Object part1(List<String> input) {
-        return "part1result";
+        parseInputMoveBricksAndStoreBrickRelation(input);
+
+        long result = 0;
+        for (Set<Triplet<Integer, Integer, Integer>> brick : bricks) {
+            boolean safeToRemove = true;
+            for (Set<Triplet<Integer, Integer, Integer>> upperBrick : lowerToUpperBricks.get(brick)) {
+                Set<Set<Triplet<Integer, Integer, Integer>>> upperBrickSupporters = upperToLowerBricks.get(upperBrick);
+                if(upperBrickSupporters.size() == 1) {
+                    safeToRemove = false;
+                }
+            }
+            if (safeToRemove) {
+                result++;
+            }
+        }
+        return result;
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        parseInputMoveBricksAndStoreBrickRelation(input);
+
+        long result = 0;
+        for (Set<Triplet<Integer, Integer, Integer>> brick : bricks) {
+            Set<Set<Triplet<Integer, Integer, Integer>>> removedBricks = new HashSet<>();
+            removedBricks.add(brick);
+            Set<Set<Triplet<Integer, Integer, Integer>>> bricksToCheck = new HashSet<>(lowerToUpperBricks.get(brick));
+            while (!bricksToCheck.isEmpty()) {
+                Set<Set<Triplet<Integer, Integer, Integer>>> newBricksToCheck = new HashSet<>();
+                for (Set<Triplet<Integer, Integer, Integer>> upper : bricksToCheck) {
+                    Set<Set<Triplet<Integer, Integer, Integer>>> upperBrickSupporters = new HashSet<>(upperToLowerBricks.get(upper));
+                    if (!upperBrickSupporters.isEmpty()) {
+                        upperBrickSupporters.removeAll(removedBricks);
+                        if (upperBrickSupporters.isEmpty()) {
+                            removedBricks.add(upper);
+                            newBricksToCheck.addAll(lowerToUpperBricks.get(upper));
+                        }
+                    }
+                }
+                bricksToCheck = newBricksToCheck;
+            }
+            result += removedBricks.size() - 1;
+        }
+        return result;
+    }
+
+    private void parseInputMoveBricksAndStoreBrickRelation(List<String> input) {
+        parseInput(input);
+        moveBricks();
+        storeBrickRelation();
+    }
+
+    private void parseInput(List<String> input) {
+        bricks = new HashSet<>();
+        for (String line : input) {
+            String[] brickEnds = line.split("~");
+            String[] positionEnd1 = brickEnds[0].split(",");
+            String[] positionEnd2 = brickEnds[1].split(",");
+            Set<Triplet<Integer, Integer, Integer>> brick = new HashSet<>();
+            for (int x = Integer.parseInt(positionEnd1[0]); x <= Integer.parseInt(positionEnd2[0]); x++) {
+                for (int y = Integer.parseInt(positionEnd1[1]); y <= Integer.parseInt(positionEnd2[1]); y++) {
+                    for (int z = Integer.parseInt(positionEnd1[2]); z <= Integer.parseInt(positionEnd2[2]); z++) {
+                        brick.add(Triplet.with(x, y, z));
+                    }
+                }
+            }
+            bricks.add(brick);
+        }
+    }
+
+    private void moveBricks() {
+        Set<Triplet<Integer, Integer, Integer>> allCubes = new HashSet<>();
+        for (Set<Triplet<Integer, Integer, Integer>> brick : bricks) {
+            allCubes.addAll(brick);
+        }
+        int maxZ = allCubes.stream().mapToInt(Triplet::getValue2).max().orElseThrow();
+
+        for (int z = 0; z <= maxZ; z++) {
+            int finalZ = z;
+            Set<Set<Triplet<Integer, Integer, Integer>>> bricksAtZ = bricks.stream().filter(brick -> brick.stream().mapToInt(Triplet::getValue2).min().orElseThrow() == finalZ).collect(Collectors.toSet());
+            Set<Set<Triplet<Integer, Integer, Integer>>> movedBricks = new HashSet<>();
+            for (Set<Triplet<Integer, Integer, Integer>> brick : bricksAtZ) {
+                allCubes.removeAll(brick);
+                boolean moving = true;
+                while (moving) {
+                    Set<Triplet<Integer, Integer, Integer>> newBrick = new HashSet<>();
+                    for (Triplet<Integer, Integer, Integer> cube : brick) {
+                        Triplet<Integer, Integer, Integer> newCube = Triplet.with(cube.getValue0(), cube.getValue1(), cube.getValue2() - 1);
+                        if (allCubes.contains(newCube) || newCube.getValue2() == 0) {
+                            moving = false;
+                            break;
+                        }
+                        newBrick.add(newCube);
+                    }
+                    if (moving) {
+                        brick = newBrick;
+                    }
+                }
+                allCubes.addAll(brick);
+                movedBricks.add(brick);
+            }
+            bricks.removeAll(bricksAtZ);
+            bricks.addAll(movedBricks);
+        }
+    }
+
+    private void storeBrickRelation() {
+        lowerToUpperBricks = new HashMap<>();
+        upperToLowerBricks = new HashMap<>();
+        for (Set<Triplet<Integer, Integer, Integer>> brick : bricks) {
+            lowerToUpperBricks.put(brick, new HashSet<>());
+            upperToLowerBricks.put(brick, new HashSet<>());
+        }
+        for (Set<Triplet<Integer, Integer, Integer>> brick : bricks) {
+            for (Triplet<Integer, Integer, Integer> cube : brick) {
+                Triplet<Integer, Integer, Integer> supportingCube = Triplet.with(cube.getValue0(), cube.getValue1(), cube.getValue2() - 1);
+                for (Set<Triplet<Integer, Integer, Integer>> otherBrick : bricks) {
+                    if (otherBrick != brick && otherBrick.contains(supportingCube)) {
+                        lowerToUpperBricks.get(otherBrick).add(brick);
+                        upperToLowerBricks.get(brick).add(otherBrick);
+                    }
+                }
+            }
+        }
     }
 
 }
--- src/test/java/de/beachboys/aoc2023/Day22Test.java
+++ src/test/java/de/beachboys/aoc2023/Day22Test.java
@@ -15,15 +15,25 @@ public class Day22Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("1,0,1~1,2,1",
+                        "0,0,2~2,0,2",
+                        "0,2,3~2,2,3",
+                        "0,0,4~0,2,4",
+                        "2,0,5~2,2,5",
+                        "0,1,6~2,1,6",
+                        "1,1,8~1,1,9"), 5, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("1,0,1~1,2,1",
+                        "0,0,2~2,0,2",
+                        "0,2,3~2,2,3",
+                        "0,0,4~0,2,4",
+                        "2,0,5~2,2,5",
+                        "0,1,6~2,1,6",
+                        "1,1,8~1,1,9"), 7, null)
         );
     }
 
