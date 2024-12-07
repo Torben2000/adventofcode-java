7386e71a8bba347f3b5eb645bc46d295b6836122
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2024-12-07 06:36:20 +0100

    Add day 2024-07

--- src/main/java/de/beachboys/aoc2024/Day07.java
+++ src/main/java/de/beachboys/aoc2024/Day07.java
@@ -1,17 +1,65 @@
 package de.beachboys.aoc2024;
 
 import de.beachboys.Day;
+import de.beachboys.Util;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Day07 extends Day {
 
+    private boolean withConcatenation;
+
     public Object part1(List<String> input) {
-        return "part1result";
+        withConcatenation = false;
+        return runLogic(input);
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        withConcatenation = true;
+        return runLogic(input);
+    }
+
+    private long runLogic(List<String> input) {
+        long result = 0;
+
+        for (String line : input) {
+            String[] split = line.split(": ");
+            long testValue = Long.parseLong(split[0]);
+            List<Long> numbers = Util.parseToLongList(split[1], " ");
+
+            Set<Long> possibleValues = calculatePossibleValues(numbers);
+            if (possibleValues.contains(testValue)) {
+                result += testValue;
+            }
+        }
+
+        return result;
+    }
+
+    private Set<Long> calculatePossibleValues(List<Long> numbers) {
+        if (numbers.size() == 1) {
+            return Set.copyOf(numbers);
+        }
+
+        long lastNumber = numbers.getLast();
+        Set<Long> possibleValuesWithoutLastNumber = calculatePossibleValues(numbers.subList(0, numbers.size() - 1));
+
+        Set<Long> possibleValues = new HashSet<>();
+        for (long possibleValueWithoutLastNumber : possibleValuesWithoutLastNumber) {
+            possibleValues.add(possibleValueWithoutLastNumber * lastNumber);
+            possibleValues.add(possibleValueWithoutLastNumber + lastNumber);
+
+            if (withConcatenation) {
+                long multiplier = 1;
+                while (multiplier <= lastNumber) {
+                    multiplier *= 10;
+                }
+                possibleValues.add(multiplier * possibleValueWithoutLastNumber + lastNumber);
+            }
+        }
+        return possibleValues;
     }
 
 }
--- src/test/java/de/beachboys/aoc2024/Day07Test.java
+++ src/test/java/de/beachboys/aoc2024/Day07Test.java
@@ -15,15 +15,29 @@ public class Day07Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("190: 10 19",
+                        "3267: 81 40 27",
+                        "83: 17 5",
+                        "156: 15 6",
+                        "7290: 6 8 6 15",
+                        "161011: 16 10 13",
+                        "192: 17 8 14",
+                        "21037: 9 7 18 13",
+                        "292: 11 6 16 20"), 3749, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("190: 10 19",
+                        "3267: 81 40 27",
+                        "83: 17 5",
+                        "156: 15 6",
+                        "7290: 6 8 6 15",
+                        "161011: 16 10 13",
+                        "192: 17 8 14",
+                        "21037: 9 7 18 13",
+                        "292: 11 6 16 20"), 11387, null)
         );
     }
 
