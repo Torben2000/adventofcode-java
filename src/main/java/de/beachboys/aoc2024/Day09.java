3ab7027aeda6266e547440e2cda6c5b293c44691
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2024-12-09 13:56:58 +0100

    Add day 2024-09

--- src/main/java/de/beachboys/aoc2024/Day09.java
+++ src/main/java/de/beachboys/aoc2024/Day09.java
@@ -1,17 +1,110 @@
 package de.beachboys.aoc2024;
 
 import de.beachboys.Day;
+import de.beachboys.Util;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day09 extends Day {
 
     public Object part1(List<String> input) {
-        return "part1result";
+        List<Integer> fileSystem = parseInitialFileSystem(input);
+
+        int firstFreeBlock = 0;
+        int lastFileBlock = fileSystem.size() - 1;
+        while (firstFreeBlock <= lastFileBlock) {
+            while (fileSystem.get(firstFreeBlock) != null) {
+                firstFreeBlock++;
+            }
+            while (fileSystem.get(lastFileBlock) == null) {
+                lastFileBlock--;
+            }
+            if (firstFreeBlock < lastFileBlock) {
+                fileSystem.set(firstFreeBlock, fileSystem.get(lastFileBlock));
+                fileSystem.set(lastFileBlock, null);
+            }
+        }
+
+        return calculateCheckSum(fileSystem);
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        List<Integer> fileSystem = parseInitialFileSystem(input);
+
+        int lastFileBlock = fileSystem.size() - 1;
+        while (lastFileBlock >= 0) {
+            while (fileSystem.get(lastFileBlock) == null) {
+                lastFileBlock--;
+            }
+            Integer fileIdOfLastFileBlock = fileSystem.get(lastFileBlock);
+            int indexBeforeLastFile = lastFileBlock;
+            while (indexBeforeLastFile >= 0 && fileIdOfLastFileBlock.equals(fileSystem.get(indexBeforeLastFile))) {
+                indexBeforeLastFile--;
+            }
+            int fileSize = lastFileBlock - indexBeforeLastFile;
+
+            int startOfFreeSpace = getStartOfEnoughFreeSpace(fileSize, fileSystem);
+
+            if (startOfFreeSpace < lastFileBlock) {
+                for (int i = 0; i < fileSize; i++) {
+                    fileSystem.set(startOfFreeSpace + i, fileIdOfLastFileBlock);
+                    fileSystem.set(lastFileBlock - i, null);
+                }
+            }
+            lastFileBlock = indexBeforeLastFile;
+        }
+
+        return calculateCheckSum(fileSystem);
+    }
+
+    private static int getStartOfEnoughFreeSpace(int fileSize, List<Integer> fileSystem) {
+        int freeSpaceSize = 0;
+        int startOfFreeSpace = 0;
+        int indexAfterFreeBlock = startOfFreeSpace;
+        while (fileSize > freeSpaceSize && startOfFreeSpace < fileSystem.size()) {
+            startOfFreeSpace = indexAfterFreeBlock;
+            while (startOfFreeSpace < fileSystem.size() && fileSystem.get(startOfFreeSpace) != null) {
+                startOfFreeSpace++;
+            }
+            indexAfterFreeBlock = startOfFreeSpace;
+            while (indexAfterFreeBlock < fileSystem.size() && fileSystem.get(indexAfterFreeBlock) == null) {
+                indexAfterFreeBlock++;
+            }
+            freeSpaceSize = indexAfterFreeBlock - startOfFreeSpace;
+        }
+        return startOfFreeSpace;
+    }
+
+    private static List<Integer> parseInitialFileSystem(List<String> input) {
+        List<Integer> mapValues = Util.parseToIntList(input.getFirst(), "");
+        List<Integer> fileSystem = new ArrayList<>();
+        int fileId = 0;
+        for (int i = 0; i < mapValues.size(); i++) {
+            Integer mapValue = mapValues.get(i);
+            if (i % 2 == 0) {
+                for (int j = 0; j < mapValue; j++) {
+                    fileSystem.add(fileId);
+                }
+                fileId++;
+            } else {
+                for (int j = 0; j < mapValue; j++) {
+                    fileSystem.add(null);
+                }
+            }
+        }
+        return fileSystem;
+    }
+
+    private static long calculateCheckSum(List<Integer> fileSystem) {
+        long result = 0;
+        for (int i = 0; i < fileSystem.size(); i++) {
+            Integer fileId = fileSystem.get(i);
+            if (fileId != null) {
+                result+= (long) i * fileId;
+            }
+        }
+        return result;
     }
 
 }
--- src/test/java/de/beachboys/aoc2024/Day09Test.java
+++ src/test/java/de/beachboys/aoc2024/Day09Test.java
@@ -15,15 +15,14 @@ public class Day09Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("12345"), 60, null),
+                Arguments.of(List.of("2333133121414131402"), 1928, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("2333133121414131402"), 2858, null)
         );
     }
 
