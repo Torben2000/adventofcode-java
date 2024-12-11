e09c4d05bf0ff993e9659f5705c6934de814f4b7
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2024-12-11 05:54:59 +0100

    Add day 2024-10

--- src/main/java/de/beachboys/aoc2024/Day10.java
+++ src/main/java/de/beachboys/aoc2024/Day10.java
@@ -1,17 +1,61 @@
 package de.beachboys.aoc2024;
 
 import de.beachboys.Day;
+import de.beachboys.Direction;
+import de.beachboys.Util;
+import org.jooq.lambda.tuple.Tuple2;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Day10 extends Day {
 
+    private Map<Tuple2<Integer, Integer>, String> map;
+    private Set<Tuple2<Integer, Integer>> trailheads;
+
     public Object part1(List<String> input) {
-        return "part1result";
+        parseInput(input);
+
+        long result = 0;
+        for (Tuple2<Integer, Integer> trailhead : trailheads) {
+            result += getHikingTrails(List.of(trailhead), 0).stream().map(List::getLast).collect(Collectors.toSet()).size();
+        }
+        return result;
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        parseInput(input);
+
+        long result = 0;
+        for (Tuple2<Integer, Integer> trailhead : trailheads) {
+            result += getHikingTrails(List.of(trailhead), 0).size();
+        }
+        return result;
+    }
+
+    private void parseInput(List<String> input) {
+        map = Util.buildImageMap(input);
+        trailheads = map.entrySet().stream().filter(e -> "0".equals(e.getValue())).map(Map.Entry::getKey).collect(Collectors.toSet());
+    }
+
+    private Set<List<Tuple2<Integer, Integer>>> getHikingTrails(List<Tuple2<Integer, Integer>> currentHikingTrail, int currentHeight) {
+        Set<List<Tuple2<Integer, Integer>>> hikingTrails = new HashSet<>();
+        if (currentHeight == 9) {
+            hikingTrails.add(currentHikingTrail);
+        } else {
+            int newHeight = currentHeight + 1;
+            Tuple2<Integer, Integer> currentPos = currentHikingTrail.getLast();
+            for (Direction value : Direction.values()) {
+                Tuple2<Integer, Integer> newPos = value.move(currentPos, 1);
+                String heightAtNewPos = map.get(newPos);
+                if (String.valueOf(newHeight).equals(heightAtNewPos)) {
+                    List<Tuple2<Integer, Integer>> newHikingTrail = new ArrayList<>(currentHikingTrail);
+                    newHikingTrail.add(newPos);
+                    hikingTrails.addAll(getHikingTrails(newHikingTrail, newHeight));
+                }
+            }
+        }
+        return hikingTrails;
     }
 
 }
--- src/test/java/de/beachboys/aoc2024/Day10Test.java
+++ src/test/java/de/beachboys/aoc2024/Day10Test.java
@@ -15,15 +15,72 @@ public class Day10Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("0123",
+                        "1234",
+                        "8765",
+                        "9876"), 1, null),
+                Arguments.of(List.of("...0...",
+                        "...1...",
+                        "...2...",
+                        "6543456",
+                        "7.....7",
+                        "8.....8",
+                        "9.....9"), 2, null),
+                Arguments.of(List.of("..90..9",
+                        "...1.98",
+                        "...2..7",
+                        "6543456",
+                        "765.987",
+                        "876....",
+                        "987...."), 4, null),
+                Arguments.of(List.of("10..9..",
+                        "2...8..",
+                        "3...7..",
+                        "4567654",
+                        "...8..3",
+                        "...9..2",
+                        ".....01"), 3, null),
+                Arguments.of(List.of("89010123",
+                        "78121874",
+                        "87430965",
+                        "96549874",
+                        "45678903",
+                        "32019012",
+                        "01329801",
+                        "10456732"), 36, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of(".....0.",
+                        "..4321.",
+                        "..5..2.",
+                        "..6543.",
+                        "..7..4.",
+                        "..8765.",
+                        "..9...."), 3, null),
+                Arguments.of(List.of("..90..9",
+                        "...1.98",
+                        "...2..7",
+                        "6543456",
+                        "765.987",
+                        "876....",
+                        "987...."), 13, null),
+                Arguments.of(List.of("012345",
+                        "123456",
+                        "234567",
+                        "345678",
+                        "4.6789",
+                        "56789."), 227, null),
+                Arguments.of(List.of("89010123",
+                        "78121874",
+                        "87430965",
+                        "96549874",
+                        "45678903",
+                        "32019012",
+                        "01329801",
+                        "10456732"), 81, null)
         );
     }
 
