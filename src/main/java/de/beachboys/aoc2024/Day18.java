49e7fd08b16d293e3588a0de310c1fb78fcb1876
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2024-12-18 06:29:21 +0100

    Add day 2024-18

--- src/main/java/de/beachboys/aoc2024/Day18.java
+++ src/main/java/de/beachboys/aoc2024/Day18.java
@@ -1,17 +1,83 @@
 package de.beachboys.aoc2024;
 
 import de.beachboys.Day;
+import de.beachboys.Direction;
+import de.beachboys.Util;
+import org.jooq.lambda.tuple.Tuple;
+import org.jooq.lambda.tuple.Tuple2;
 
-import java.util.List;
+import java.util.*;
+import java.util.regex.Pattern;
 
 public class Day18 extends Day {
 
+    private final HashMap<Tuple2<Integer, Integer>, String> map = new HashMap<>();
+    private Tuple2<Integer, Integer> start;
+    private Tuple2<Integer, Integer> end;
+    private int initialBytes;
+
     public Object part1(List<String> input) {
-        return "part1result";
+        init(input);
+        return findShortestPath();
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        init(input);
+        for (int i = initialBytes; i < input.size(); i++) {
+            putByteIntoMap(input, i);
+            if (findShortestPath() == -1) {
+                return input.get(i);
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
+    private void init(List<String> input) {
+        initialBytes = Util.getIntValueFromUser("Initial number of bytes to fall", 1024, io);
+        int maxXY = Util.getIntValueFromUser("Max x or y value", 70, io);
+
+        parseInitialMap(input, maxXY, initialBytes);
+
+        start = Tuple.tuple(0,0);
+        end = Tuple.tuple(maxXY, maxXY);
+    }
+
+    private int findShortestPath() {
+        Deque<Tuple2<Tuple2<Integer, Integer>, Integer>> queue = new LinkedList<>();
+        queue.add(Tuple.tuple(start, 0));
+        Set<Tuple2<Integer, Integer>> history = new HashSet<>();
+        while (!queue.isEmpty()) {
+            Tuple2<Tuple2<Integer, Integer>, Integer> e = queue.poll();
+            if (end.equals(e.v1)) {
+                return e.v2;
+            }
+            for (Direction dir : Direction.values()) {
+                Tuple2<Integer, Integer> newPos = dir.move(e.v1, 1);
+                if (!history.contains(newPos) && ".".equals(map.get(newPos))) {
+                    history.add(newPos);
+                    queue.add(Tuple.tuple(newPos, e.v2+1));
+                }
+            }
+        }
+        return -1;
+    }
+
+    private void parseInitialMap(List<String> input, int maxXY, int initialBytes) {
+        map.clear();
+        for (int i = 0; i <= maxXY; i++) {
+            for (int j = 0; j <= maxXY; j++) {
+                map.put(Tuple.tuple(i, j), ".");
+            }
+        }
+        for (int i = 0; i < initialBytes; i++) {
+            putByteIntoMap(input, i);
+        }
+    }
+
+    private void putByteIntoMap(List<String> input, int i) {
+        String line = input.get(i);
+        String[] split = line.split(Pattern.quote(","));
+        map.put(Tuple.tuple(Integer.valueOf(split[0]), Integer.valueOf(split[1])), "#");
     }
 
 }
--- src/test/java/de/beachboys/aoc2024/Day18Test.java
+++ src/test/java/de/beachboys/aoc2024/Day18Test.java
@@ -3,6 +3,7 @@ package de.beachboys.aoc2024;
 import de.beachboys.Day;
 import de.beachboys.DayTest;
 import de.beachboys.IOHelper;
+import de.beachboys.IOHelperForTests;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -15,15 +16,61 @@ public class Day18Test extends DayTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("5,4",
+                        "4,2",
+                        "4,5",
+                        "3,0",
+                        "2,1",
+                        "6,3",
+                        "2,4",
+                        "1,5",
+                        "0,6",
+                        "3,3",
+                        "2,6",
+                        "5,1",
+                        "1,2",
+                        "5,5",
+                        "2,5",
+                        "6,5",
+                        "1,4",
+                        "0,4",
+                        "6,4",
+                        "1,1",
+                        "6,1",
+                        "1,0",
+                        "0,5",
+                        "1,6",
+                        "2,0"), 22, new IOHelperForTests(List.of("12", "6"), null))
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("5,4",
+                        "4,2",
+                        "4,5",
+                        "3,0",
+                        "2,1",
+                        "6,3",
+                        "2,4",
+                        "1,5",
+                        "0,6",
+                        "3,3",
+                        "2,6",
+                        "5,1",
+                        "1,2",
+                        "5,5",
+                        "2,5",
+                        "6,5",
+                        "1,4",
+                        "0,4",
+                        "6,4",
+                        "1,1",
+                        "6,1",
+                        "1,0",
+                        "0,5",
+                        "1,6",
+                        "2,0"), "6,1", new IOHelperForTests(List.of("12", "6"), null))
         );
     }
 
