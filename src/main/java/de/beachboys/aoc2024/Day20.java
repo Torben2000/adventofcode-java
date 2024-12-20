e8f9b80560873770069ce3d9efbccec20f9bf4a7
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2024-12-20 08:04:21 +0100

    Add day 2024-20

--- src/main/java/de/beachboys/aoc2024/Day20.java
+++ src/main/java/de/beachboys/aoc2024/Day20.java
@@ -1,17 +1,83 @@
 package de.beachboys.aoc2024;
 
 import de.beachboys.Day;
+import de.beachboys.Direction;
+import de.beachboys.Util;
+import org.jooq.lambda.tuple.Tuple2;
 
-import java.util.List;
+import java.util.*;
 
 public class Day20 extends Day {
 
     public Object part1(List<String> input) {
-        return "part1result";
+        return runLogic(input, 2);
     }
 
     public Object part2(List<String> input) {
-        return 2;
+        return runLogic(input, 20);
+    }
+
+    private Map<Tuple2<Integer, Integer>, String> map;
+    private Tuple2<Integer, Integer> start;
+    private Tuple2<Integer, Integer> end;
+    private List<Tuple2<Integer, Integer>> track;
+    private long runLogic(List<String> input, int defaultValueCheatLength) {
+        int minimumToSave = Util.getIntValueFromUser("Minimum to save", 100, io);
+        int cheatLength = Util.getIntValueFromUser("Maximum cheat length", defaultValueCheatLength, io);
+
+        parse(input);
+
+        Map<Tuple2<Integer, Integer>, Integer> distanceFromStart = buildDistancesMap(start);
+        Map<Tuple2<Integer, Integer>, Integer> distanceFromEnd = buildDistancesMap(end);
+
+        long result = 0;
+        int initialScore = distanceFromStart.get(end);
+        for (Tuple2<Integer, Integer> cheatStart : track) {
+            for (Tuple2<Integer, Integer> cheatEnd : track) {
+                int manhattanDistance = Util.getManhattanDistance(cheatStart, cheatEnd);
+                if (manhattanDistance <= cheatLength && initialScore - minimumToSave >= distanceFromStart.get(cheatStart) + manhattanDistance + distanceFromEnd.get(cheatEnd)) {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+
+    private void parse(List<String> input) {
+        map = Util.buildImageMap(input);
+        start = map.entrySet().stream().filter(e -> "S".equals(e.getValue())).map(Map.Entry::getKey).findFirst().orElseThrow();
+        end = map.entrySet().stream().filter(e -> "E".equals(e.getValue())).map(Map.Entry::getKey).findFirst().orElseThrow();
+        track = map.entrySet().stream().filter(e -> !"#".equals(e.getValue())).map(Map.Entry::getKey).toList();
+    }
+
+
+    private Map<Tuple2<Integer, Integer>, Integer> buildDistancesMap(Tuple2<Integer, Integer> start) {
+        Map<Tuple2<Integer, Integer>, Integer> distancesMap = new HashMap<>();
+        Set<Tuple2<Integer, Integer>> trackElementToAddToMap = new HashSet<>(track);
+
+        Set<Tuple2<Integer, Integer>> history = new HashSet<>();
+        history.add(start);
+        List<Tuple2<Integer, Integer>> currentTrackElements = List.of(start);
+        for (int distance = 0; !trackElementToAddToMap.isEmpty(); distance++) {
+            List<Tuple2<Integer, Integer>> nextTrackElements = new LinkedList<>();
+            for (Tuple2<Integer, Integer> trackElement : currentTrackElements) {
+                if (trackElementToAddToMap.contains(trackElement)) {
+                    distancesMap.put(trackElement, distance);
+                    trackElementToAddToMap.remove(trackElement);
+                }
+                for (Direction value : Direction.values()) {
+                    Tuple2<Integer, Integer> newPos = value.move(trackElement, 1);
+                    if (!history.contains(newPos) && map.containsKey(newPos)) {
+                        if (!"#".equals(map.get(newPos))) {
+                            nextTrackElements.add(newPos);
+                            history.add(newPos);
+                        }
+                    }
+                }
+                currentTrackElements = nextTrackElements;
+            }
+        }
+        return distancesMap;
     }
 
 }
--- src/test/java/de/beachboys/aoc2024/Day20Test.java
+++ src/test/java/de/beachboys/aoc2024/Day20Test.java
@@ -3,6 +3,7 @@ package de.beachboys.aoc2024;
 import de.beachboys.Day;
 import de.beachboys.DayTest;
 import de.beachboys.IOHelper;
+import de.beachboys.IOHelperForTests;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -11,18 +12,55 @@ import java.util.stream.Stream;
 
 public class Day20Test extends DayTest {
 
+    public static final List<String> TEST_MAZE = List.of("###############",
+            "#...#...#.....#",
+            "#.#.#.#.#.###.#",
+            "#S#...#.#.#...#",
+            "#######.#.#.###",
+            "#######.#.#...#",
+            "#######.#.###.#",
+            "###..E#...#...#",
+            "###.#######.###",
+            "#...###...#...#",
+            "#.#####.#.###.#",
+            "#.#...#.#.#...#",
+            "#.#.#.#.#.#.###",
+            "#...#...#...###",
+            "###############");
     private final Day day = new Day20();
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(TEST_MAZE, 1, new IOHelperForTests(List.of("64", "2"), null)),
+                Arguments.of(TEST_MAZE, 1+1, new IOHelperForTests(List.of("40", "2"), null)),
+                Arguments.of(TEST_MAZE, 1+1+1, new IOHelperForTests(List.of("38", "2"), null)),
+                Arguments.of(TEST_MAZE, 1+1+1+1, new IOHelperForTests(List.of("36", "2"), null)),
+                Arguments.of(TEST_MAZE, 1+1+1+1+1, new IOHelperForTests(List.of("20", "2"), null)),
+                Arguments.of(TEST_MAZE, 1+1+1+1+1+3, new IOHelperForTests(List.of("12", "2"), null)),
+                Arguments.of(TEST_MAZE, 1+1+1+1+1+3+2, new IOHelperForTests(List.of("10", "2"), null)),
+                Arguments.of(TEST_MAZE, 1+1+1+1+1+3+2+4, new IOHelperForTests(List.of("8", "2"), null)),
+                Arguments.of(TEST_MAZE, 1+1+1+1+1+3+2+4+2, new IOHelperForTests(List.of("6", "2"), null)),
+                Arguments.of(TEST_MAZE, 1+1+1+1+1+3+2+4+2+14, new IOHelperForTests(List.of("4", "2"), null)),
+                Arguments.of(TEST_MAZE, 1+1+1+1+1+3+2+4+2+14+14, new IOHelperForTests(List.of("2", "2"), null))
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
+                Arguments.of(TEST_MAZE, 3, new IOHelperForTests(List.of("76", "20"), null)),
+                Arguments.of(TEST_MAZE, 3+4, new IOHelperForTests(List.of("74", "20"), null)),
+                Arguments.of(TEST_MAZE, 3+4+22, new IOHelperForTests(List.of("72", "20"), null)),
+                Arguments.of(TEST_MAZE, 3+4+22+12, new IOHelperForTests(List.of("70", "20"), null)),
+                Arguments.of(TEST_MAZE, 3+4+22+12+14, new IOHelperForTests(List.of("68", "20"), null)),
+                Arguments.of(TEST_MAZE, 3+4+22+12+14+12, new IOHelperForTests(List.of("66", "20"), null)),
+                Arguments.of(TEST_MAZE, 3+4+22+12+14+12+19, new IOHelperForTests(List.of("64", "20"), null)),
+                Arguments.of(TEST_MAZE, 3+4+22+12+14+12+19+20, new IOHelperForTests(List.of("62", "20"), null)),
+                Arguments.of(TEST_MAZE, 3+4+22+12+14+12+19+20+23, new IOHelperForTests(List.of("60", "20"), null)),
+                Arguments.of(TEST_MAZE, 3+4+22+12+14+12+19+20+23+25, new IOHelperForTests(List.of("58", "20"), null)),
+                Arguments.of(TEST_MAZE, 3+4+22+12+14+12+19+20+23+25+39, new IOHelperForTests(List.of("56", "20"), null)),
+                Arguments.of(TEST_MAZE, 3+4+22+12+14+12+19+20+23+25+39+29, new IOHelperForTests(List.of("54", "20"), null)),
+                Arguments.of(TEST_MAZE, 3+4+22+12+14+12+19+20+23+25+39+29+31, new IOHelperForTests(List.of("52", "20"), null)),
+                Arguments.of(TEST_MAZE, 3+4+22+12+14+12+19+20+23+25+39+29+31+32, new IOHelperForTests(List.of("50", "20"), null))
 
         );
     }
