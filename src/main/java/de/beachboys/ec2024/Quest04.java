a94bf82841aac9063d6c16329466df3978324dd4
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2025-10-18 22:31:25 +0200

    Add quest 2024-04

--- src/main/java/de/beachboys/ec2024/Quest04.java
+++ src/main/java/de/beachboys/ec2024/Quest04.java
@@ -2,22 +2,38 @@ package de.beachboys.ec2024;
 
 import de.beachboys.Quest;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Quest04 extends Quest {
 
     @Override
     public Object part1(List<String> input) {
-        return "part1result";
-    }
+        int[] nails = input.stream().mapToInt(Integer::valueOf).toArray();
+
+        int minHeight = Arrays.stream(nails).min().orElseThrow();
+
+        return Arrays.stream(nails).map(nail -> nail - minHeight).sum();
+     }
 
     @Override
     public Object part2(List<String> input) {
-        return 2;
+         return part1(input);
     }
 
     @Override
     public Object part3(List<String> input) {
-        return 3;
+        long result = Long.MAX_VALUE;
+        int[] nails = input.stream().mapToInt(Integer::valueOf).toArray();
+
+        int minHeight = Arrays.stream(nails).min().orElseThrow();
+        int maxHeight = Arrays.stream(nails).max().orElseThrow();
+
+        for (int height = minHeight; height <= maxHeight; height++) {
+            int currentHeight = height;
+            result = Math.min(result, Arrays.stream(nails).map(nail -> Math.abs(nail - currentHeight)).sum());
+        }
+
+        return result;
     }
 }
--- src/test/java/de/beachboys/ec2024/Quest04Test.java
+++ src/test/java/de/beachboys/ec2024/Quest04Test.java
@@ -16,21 +16,31 @@ public class Quest04Test extends QuestTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
+                Arguments.of(List.of("3",
+                        "4",
+                        "7",
+                        "8"), 10, null)
 
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
+                Arguments.of(List.of("3",
+                        "4",
+                        "7",
+                        "8"), 10, null)
 
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart3() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 3, null)
+                Arguments.of(List.of("2",
+                        "4",
+                        "5",
+                        "6",
+                        "8"), 8, null)
 
         );
     }
