96d7c4246d559d9a36b80eff7f7f89eb539f694d
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2025-10-23 22:50:46 +0200

    Add quest 2024-14

--- src/test/java/de/beachboys/ec2024/Quest14Test.java
+++ src/test/java/de/beachboys/ec2024/Quest14Test.java
@@ -16,22 +16,26 @@ public class Quest14Test extends QuestTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("U5,R3,D2,L5,U4,R5,D2"), 7, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("U5,R3,D2,L5,U4,R5,D2",
+                        "U6,L1,D2,R3,U2,L1"), 32, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart3() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 3, null)
-
+                Arguments.of(List.of("U5,R3,D2,L5,U4,R5,D2",
+                        "U6,L1,D2,R3,U2,L1"), 5, null),
+                Arguments.of(List.of("U20,L1,B1,L2,B1,R2,L1,F1,U1",
+                        "U10,F1,B1,R1,L1,B1,L1,F1,R2,U1",
+                        "U30,L2,F1,R1,B1,R1,F2,U1,F1",
+                        "U25,R1,L2,B1,U1,R2,F1,L2",
+                        "U16,L1,B1,L1,B3,L1,B1,F1"), 46, null)
         );
     }
 
