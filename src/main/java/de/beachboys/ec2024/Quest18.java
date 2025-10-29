1f22913b02c0f241c2c887d06915011ac9dd5ee5
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2025-10-29 16:44:21 +0100

    Add quest 2024-18

--- src/main/java/de/beachboys/ec2024/Quest18.java
+++ src/main/java/de/beachboys/ec2024/Quest18.java
@@ -1,23 +1,80 @@
 package de.beachboys.ec2024;
 
+import de.beachboys.Direction;
 import de.beachboys.Quest;
+import de.beachboys.Util;
+import org.jooq.lambda.tuple.Tuple;
+import org.jooq.lambda.tuple.Tuple2;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Quest18 extends Quest {
 
+    private Map<Tuple2<Integer, Integer>, String> map;
+    private Set<Tuple2<Integer, Integer>> palmTrees;
+
     @Override
     public Object part1(List<String> input) {
-        return "part1result";
+        fillMapAndPalmTrees(input);
+        Set<Tuple2<Integer, Integer>> startingPositions = Set.of(Tuple.tuple(0, 1));
+        return getDurationsToReachGoals(startingPositions, palmTrees).values().stream().mapToInt(Integer::intValue).max().orElseThrow();
+
     }
 
     @Override
     public Object part2(List<String> input) {
-        return 2;
+        fillMapAndPalmTrees(input);
+        Set<Tuple2<Integer, Integer>> startingPositions = Set.of(Tuple.tuple(0, 1), Tuple.tuple(input.getFirst().length() - 1, input.size() - 2));
+        return getDurationsToReachGoals(startingPositions, palmTrees).values().stream().mapToInt(Integer::intValue).max().orElseThrow();
     }
 
     @Override
     public Object part3(List<String> input) {
-        return 3;
+        fillMapAndPalmTrees(input);
+        Set<Tuple2<Integer, Integer>> wellLocations = map.entrySet().stream().filter(e -> ".".equals(e.getValue())).map(Map.Entry::getKey).collect(Collectors.toSet());
+
+        Map<Tuple2<Integer, Integer>, Integer> sumsPerWellLocation = new HashMap<>();
+        for (Tuple2<Integer, Integer> palmTree : palmTrees) {
+            Map<Tuple2<Integer, Integer>, Integer> durationsToReachWellLocations = getDurationsToReachGoals(Set.of(palmTree), wellLocations);
+            for (Tuple2<Integer, Integer> wellLocation : durationsToReachWellLocations.keySet()) {
+                sumsPerWellLocation.put(wellLocation, sumsPerWellLocation.getOrDefault(wellLocation, 0) + durationsToReachWellLocations.get(wellLocation));
+            }
+        }
+
+        return sumsPerWellLocation.values().stream().mapToInt(Integer::intValue).min().orElseThrow();
+    }
+
+    private void fillMapAndPalmTrees(List<String> input) {
+        map = Util.buildImageMap(input);
+        palmTrees = map.entrySet().stream().filter(e -> "P".equals(e.getValue())).map(Map.Entry::getKey).collect(Collectors.toSet());
+    }
+
+    private Map<Tuple2<Integer, Integer>, Integer> getDurationsToReachGoals(Set<Tuple2<Integer, Integer>> startingPositions, Set<Tuple2<Integer, Integer>> goals) {
+        Set<Tuple2<Integer, Integer>> seenGoals = new HashSet<>();
+        Set<Tuple2<Integer, Integer>> seen = new HashSet<>();
+        Map<Tuple2<Integer, Integer>, Integer> returnMap = new HashMap<>();
+        Set<Tuple2<Integer, Integer>> currentPositions = startingPositions;
+        for (int minute = 0; minute < Integer.MAX_VALUE; minute++) {
+            Set<Tuple2<Integer, Integer>> newCurrentPositions = new HashSet<>();
+            for (Tuple2<Integer, Integer> pos : currentPositions) {
+                if (goals.contains(pos) && !seenGoals.contains(pos)) {
+                    seenGoals.add(pos);
+                    returnMap.put(pos, minute);
+                    if (seenGoals.size() == goals.size()) {
+                        return returnMap;
+                    }
+                }
+                seen.add(pos);
+                for (Direction dir : Direction.values()) {
+                    Tuple2<Integer, Integer> newPos = dir.move(pos, 1);
+                    if (map.containsKey(newPos) && !seen.contains(newPos) && !"#".equals(map.get(newPos))) {
+                        newCurrentPositions.add(newPos);
+                    }
+                }
+            }
+            currentPositions = newCurrentPositions;
+        }
+        throw new IllegalArgumentException();
     }
 }
--- src/test/java/de/beachboys/ec2024/Quest18Test.java
+++ src/test/java/de/beachboys/ec2024/Quest18Test.java
@@ -16,22 +16,35 @@ public class Quest18Test extends QuestTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
-
+                Arguments.of(List.of("##########",
+                        "..#......#",
+                        "#.P.####P#",
+                        "#.#...P#.#",
+                        "##########"), 11, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
-
+                Arguments.of(List.of("#######################",
+                        "...P..P...#P....#.....#",
+                        "#.#######.#.#.#.#####.#",
+                        "#.....#...#P#.#..P....#",
+                        "#.#####.#####.#########",
+                        "#...P....P.P.P.....P#.#",
+                        "#.#######.#####.#.#.#.#",
+                        "#...#.....#P...P#.#....",
+                        "#######################"), 21, null)
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart3() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 3, null)
-
+                Arguments.of(List.of("##########",
+                        "#.#......#",
+                        "#.P.####P#",
+                        "#.#...P#.#",
+                        "##########"), 12, null)
         );
     }
 
