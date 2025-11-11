32865283bb707c5b80d2b52acb42e1a8c4c3ea03
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2025-11-12 00:48:41 +0100

    Add quest 2025-07

--- src/main/java/de/beachboys/ec2025/Quest07.java
+++ src/main/java/de/beachboys/ec2025/Quest07.java
@@ -1,23 +1,90 @@
 package de.beachboys.ec2025;
 
 import de.beachboys.Quest;
+import de.beachboys.Util;
 
-import java.util.List;
+import java.util.*;
+import java.util.regex.Pattern;
 
 public class Quest07 extends Quest {
 
+    private List<String> names;
+    private Map<String, List<String>> rules;
     @Override
     public Object part1(List<String> input) {
-        return "part1result";
+        parseInput(input);
+
+        for (String name : names) {
+            if (isValidName(name)) {
+                return name;
+            }
+        }
+        throw new IllegalArgumentException();
     }
 
     @Override
     public Object part2(List<String> input) {
-        return 2;
+        parseInput(input);
+
+        long result = 0;
+        for (int i = 0; i < names.size(); i++) {
+            String name = names.get(i);
+            if (isValidName(name)) {
+                result += i + 1;
+            }
+        }
+        return result;
     }
 
     @Override
     public Object part3(List<String> input) {
-        return 3;
+        parseInput(input);
+
+        Set<String> res = new HashSet<>();
+        for (String name : names) {
+            if (isValidName(name)) {
+                res.addAll(getAllPossibleNames(name));
+            }
+        }
+        return res.size();
+    }
+
+    private boolean isValidName(String name) {
+        for (int i = 0; i < name.length() - 1; i++) {
+            String letter = name.substring(i, i + 1);
+            String nextLetter = name.substring(i + 1, i + 2);
+            if (!rules.containsKey(letter) || !rules.get(letter).contains(nextLetter)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private Set<String> getAllPossibleNames(String prefix) {
+        if (prefix.length() > 11) {
+            return Set.of();
+        }
+        Set<String> result = new HashSet<>();
+        if (prefix.length() >= 7) {
+            result.add(prefix);
+        }
+        String lastLetterOfPrefix = prefix.substring(prefix.length() - 1);
+        if (rules.containsKey(lastLetterOfPrefix)) {
+            for (String nextLetter : rules.get(lastLetterOfPrefix)) {
+                result.addAll(getAllPossibleNames(prefix + nextLetter));
+            }
+        }
+        return result;
+    }
+
+    private void parseInput(List<String> input) {
+        names = Util.parseCsv(input.getFirst());
+
+        rules = new HashMap<>();
+        for (int i = 2; i < input.size(); i++) {
+            String ruleLine = input.get(i);
+            String[] splitRule = ruleLine.split(Pattern.quote(" > "));
+            rules.put(splitRule[0], Util.parseCsv(splitRule[1]));
+        }
     }
 }
--- src/test/java/de/beachboys/ec2025/Quest07Test.java
+++ src/test/java/de/beachboys/ec2025/Quest07Test.java
@@ -16,21 +16,83 @@ public class Quest07Test extends QuestTest {
 
     private static Stream<Arguments> provideTestDataForPart1() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), "part1result", null)
+                Arguments.of(List.of("Oronris,Urakris,Oroneth,Uraketh",
+                        "",
+                        "r > a,i,o",
+                        "i > p,w",
+                        "n > e,r",
+                        "o > n,m",
+                        "k > f,r",
+                        "a > k",
+                        "U > r",
+                        "e > t",
+                        "O > r",
+                        "t > h"), "Oroneth", null)
 
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart2() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 2, null)
+                Arguments.of(List.of("Xanverax,Khargyth,Nexzeth,Helther,Braerex,Tirgryph,Kharverax",
+                        "",
+                        "r > v,e,a,g,y",
+                        "a > e,v,x,r",
+                        "e > r,x,v,t",
+                        "h > a,e,v",
+                        "g > r,y",
+                        "y > p,t",
+                        "i > v,r",
+                        "K > h",
+                        "v > e",
+                        "B > r",
+                        "t > h",
+                        "N > e",
+                        "p > h",
+                        "H > e",
+                        "l > t",
+                        "z > e",
+                        "X > a",
+                        "n > v",
+                        "x > z",
+                        "T > i"), 23, null)
 
         );
     }
 
     private static Stream<Arguments> provideTestDataForPart3() {
         return Stream.of(
-                Arguments.of(List.of("inputLines"), 3, null)
+                Arguments.of(List.of("Xaryt",
+                        "",
+                        "X > a,o",
+                        "a > r,t",
+                        "r > y,e,a",
+                        "h > a,e,v",
+                        "t > h",
+                        "v > e",
+                        "y > p,t"), 25, null),
+                Arguments.of(List.of("Khara,Xaryt,Noxer,Kharax",
+                        "",
+                        "r > v,e,a,g,y",
+                        "a > e,v,x,r,g",
+                        "e > r,x,v,t",
+                        "h > a,e,v",
+                        "g > r,y",
+                        "y > p,t",
+                        "i > v,r",
+                        "K > h",
+                        "v > e",
+                        "B > r",
+                        "t > h",
+                        "N > e",
+                        "p > h",
+                        "H > e",
+                        "l > t",
+                        "z > e",
+                        "X > a",
+                        "n > v",
+                        "x > z",
+                        "T > i"), 1154, null)
 
         );
     }
