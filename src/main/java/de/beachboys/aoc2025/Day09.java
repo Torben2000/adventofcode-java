49c180b51089c41bece2a68fb97b5d745254e934
Torben2000 <7420304+Torben2000@users.noreply.github.com>
2025-12-09 22:30:00 +0100

    Rewrote day 2025-09 for a huge performance boost

--- src/main/java/de/beachboys/aoc2025/Day09.java
+++ src/main/java/de/beachboys/aoc2025/Day09.java
@@ -3,50 +3,32 @@ package de.beachboys.aoc2025;
 import de.beachboys.Day;
 import org.jooq.lambda.tuple.Tuple;
 import org.jooq.lambda.tuple.Tuple2;
-import org.locationtech.jts.geom.Coordinate;
-import org.locationtech.jts.geom.CoordinateXY;
-import org.locationtech.jts.geom.GeometryFactory;
-import org.locationtech.jts.geom.Polygon;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiPredicate;
 import java.util.regex.Pattern;
 
 public class Day09 extends Day {
 
+    private final List<Tuple2<Integer, Integer>> redTiles = new ArrayList<>();
+
     public Object part1(List<String> input) {
-        long result = 0;
-        List<Tuple2<Integer, Integer>> redTiles = parseInput(input);
-        for (int i = 0; i < redTiles.size(); i++) {
-            Tuple2<Integer, Integer> t1 = redTiles.get(i);
-            for (int j = i + 1; j < redTiles.size(); j++) {
-                Tuple2<Integer, Integer> t2 = redTiles.get(j);
-                long area = getBoxArea(t1, t2);
-                if (area > result) {
-                    result = area;
-                }
-            }
-        }
-        return result;
+        return runLogic(input, (t1, t2) -> true);
     }
 
     public Object part2(List<String> input) {
+        return runLogic(input, this::isRectangleInRedTilesPolygon);
+    }
+
+    private long runLogic(List<String> input, BiPredicate<Tuple2<Integer, Integer>, Tuple2<Integer, Integer>> isAllowedRectangle) {
+        parseInput(input);
         long result = 0;
-        List<Tuple2<Integer, Integer>> redTiles = parseInput(input);
-
-        GeometryFactory f = new GeometryFactory();
-        Coordinate[] polygonCoordinates = new Coordinate[redTiles.size()+1];
-        for (int i = 0; i < redTiles.size(); i++) {
-            polygonCoordinates[i] = new CoordinateXY(redTiles.get(i).v1, redTiles.get(i).v2);
-        }
-        polygonCoordinates[redTiles.size()] = new CoordinateXY(redTiles.getFirst().v1, redTiles.getFirst().v2);
-        Polygon polygon = f.createPolygon(polygonCoordinates);
-
         for (int i = 0; i < redTiles.size(); i++) {
             Tuple2<Integer, Integer> t1 = redTiles.get(i);
             for (int j = i + 1; j < redTiles.size(); j++) {
                 Tuple2<Integer, Integer> t2 = redTiles.get(j);
-                if (polygon.contains(getRectangle(t1, t2, f))) {
+                if (isAllowedRectangle.test(t1, t2)) {
                     long area = getBoxArea(t1, t2);
                     if (area > result) {
                         result = area;
@@ -57,27 +39,34 @@ public class Day09 extends Day {
         return result;
     }
 
+    private boolean isRectangleInRedTilesPolygon(Tuple2<Integer, Integer> cornerOfRectangle, Tuple2<Integer, Integer> oppositeCornerOfRectangle) {
+        for (int k = 0; k < redTiles.size(); k++) {
+            Tuple2<Integer, Integer> lineStart = redTiles.get(k);
+            Tuple2<Integer, Integer> lineEnd = redTiles.get((k + 1) % redTiles.size());
+            if (lineCrossesRectangleBorders(lineStart, lineEnd, cornerOfRectangle, oppositeCornerOfRectangle)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean lineCrossesRectangleBorders(Tuple2<Integer, Integer> lineStart, Tuple2<Integer, Integer> lineEnd, Tuple2<Integer, Integer> cornerOfRectangle, Tuple2<Integer, Integer> oppositeCornerOfRectangle) {
+        return Math.min(lineStart.v1, lineEnd.v1) < Math.max(cornerOfRectangle.v1, oppositeCornerOfRectangle.v1)
+                && Math.max(lineStart.v1, lineEnd.v1) > Math.min(cornerOfRectangle.v1, oppositeCornerOfRectangle.v1)
+                && Math.min(lineStart.v2, lineEnd.v2) < Math.max(cornerOfRectangle.v2, oppositeCornerOfRectangle.v2)
+                && Math.max(lineStart.v2, lineEnd.v2) > Math.min(cornerOfRectangle.v2, oppositeCornerOfRectangle.v2);
+    }
+
     private static long getBoxArea(Tuple2<Integer, Integer> t1, Tuple2<Integer, Integer> t2) {
-        return (long) (Math.abs(t1.v1 - t2.v1) + 1) * (Math.abs(t1.v2 - t2.v2) + 1);
+        return (long) (Math.abs(t1.v1 - t2.v1) + 1) * (long)(Math.abs(t1.v2 - t2.v2) + 1);
     }
 
-    private static Polygon getRectangle(Tuple2<Integer, Integer> t1, Tuple2<Integer, Integer> t2, GeometryFactory f) {
-        Coordinate[] rectangleCoordinates = new Coordinate[5];
-        rectangleCoordinates[0] = new CoordinateXY(t1.v1, t1.v2);
-        rectangleCoordinates[1] = new CoordinateXY(t1.v1, t2.v2);
-        rectangleCoordinates[2] = new CoordinateXY(t2.v1, t2.v2);
-        rectangleCoordinates[3] = new CoordinateXY(t2.v1, t1.v2);
-        rectangleCoordinates[4] = new CoordinateXY(t1.v1, t1.v2);
-        return f.createPolygon(rectangleCoordinates);
-    }
-
-    private static List<Tuple2<Integer, Integer>> parseInput(List<String> input) {
-        List<Tuple2<Integer, Integer>> redTiles = new ArrayList<>();
+    private void parseInput(List<String> input) {
+        redTiles.clear();
         for (String line : input) {
             String[] split = line.split(Pattern.quote(","));
             redTiles.add(Tuple.tuple(Integer.parseInt(split[0]), Integer.parseInt(split[1])));
         }
-        return redTiles;
     }
 
 
